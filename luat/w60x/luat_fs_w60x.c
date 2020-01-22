2947c2f5ac2fd3ab64e0cf5fb2385f28773eceb8
Wendal Chen <wendal1985@gmail.com>
2020-01-22 12:25:52 +0800

    change: 恢复使用内置flash存储lua脚本.using onchip flash for lua script

--- luat/w60x/luat_fs_w60x.c
+++ luat/w60x/luat_fs_w60x.c
@@ -13,27 +13,26 @@
 
 int luat_fs_init() {
     //luat_lfs_init();
-    #ifdef RT_USING_SFUD
-    dfs_mount("W25QXX", "/", "elm", 0, 0);
-    #endif
-    //dfs_mount("spi01", "/", "lfs2", 0, 0);
+    //#ifdef RT_USING_SFUD
+    //dfs_mount("W25QXX", "/", "elm", 0, 0);
+    //#endif
+    dfs_mount("spi01", "/", "lfs2", 0, 0);
 }
 
-#ifdef RT_USING_SFUD
 rt_err_t wm_spi_bus_attach_device(const char *bus_name, const char *device_name, rt_uint32_t pin);
 static int rt_hw_spi_flash_init(void)
 {
   wm_spi_bus_attach_device(WM_SPI_BUS_NAME, "spi01", 20);
 
+#ifdef RT_USING_SFUD
   if (RT_NULL == rt_sfud_flash_probe("W25QXX", "spi01"))
   {
     //return -RT_ERROR;
   }
-  
+#endif
   return RT_EOK;
 }
 INIT_COMPONENT_EXPORT(rt_hw_spi_flash_init);
-#endif
 
 #endif
 

39eff244e4cac18f4419059c5093e3aa00729fbe
Wendal Chen <wendal1985@gmail.com>
2020-02-23 21:04:43 +0800

    add: 添加i2c模块的测试版,但尝试读取DHT12温湿度传感器的数据失败了.
    测试U8G2控制SSD1306,正常使用, 证明i2c配置是对的, 起码单向写数据正常.

---
+++ luat/modules/luat_lib_i2c.c
@@ -0,0 +1,95 @@
+
+#include "luat_base.h"
+#include "luat_log.h"
+#include "luat_timer.h"
+#include "luat_malloc.h"
+#include "luat_i2c.h"
+
+static int l_i2c_exist(lua_State *L) {
+    int re = luat_i2c_exist(luaL_checkinteger(L, 1));
+    lua_pushinteger(L, re);
+    return 1;
+}
+
+static int l_i2c_setup(lua_State *L) {
+    int re = luat_i2c_setup(luaL_checkinteger(L, 1), luaL_optinteger(L, 2, 0), luaL_optinteger(L, 3, 0));
+    lua_pushinteger(L, re == 0 ? 1 : 0);
+    return 1;
+}
+
+static int l_i2c_send(lua_State *L) {
+    int id = luaL_checkinteger(L, 1);
+    int addr = luaL_checkinteger(L, 2);
+    size_t len;
+    if (lua_isstring(L, 3)) {
+        const char* buff = luaL_checklstring(L, 3, &len);
+        luat_i2c_send(id, addr, buff, len);
+    }
+    else if (lua_isinteger(L, 3)) {
+        len = lua_gettop(L) - 2;
+        char buff[len+1];
+        for (size_t i = 0; i < len; i++)
+        {
+            buff[i] = lua_tointeger(L, 3+i);
+        }
+        luat_i2c_send(id, addr, buff, len);
+    }
+    return 0;
+}
+
+static int l_i2c_recv(lua_State *L) {
+    int id = luaL_checkinteger(L, 1);
+    int addr = luaL_checkinteger(L, 2);
+    int len = luaL_checkinteger(L, 3);
+    char buf[len];
+    luat_i2c_recv(id, addr, &buf[0], len);
+    lua_pushlstring(L, buf, len);
+    return 1;
+}
+
+static int l_i2c_write_reg(lua_State *L) {
+    int id = luaL_checkinteger(L, 1);
+    int addr = luaL_checkinteger(L, 2);
+    int reg = luaL_checkinteger(L, 3);
+    size_t len;
+    if (lua_isstring(L, 4)) {
+        const char* buff = luaL_checklstring(L, 4, &len);
+        luat_i2c_write_reg(id, addr, reg, buff, len);
+    }
+    else if (lua_isinteger(L, 4)) {
+        len = lua_gettop(L) - 2;
+        char buff[len+1];
+        for (size_t i = 0; i < len; i++)
+        {
+            buff[i] = lua_tointeger(L, 3+i);
+        }
+        luat_i2c_write_reg(id, addr, reg, buff, len);
+    }
+    return 0;
+}
+
+static int l_i2c_close(lua_State *L) {
+    int id = luaL_checkinteger(L, 1);
+    luat_ic2_close(id);
+    return 0;
+}
+
+#include "rotable.h"
+static const rotable_Reg reg_i2c[] =
+{
+    { "exist", l_i2c_exist, 0},
+    { "setup", l_i2c_setup, 0},
+    { "send", l_i2c_send, 0},
+    { "recv", l_i2c_recv, 0},
+    { "writeReg", l_i2c_write_reg, 0},
+    // { "readReg", l_i2c_read_reg, 0},
+    { "close", l_i2c_close, 0},
+    { "FAST",  NULL, 1},
+    { "SLOW",  NULL, 0},
+	{ NULL, NULL, 0}
+};
+
+LUAMOD_API int luaopen_i2c( lua_State *L ) {
+    rotable_newlib(L, reg_i2c);
+    return 1;
+}
---
+++ script/demo/12.dht12/main.lua
@@ -0,0 +1,41 @@
+local sys = require "sys"
+
+-- 初始化并打开I2C操作DHT12
+local function read_dht12(id)
+    --if i2c.setup(id, i2c.SLOW) ~= i2c.SLOW then
+    --    log.error("I2C.init is: ", "fail")
+    --    i2c.close(id)
+    --    return
+    --end
+    local addr = 0xB8
+    i2c.writeReg(id, addr, 0x00, 0x00)
+    sys.wait(400)
+    local data = i2c.recv(id, addr, 5)
+    --i2c.close(id)
+    log.info("DHT12", data:byte(1), data:byte(2), data:byte(3), data:byte(4), data:byte(5))
+    --log.info("DHT12 HEX data: ", data:toHex())
+    -- 分别是湿度整数,湿度小数,温度整数,温度湿度
+    --[[
+    local _, h_H, h_L, t_H, t_L = pack.unpack(data, 'b4')
+    log.info("DHT12 data: ", h_H, h_L, t_H, t_L)
+    -- 需要考虑温度低于0度的情况, t_L第0位是符号位
+    local t_L2 = tonumber(t_L)
+    if t_L2 > 127 then
+        return h_H .. ".".. h_L, "-" .. t_H .. "." .. tostring(t_L2 - 128)
+    else
+        return h_H .. ".".. h_L, t_H .. "." .. t_L
+    end
+    ]]
+end
+
+sys.timerLoopStart(function() print("READY") end, 3000)
+
+
+sys.taskInit(function()
+    while 1 do
+        print("DHT12", read_dht12(1))
+        sys.wait(3000)
+    end
+end)
+
+sys.run()

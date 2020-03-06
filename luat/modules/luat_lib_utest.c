fd99f9b69644170b6e48981708e18e7b46d6944c
Wendal Chen <wendal1985@gmail.com>
2020-03-06 11:49:04 +0800

    fix: 修正rtos.timer消息回调导致的内存泄漏问题(repeat参数类型用了无符号数) change: 修改msgbus的机制,添加2个参数 change: rtos执行回调时,把msg压入栈 change: 根据msgbus和rtos的调整, 重构uart库 update: sys.lua协程出错时也打印出错原因

--- luat/modules/luat_lib_utest.c
+++ luat/modules/luat_lib_utest.c
@@ -11,31 +11,6 @@
 // #include "ulog.h"
 
 static int l_utest_600_mem_check(lua_State *L) {
-    void* ptr = 0x20028000 + 64*1024; // 当前设置的内存大小是64k, 所以从64k的位置开始测试
-    
-    void* blank = rt_malloc(1024);
-    char* t;
-    rt_memset(blank, 0, 1024);
-    for (size_t i = 0; i < 64; i++)
-    {
-        //if (rt_memcmp(blank, ptr+i*1024, 1024)) {
-        //    rt_kprintf("Found Not Zero at 0x%08X\n", ptr+i*1024);
-        //    LOG_HEX("128K", 16, ptr+i*1024, 1024);
-        //    break;
-        //}
-        for (size_t j = 0; j < 1024; j++)
-        {
-            t = (char*)(ptr+1024*i+j);
-            if (*t != 0) {
-                rt_kprintf("Found Not Zero at 0x%08X\n", t);
-                //LOG_HEX("128K", 16, t, 1024);
-                i = 1024;
-                break;
-            }
-        }
-        
-    }
-    
     return 0;
 }
 
--- script/demo/11.uart_irq/main.lua
+++ script/demo/11.uart_irq/main.lua
@@ -1,34 +1,24 @@
 local sys = require "sys"
 
 local uartid = 1
-local maxBuffer = 4*100
 
 --初始化
 local result = uart.setup(
     uartid,--串口id
     115200,--波特率
     8,--数据位
-    1,--停止位
-    uart.None,--校验位
-    uart.LSB,--高低位顺序
-    maxBuffer,--缓冲区大小
-    function ()--接收回调
-        local str = uart.read(uartid,maxBuffer)
-        print("uart","receive:"..str)
-    end,
-    function ()--发送完成回调
-        print("uart","send ok")
-    end
+    1--停止位
 )
 
---返回值为0，表示打开成功
-if result ~= 0 then
-    print("uart open error",result)
-    error("uart open fail")
-end
 
 --循环发数据
 sys.timerLoopStart(uart.write,1000,uartid,"test")
+uart.on(uartid, "receive", function(id, len)
+    log.info("uart", "receive", id, len, uart.read(uartid, len))
+end)
+uart.on(uartid, "sent", function(id)
+    log.info("uart", "sent", id)
+end)
 
 --轮询例子
 -- sys.taskInit(function ()
@@ -41,5 +31,20 @@ sys.timerLoopStart(uart.write,1000,uartid,"test")
 --         sys.wait(100)--不阻塞的延时函数
 --     end
 -- end)
+--[[
+
+    uart.None,--校验位
+    uart.LSB,--高低位顺序
+    maxBuffer,--缓冲区大小
+    function ()--接收回调
+        local str = uart.read(uartid,maxBuffer)
+        print("uart","receive:"..str)
+    end,
+    function ()--发送完成回调
+        print("uart","send ok")
+    end
+
+
+]]
 
 sys.run()

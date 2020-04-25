e37494424839c608ec728cedaca46f8d2101650b
Wendal Chen <wendal1985@gmail.com>
2020-04-25 21:22:24 +0800

    add: 添加tcp长连接的demo

---
+++ script/demo/81.socket/main.lua
@@ -0,0 +1,76 @@
+--[[
+demo说明:
+wifi信息: 修改 wlan.connect
+服务器信息, 修改 s:host s:port
+可访问 http://tcplab.openluat.com 获取一个临时的tcp端口, 只适合测试ASCII字符
+]]
+_G.sys = require("sys")
+
+log.info("main", "socket demo, tcp long connect")
+
+log.info("mac", wlan.get_mac())
+wlan.connect("uiot", "12345678") -- wifi信息在这里, 如果想用airkiss, 看app/playit/main.lua
+
+sys.subscribe("WLAN_READY", function ()
+    print("!!! wlan ready event !!! send ntp")
+    socket.ntpSync()
+end)
+
+local PB7 = 27
+gpio.setup(PB7, function(msg) log.info("IQR", "PB7/27", msg) end)
+
+sys.taskInit(function()
+    -- 等待联网成功
+    sys.waitUntil("WLAN_READY", 30000)
+    while 1 do
+        log.info("main", "socket loop")
+        if wlan.ready() then -- TODO 感觉wlan.ready()不是很准确,还得研究一下
+            collectgarbage("collect")
+            collectgarbage("collect")
+            local s = socket.tcp()
+            -- inews.gtimg.com/newsapp_bt/0/11443709755/1000
+            s:host("180.97.81.180") -- 改成服务器ip或者域名
+            s:port(57892) -- 改成服务器端口
+            s:on("connect", function(id, re)
+                -- 连接成功后, 发注册包 {mac:"AABBCCDDEEFF"}
+                log.info("netc", "connect result", re)
+                if re then
+                    local jdata = json.encode({mac=wlan.get_mac()})
+                    s:send(jdata)
+                end
+            end)
+
+            s:on("recv", function(id, re)
+                print("recv", id, #re, re)
+                if re then
+                    -- 如果有数据就回显, 简单环路测试
+                    s:send(re)
+                end
+            end)
+            --log.info("netc", "info", s)
+            -- 启动tcp独立线程(系统级的)
+            if s:start() == 0 then
+                -- 启动成功后, 连接中断时必然有NETC_END_XX事件
+                -- 因为是长连接, 就这样等着就差不多了
+                while not sys.waitUntil("NETC_END_" .. s:id(), 30000) do
+                    -- 定时发个心跳什么的
+                    s:send("ping")
+                end
+                log.info("netc", "GET NETC_END or timeout")
+            else
+                log.info("netc", "netc start fail!!")
+            end
+            -- 一定要清理好, 不然内存泄漏, 模块内存少啊!!!
+            s:clean()
+            collectgarbage("collect")
+            collectgarbage("collect")
+            --sys_memdump()
+            sys.wait(2000) -- 没连上, 或者断开了? 等2秒重试
+        else
+            log.info("wifi", "wifi NOT ready yet")
+            sys.waitUntil("WLAN_READY", 30000)
+        end
+    end
+end)
+
+sys.run()

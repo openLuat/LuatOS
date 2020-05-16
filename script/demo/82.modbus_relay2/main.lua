e02270af98f8ad864609be73c2d4e78bbc829be9
Wendal Chen <wendal1985@gmail.com>
2020-05-16 21:53:14 +0800

    add: wifi联网后,发布通用联网消息NET_READY. update: 更新demo-82.modbus_relay2,优化为wifi代码和通用tcp联网演示,两个部分

--- script/demo/82.modbus_relay2/main.lua
+++ script/demo/82.modbus_relay2/main.lua
@@ -1,20 +1,48 @@
 --[[
 demo说明:
-wifi信息: 修改 wlan.connect
+1. 演示wifi联网操作
+2. 演示长连接操作
+3. 演示简易的网络状态灯
 ]]
 _G.sys = require("sys")
 
 log.info("main", "simple modbus relay demo")
 
-log.info("mac", wlan.get_mac())
-wlan.connect("uiot", "12345678") -- wifi信息在这里, 如果想用airkiss, 看app/playit/main.lua
+-- //////////////////////////////////////////////////////////////////////////////////////
+-- wifi 相关的代码
+if wlan ~= nil then
+    log.info("mac", wlan.get_mac())
+    local ssid = "uiot"
+    local password = "12345678"
+    -- 方式1 直接连接, 简单快捷
+    --wlan.connect(ssid, password) -- 直接连
+    -- 方式2 先扫描,再连接. 例如根据rssi(信号强度)的不同, 择优选择ssid
+    sys.taskInit(function()
+        wlan.scan()
+        sys.waitUntil("WLAN_SCAN_DONE", 30000)
+        local re = wlan.scan_get_info()
+        log.info("wlan", "scan done", #re)
+        for i in ipairs(re) do
+            log.info("wlan", "info", re[i].ssid, re[i].rssi)
+        end
+        log.info("wlan", "try connect to wifi")
+        wlan.connect(ssid, password)
+        sys.waitUntil("WLAN_READY", 15000)
+        log.info("wifi", "self ip", socket.ip())
+    end)
+    -- 方法3 airkiss配网, 可参考 app/playit/main.lua
+end
+
 -- airkiss.auto(27) -- 预留的功能,未完成 
+-- //////////////////////////////////////////////////////////////////////////////////////
+
+--- 从这里开始, 代码与具体网络无关
 
 -- 联网后自动同步时间
-sys.subscribe("WLAN_READY", function ()
-    print("!!! wlan ready event !!! send ntp")
+sys.subscribe("NET_READY", function ()
+    log.info("net", "!!! network ready event !!! send ntp")
     sys.taskInit(function()
-        sys.wait(3000)
+        sys.wait(2000)
         socket.ntpSync()
     end)
 end)
@@ -42,7 +70,7 @@ _G.use_netled = 1 -- 启用1, 关闭0
 sys.taskInit(function()
     while 1 do
         --log.info("wlan", "ready?", wlan.ready())
-        if wlan.ready() then
+        if socket.isReady() then
             --log.info("netled", "net ready, slow")
             gpio.set(21, 1 * use_netled)
             sys.wait(1900)
@@ -58,7 +86,7 @@ sys.taskInit(function()
     end
 end)
 
-sys.subscribe("net_recv", function(id, re, s)
+function net_recv(id, re, s)
     local hex = re:toHex()
     log.info("recv", id, hex, #re, re)
     uart.write(1, re)
@@ -78,64 +106,63 @@ sys.subscribe("net_recv", function(id, re, s)
             s:send(string.char(0))
         end
     end
-end)
+end
 
 
 sys.taskInit(function()
     -- 等待联网成功
-    sys.waitUntil("WLAN_READY", 30000)
-    while 1 do
-        log.info("main", "socket loop")
-        if wlan.ready() then -- TODO 感觉wlan.ready()不是很准确,还得研究一下
-            collectgarbage("collect")
-            local s = socket.tcp()
-            s:host("modbus.git4.cn") -- 改成服务器ip或者域名
-            s:port(19001) -- 改成服务器端口
-            s:on("connect", function(id, re)
-                -- 连接成功后, 发注册包 {imei:"AABBCCDDEEFF",csq:-66}
-                log.info("netc", "connect result", re)
-                if re then
-                    local jdata = json.encode({imei=wlan.get_mac(),csq=wlan.rssi()})
-                    log.info("send reg", jdata)
-                    s:send(jdata)
-                    _G.net_link = s
-                end
-            end)
-
-            s:on("recv", function(id, re)
-                if #re > 0 then
-                    sys.publish("net_recv", id, re, s)
-                end
-            end)
-            --log.info("netc", "info", s)
-            -- 启动tcp独立线程(系统级的)
-            if s:start() == 0 then
-                -- 启动成功后, 连接中断时必然有NETC_END_XX事件
-                -- 因为是长连接, 就这样等着就差不多了
-                local tcount = 0
-                while s:closed() == 0 do
-                    sys.waitUntil("NETC_END_" .. s:id(), 3000)
-                    -- 定时发个心跳什么的
-                    tcount = tcount + 1
-                    if tcount == 20 then
-                        log.info("netc", "heartbeat ping")
-                        s:send(string.char(0))
-                        tcount = 0
-                    end
-                end
-            else
-                log.info("netc", "netc start fail!!")
-            end
-            -- 一定要清理好, 不然内存泄漏, 模块内存少啊!!!
-            _G.net_link = nil
-            s:clean()
-            collectgarbage("collect")
-            --sys_memdump()
-            sys.wait(2000) -- 没连上, 或者断开了? 等2秒重试
-        else
-            log.info("wifi", "wifi NOT ready yet")
-            sys.waitUntil("WLAN_READY", 30000)
+    while true do
+        while not socket.isReady() do 
+            log.info("net", "wait for network ready")
+            sys.waitUntil("NET_READY", 3000)
         end
+        log.info("main", "socket loop")
+        
+        collectgarbage("collect")
+        local s = socket.tcp()
+        s:host("modbus.git4.cn") -- 改成服务器ip或者域名
+        s:port(19001) -- 改成服务器端口
+        s:on("connect", function(id, re)
+            -- 连接成功后, 发注册包 {imei:"AABBCCDDEEFF",csq:-66}
+            log.info("netc", "connect result", re)
+            if re then
+                local jdata = json.encode({imei=wlan.get_mac(),csq=wlan.rssi()})
+                log.info("netc", "send reg", jdata)
+                s:send(jdata)
+                _G.net_link = s
+            end
+        end)
+
+        s:on("recv", function(id, re)
+            if #re > 0 then
+                net_recv(id, re, s)
+            end
+        end)
+        --log.info("netc", "info", s)
+        -- 启动tcp独立线程(系统级的)
+        if s:start() == 0 then
+            -- 启动成功后, 连接中断时必然有NETC_END_XX事件
+            -- 因为是长连接, 就这样等着就差不多了
+            local tcount = 0
+            while s:closed() == 0 do
+                sys.waitUntil("NETC_END_" .. s:id(), 3000)
+                -- 定时发个心跳什么的
+                tcount = tcount + 1
+                if tcount == 20 then
+                    log.info("netc", "heartbeat ping")
+                    s:send(string.char(0))
+                    tcount = 0
+                end
+            end
+        else
+            log.info("netc", "netc start fail!!")
+        end
+        -- 一定要清理好, 不然内存泄漏, 模块内存少啊!!!
+        _G.net_link = nil
+        s:clean()
+        collectgarbage("collect")
+        log.info("tcp", "link is broken, sleep 2s before retry")
+        sys.wait(2000) -- 没连上, 或者断开了? 等2秒重试
     end
 end)
 

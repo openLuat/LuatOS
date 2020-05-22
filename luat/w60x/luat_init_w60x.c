3b142774b9ff806f0ad80dc93336f84cae031876
Wendal Chen <wendal1985@gmail.com>
2020-05-22 20:46:28 +0800

    fix: w600默认将wlan0设置为station

---
+++ luat/w60x/luat_init_w60x.c
@@ -0,0 +1,22 @@
+#include "luat_base.h"
+#include "luat_malloc.h"
+#include "luat_msgbus.h"
+#include "luat_timer.h"
+#include "luat_gpio.h"
+
+#include "rtthread.h"
+#include <rtdevice.h>
+#include "wlan_mgnt.h"
+
+#define DBG_TAG           "w60x.init"
+#define DBG_LVL           DBG_LOG
+#include <rtdbg.h>
+
+#ifdef BSP_USING_WM_LIBRARIES
+static int rtt_w60x_init() {
+  rt_wlan_set_mode("wlan0", RT_WLAN_STATION);
+  return RT_EOK;
+}
+INIT_COMPONENT_EXPORT(rtt_w60x_init);
+#endif
+

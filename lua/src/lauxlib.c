369ff5ff66bc89b447bc69255eafea1967f6eaa8
Wendal Chen <wendal1985@gmail.com>
2020-06-09 16:12:25 +0800

    change: lauxlib将freopen替换为fopen

--- lua/src/lauxlib.c
+++ lua/src/lauxlib.c
@@ -721,7 +721,8 @@ LUALIB_API int luaL_loadfilex (lua_State *L, const char *filename,
   if (skipcomment(&lf, &c))  /* read initial portion */
     lf.buff[lf.n++] = '\n';  /* add line to correct line numbers */
   if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
-    lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
+    fclose(lf.f);
+    lf.f = fopen(filename, "rb");  /* reopen in binary mode */
     if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
     skipcomment(&lf, &c);  /* re-read initial portion */
   }

693158921b0a6b52988fb7415db3807a38364e73
Wendal Chen <wendal1985@gmail.com>
2020-06-11 11:19:53 +0800

    add: crypto库添加sha1

--- luat/include/luat_crypto.h
+++ luat/include/luat_crypto.h
@@ -5,4 +5,4 @@ int luat_crypto_md5_simple(const char* str, size_t str_size, void* out_ptr);
 int luat_crypto_hmac_md5_simple(const char* str, size_t str_size, const char* mac, size_t mac_size, void* out_ptr);
 
 int luat_crypto_sha1_simple(const char* str, size_t str_size, void* out_ptr);
-
+int luat_crypto_hmac_sha1_simple(const char* str, size_t str_size, const char* mac, size_t mac_size, void* out_ptr);

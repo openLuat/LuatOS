47543d4fc27156fed8b3ed387cf3c3ae4dc960c3
Wendal Chen <wendal1985@gmail.com>
2020-06-19 15:09:15 +0800

    fix: luat_lib_spi有几个错误,修正之

--- luat/modules/luat_lib_spi.c
+++ luat/modules/luat_lib_spi.c
@@ -13,19 +13,19 @@
 #include "luat_spi.h"
 
 static int l_spi_setup(lua_State *L) {
-    luat_spi_t *spi_config = (luat_spi_t *)luat_heap_malloc(sizeof(luat_spi_t));
-    spi_config->id = luaL_checkinteger(L, 1);
-    spi_config->cs = luaL_checkinteger(L, 2);
-    spi_config->CPHA = luaL_checkinteger(L, 3);
-    spi_config->CPOL = luaL_checkinteger(L, 4);
-    spi_config->dataw = luaL_checkinteger(L, 5);
-    spi_config->bandrate = luaL_checkinteger(L, 6);
-    spi_config->bit_dict = luaL_optinteger(L, 7, 1);
-    spi_config->master = luaL_optinteger(L, 8, 1);
-    spi_config->mode = luaL_optinteger(L, 9, 1);
+    luat_spi_t spi_config = {0};
 
-    luat_spi_setup(spi_config);
-    luat_heap_free(spi_config);
+    spi_config.id = luaL_checkinteger(L, 1);
+    spi_config.cs = luaL_optinteger(L, 2, 255); // 默认无
+    spi_config.CPHA = luaL_optinteger(L, 3, 0); // CPHA0
+    spi_config.CPOL = luaL_optinteger(L, 4, 0); // CPOL0
+    spi_config.dataw = luaL_optinteger(L, 5, 8); // 8bit
+    spi_config.bandrate = luaL_optinteger(L, 6, 2000000U); // 2000000U
+    spi_config.bit_dict = luaL_optinteger(L, 7, 1); // MSB=1, LSB=0
+    spi_config.master = luaL_optinteger(L, 8, 1); // master=1,slave=0
+    spi_config.mode = luaL_optinteger(L, 9, 1); // FULL=1, half=0
+
+    luat_spi_setup(&spi_config);
     return 0;
 }
 static int l_spi_close(lua_State *L) {
@@ -36,30 +36,37 @@ static int l_spi_close(lua_State *L) {
 static int l_spi_transfer(lua_State *L) {
     int id = luaL_checkinteger(L, 1);
     size_t len;
-    char* recv_buff;
     const char* send_buff;
     send_buff = lua_tolstring(L, 2, &len);
-    len = luat_spi_transfer(id, send_buff, recv_buff, len);
-    if (len > 0) {
-        lua_pushlstring(L, recv_buff, len);
+    char* recv_buff = luat_heap_malloc(len);
+    int ret = luat_spi_transfer(id, (const uint8_t* )send_buff, (uint8_t* )recv_buff, len);
+    if (ret > 0) {
+        lua_pushlstring(L, recv_buff, ret);
+        luat_heap_free(recv_buff);
         return 1;
     }
+    luat_heap_free(recv_buff);
     return 0;
 }
 static int l_spi_recv(lua_State *L) {
     int id = luaL_checkinteger(L, 1);
-    char* recv_buff;
     int len = luaL_checkinteger(L, 2);
-    luat_spi_recv(id, recv_buff, len);
+    char* recv_buff = luat_heap_malloc(len);
+    int ret = luat_spi_recv(id, (uint8_t* )recv_buff, len);
+    if (ret > 0) {
+        lua_pushlstring(L, recv_buff, ret);
+        luat_heap_free(recv_buff);
+        return 1;
+    }
+    luat_heap_free(recv_buff);
     return 0;
 }
 static int l_spi_send(lua_State *L) {
     int id = luaL_checkinteger(L, 1);
     size_t len;
-    char* recv_buff;
     const char* send_buff;
     send_buff = lua_tolstring(L, 2, &len);
-    luat_spi_send(id, send_buff, len);
+    luat_spi_send(id, (const uint8_t* )send_buff, len);
     return 0;
 }
 
@@ -85,6 +92,6 @@ static const rotable_Reg reg_spi[] =
 };
 
 LUAMOD_API int luaopen_spi( lua_State *L ) {
-    rotable_newlib(L, luaopen_spi);
+    rotable_newlib(L, reg_spi);
     return 1;
 }

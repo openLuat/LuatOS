ca1e6ba6d84989c0c459160b279951d9412f551d
Wendal Chen <wendal1985@gmail.com>
2020-07-07 23:45:47 +0800

    add: 完善luat_lib_http的代码

--- luat/include/luat_http.h
+++ luat/include/luat_http.h
@@ -1,10 +1,12 @@
 
 #include "luat_base.h"
 
-#define LUAT_HTTP_GET 1
-#define LUAT_HTTP_POST 2
-#define LUAT_HTTP_PUT 3
-#define LUAT_HTTP_DELETE 4
+#define LUAT_HTTP_GET 0
+#define LUAT_HTTP_POST 1
+#define LUAT_HTTP_PUT 2
+#define LUAT_HTTP_DELETE 3
+#define LUAT_HTTP_HEAD 4
+
 
 typedef struct luat_lib_http_body
 {
@@ -14,29 +16,37 @@ typedef struct luat_lib_http_body
     void *next;
 }luat_lib_http_body_t;
 
-typedef struct luat_lib_http_header
+typedef struct luat_lib_http_headers
 {
-    char* str;
-    void *next;
-}luat_lib_http_header_t;
-
-typedef struct luat_lib_http_req
-{
-    uint8_t method;
-    char* url;
-    size_t url_len;
-    char* ca;
-    size_t ca_len;
-    luat_lib_http_header_t headers;
-    luat_lib_http_body_t body;
-    int cb;
-}luat_lib_http_req_t;
+    char** ptr;
+    size_t size;
+}luat_lib_http_headers_t;
 
 typedef struct luat_lib_http_resp
 {
     int code;
-    luat_lib_http_header_t headers;
+    luat_lib_http_headers_t headers;
     luat_lib_http_body_t body;
+    int luacb;
 }luat_lib_http_resp_t;
 
+typedef int (* luat_http_cb) (luat_lib_http_resp_t *resp);
+
+typedef struct luat_lib_http_req
+{
+    uint8_t method;
+    uint8_t timeout_s;
+    char* url;
+    size_t url_len;
+    char* ca;
+    size_t ca_len;
+    luat_lib_http_headers_t headers;
+    luat_lib_http_body_t body;
+    int luacb;
+    luat_http_cb httpcb;
+}luat_lib_http_req_t;
+
 int luat_http_req(luat_lib_http_req_t *req);
+
+void luat_http_req_gc(luat_lib_http_req_t *req);
+void luat_http_resp_gc(luat_lib_http_resp_t *req);

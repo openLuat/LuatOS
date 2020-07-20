b4f728b050ba6c63d691a9da5352706201325393
Wendal Chen <wendal1985@gmail.com>
2020-07-20 12:10:44 +0800

    Revert "fix: string.format在air302下有问题"
    This reverts commit 9fcc4c046fff8e35117dabde1a696dd849b3ca93.

--- luat/packages/vsprintf/vsprintf.h
+++ luat/packages/vsprintf/vsprintf.h
@@ -1,13 +1,12 @@
 #ifndef CUSTOM_VSPRINTF
 #define CUSTOM_VSPRINTF
 
-#include <stdarg.h>
 #include "luat_base.h"
+#include <stdarg.h>
 
 int atob(uint32_t *vp, char *p, int base);
 char * btoa(char *dst, uint32_t value, int base);
 int custom_vsprintf (char *d, const char *s, va_list ap);
-char* llbtoa(char *dst, long long value, int base);
 
 
 #endif

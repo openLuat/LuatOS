6406e1c8baef7f184a1f39af409d3b4297fe7601
Wendal Chen <wendal1985@gmail.com>
2020-09-01 22:35:13 +0800

    update: 禁止printf.h覆盖编译环境自带的printf/sprintf/vprintf等函数

--- luat/packages/vsprintf/printf.h
+++ luat/packages/vsprintf/printf.h
@@ -57,7 +57,7 @@ void _putchar(char character);
  * \param format A string that specifies the format of the output
  * \return The number of characters that are written into the array, not counting the terminating null character
  */
-#define printf printf_
+//#define printf printf_
 int printf_(const char* format, ...);
 
 
@@ -68,7 +68,7 @@ int printf_(const char* format, ...);
  * \param format A string that specifies the format of the output
  * \return The number of characters that are WRITTEN into the buffer, not counting the terminating null character
  */
-#define sprintf sprintf_
+//#define sprintf sprintf_
 int sprintf_(char* buffer, const char* format, ...);
 
 
@@ -82,8 +82,8 @@ int sprintf_(char* buffer, const char* format, ...);
  *         null character. A value equal or larger than count indicates truncation. Only when the returned value
  *         is non-negative and less than count, the string has been completely written.
  */
-#define snprintf  snprintf_
-#define vsnprintf vsnprintf_
+//#define snprintf  snprintf_
+//#define vsnprintf vsnprintf_
 int  snprintf_(char* buffer, size_t count, const char* format, ...);
 int vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
 
@@ -94,7 +94,7 @@ int vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
  * \param va A value identifying a variable arguments list
  * \return The number of characters that are WRITTEN into the buffer, not counting the terminating null character
  */
-#define vprintf vprintf_
+//#define vprintf vprintf_
 int vprintf_(const char* format, va_list va);
 
 

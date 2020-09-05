dd854305508fce1a3429d70eb2e169ce762fb518
Wendal Chen <wendal1985@gmail.com>
2020-09-05 22:46:40 +0800

    add: 添加air001的概念实现(非常初步)

--- bsp/air001/src/app.c
+++ bsp/air001/src/app.c
@@ -1,6 +1,18 @@
 
 #include "stdio.h"
 
-int main() {
+#include "luat_vdev.h"
+
+int main(int argc, const char* argv[]) {
+
+    // TODO 读取命令行参数
+
+    // TODO 读取环境变量
+
+    // 初始化虚拟设备
+    luat_vdev_init();
+
+    luat_main(0, (char**)NULL, 0);
+
     return 0;
 }

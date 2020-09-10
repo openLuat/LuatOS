79e234dfe7e30381d05bb6cf31d3828f5214cfed
Wendal Chen <wendal1985@gmail.com>
2020-09-10 17:22:05 +0800

    update: 让air100st能编译过去

--- bsp/air100st/rtt/board/ports/fal_cfg.h
+++ bsp/air100st/rtt/board/ports/fal_cfg.h
@@ -14,19 +14,25 @@
 #include <rtthread.h>
 #include <board.h>
 
-//extern const struct fal_flash_dev stm32f4_onchip_flash;
+#define FLASH_SIZE_GRANULARITY_16K   (4 * 16 * 1024)
+#define FLASH_SIZE_GRANULARITY_64K   (64 * 1024)
+#define FLASH_SIZE_GRANULARITY_128K  (7 * 128 * 1024)
 
-/* ===================== Flash device Configuration ========================= */
-extern struct fal_flash_dev nor_flash0;
-extern struct fal_flash_dev stm32f4_onchip_flash;
+#define STM32_FLASH_START_ADRESS_16K  STM32_FLASH_START_ADRESS
+#define STM32_FLASH_START_ADRESS_64K  (STM32_FLASH_START_ADRESS_16K + FLASH_SIZE_GRANULARITY_16K)
+#define STM32_FLASH_START_ADRESS_128K (STM32_FLASH_START_ADRESS_64K + FLASH_SIZE_GRANULARITY_64K)
+
+extern const struct fal_flash_dev stm32_onchip_flash_16k;
+extern const struct fal_flash_dev stm32_onchip_flash_64k;
+extern const struct fal_flash_dev stm32_onchip_flash_128k;
 
 /* flash device table */
 #define FAL_FLASH_DEV_TABLE                                          \
 {                                                                    \
-    &stm32f4_onchip_flash,                                           \
-    &nor_flash0,                                                     \
+    &stm32_onchip_flash_16k,                                         \
+    &stm32_onchip_flash_64k,                                         \
+    &stm32_onchip_flash_128k,                                        \
 }
-
 /* ====================== Partition Configuration ========================== */
 #ifdef FAL_PART_HAS_TABLE_CFG
 

61af9d92ae3f3e12482ab75a04feaccf82edeed3
Wendal Chen <wendal1985@gmail.com>
2020-09-11 22:05:27 +0800

    update: 补齐air724_sdk所需要的2个文件,这次能编译通过了

---
+++ bsp/air724/sdk/app/luatos/luatos_main.c
@@ -0,0 +1,14 @@
+
+#include "iot_os.h"
+#include "iot_debug.h"
+
+int appimg_enter(void *param)
+{    
+	iot_debug_print("[os] appimg_enter");
+    return 0;
+}
+
+void appimg_exit(void)
+{
+    iot_debug_print("[os] appimg_exit");
+}

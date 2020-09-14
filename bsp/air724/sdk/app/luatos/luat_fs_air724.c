60da5cee45bf4366be54da1cf6c2d5ec752a2b42
Wendal Chen <wendal1985@gmail.com>
2020-09-14 15:27:44 +0800

    add: 补齐air724编译所缺失的函数

---
+++ bsp/air724/sdk/app/luatos/luat_fs_air724.c
@@ -0,0 +1,7 @@
+
+#include "luat_fs.h"
+#include "luat_malloc.h"
+
+int luat_fs_init(void) {
+    return 0;
+}
---
+++ bsp/air724/sdk/app/luatos/luat_timer_air724.c
@@ -0,0 +1,9 @@
+
+#include "luat_base.h"
+#include "luat_timer.h"
+
+
+
+int luat_timer_mdelay(size_t ms) {
+    return 0; // TODO
+}

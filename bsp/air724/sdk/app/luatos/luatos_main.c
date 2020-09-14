d04ccb7c9bfb36bb2283f96e34b6c91a351ffb38
Wendal Chen <wendal1985@gmail.com>
2020-09-14 10:57:07 +0800

    update: 继续更新air724的CMakefile文件

--- bsp/air724/sdk/app/luatos/luatos_main.c
+++ bsp/air724/sdk/app/luatos/luatos_main.c
@@ -3,10 +3,16 @@
 #include "iot_debug.h"
 
 #include "luat_base.h"
+#include "bget.h"
+
+#define HEAP_SIZE (512*1024)
+static char luatos_heap[HEAP_SIZE];
 
 int appimg_enter(void *param)
 {    
 	iot_debug_print("[os] appimg_enter");
+    bpool(luatos_heap, HEAP_SIZE);
+    luat_main();
     return 0;
 }
 

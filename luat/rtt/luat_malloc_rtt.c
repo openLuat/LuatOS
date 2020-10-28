1e907d299c3235174fe938a8d567eded09517589
Wendal Chen <wendal1985@gmail.com>
2020-10-28 17:29:06 +0800

    update: air640w的mcu模式固件,lua内存扩展到176kb

--- luat/rtt/luat_malloc_rtt.c
+++ luat/rtt/luat_malloc_rtt.c
@@ -5,16 +5,15 @@
 
 #include "bget.h"
 
-#define ALI8 __attribute__ ((aligned (8)))
-
 #ifdef BSP_USING_WM_LIBRARIES
+    #include "wm_ram_config.h"
     #define LUAT_HEAP_SIZE 64*1024
     #define W600_HEAP_ADDR 0x20028000
     #ifdef RT_USING_WIFI
 
     #else
     #define W600_MUC_HEAP_SIZE (64*1024)
-    ALI8 static char w600_mcu_heap[W600_MUC_HEAP_SIZE]; // MCU模式下, rtt起码剩余140kb内存, 用64kb不过分吧
+    ALIGN(RT_ALIGN_SIZE) static char w600_mcu_heap[W600_MUC_HEAP_SIZE]; // MCU模式下, rtt起码剩余140kb内存, 用64kb不过分吧
 
     #endif
 #else
@@ -25,7 +24,7 @@
             #define LUAT_HEAP_SIZE 128*1024
         #endif
     #endif
-    ALI8 static char luavm_buff[LUAT_HEAP_SIZE] = {0};
+    ALIGN(RT_ALIGN_SIZE) static char luavm_buff[LUAT_HEAP_SIZE] = {0};
 #endif
 
 static int rtt_mem_init() {
@@ -33,8 +32,10 @@ static int rtt_mem_init() {
     #ifdef RT_USING_WIFI
         // nothing
     #else
-        // MUC heap 
-        bpool(w600_mcu_heap, W600_MUC_HEAP_SIZE);
+        // MUC heap , 占用一部分rtt heap
+        bpool((void*)w600_mcu_heap, W600_MUC_HEAP_SIZE);
+        // 把wifi的内存全部吃掉
+        bpool((void*)WIFI_MEM_START_ADDR, (64 - 16)*1024);
     #endif
     void *ptr = W600_HEAP_ADDR;
     #else

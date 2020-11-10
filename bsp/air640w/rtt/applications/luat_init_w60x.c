251185097385d6f11bac917c0ec09a759856f9b6
Wendal Chen <wendal1985@gmail.com>
2020-11-10 23:32:46 +0800

    fix: air640w执行loadstr的逻辑应该通过msgbus传递

--- bsp/air640w/rtt/applications/luat_init_w60x.c
+++ bsp/air640w/rtt/applications/luat_init_w60x.c
@@ -9,16 +9,23 @@
 
 #ifdef RT_USING_WIFI
 #include "wlan_mgnt.h"
+#endif
 
 #define DBG_TAG           "w60x.init"
 #define DBG_LVL           DBG_LOG
 #include <rtdbg.h>
 
 #ifdef BSP_USING_WM_LIBRARIES
+static rt_err_t w600_bt(void *context) {
+  rt_kprintf("\r\nFUCK!!\r\n");
+  return 0;
+} 
 static int rtt_w60x_init() {
+  #ifdef RT_USING_WIFI
   rt_wlan_set_mode("wlan0", RT_WLAN_STATION);
+  #endif
+  rt_hw_exception_install(w600_bt);
   return RT_EOK;
 }
 INIT_COMPONENT_EXPORT(rtt_w60x_init);
-#endif
 #endif

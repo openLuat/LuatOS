ff72fc0a87c73eee42a39d0b97d2e67779d5cf28
alienwalker <alienwalker@sina.com>
2020-11-12 19:19:05 +0800

    update:gpio中断测试脚本优化

--- bsp/air302/demo/gpio/main.lua
+++ bsp/air302/demo/gpio/main.lua
@@ -7,19 +7,25 @@ VERSION = "1.0.0"
 _G.sys = require("sys")
 
 local NETLED = gpio.setup(19, 0, gpio.PULLUP) -- 输出模式
-local G18 = gpio.setup(18, nil, gpio.PULLUP) -- 输入模式
+local G18 = gpio.setup(18, 0, gpio.PULLUP) -- 输出模式
+local G7 = gpio.setup(7, function() -- 中断模式, 下降沿，需要将GPIO18和GPIO7连在一起
+    log.info("gpio7", "BOOT button release")
+end, gpio.PULLUP,gpio.FALLING)
 local G1 = gpio.setup(1, function() -- 中断模式, 下降沿
-    log.info("gpio", "BOOT button release")
-end, gpio.PULLUP)
-
+    log.info("gpio1", "BOOT button release")
+end, gpio.PULLUP,gpio.FALLING)
 sys.taskInit(function()
+
     while 1 do
         -- 一闪一闪亮晶晶
         NETLED(0)
+        G18(0)
+        log.info("gpio", "7", G7())
         sys.wait(500)
         NETLED(1)
+        G18(1)
         sys.wait(500)
-        log.info("gpio", "18", G18())
+        log.info("gpio", "7", G7())
     end
 end)
 

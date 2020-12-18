b9c78751cd399f676e39f703885214d0b3b5b74e
Wendal Chen <wendal1985@gmail.com>
2020-12-19 00:29:28 +0800

    update: 使得eink/epaper兼容air640w的scons编译

--- luat/packages/eink/epd2in9.h
+++ luat/packages/eink/epd2in9.h
@@ -58,8 +58,8 @@
 #define SET_RAM_Y_ADDRESS_COUNTER                   0x4F
 #define TERMINATE_FRAME_READ_WRITE                  0xFF
 
-extern const unsigned char lut_full_update[];
-extern const unsigned char lut_partial_update[];
+static const unsigned char lut_full_update[];
+static const unsigned char lut_partial_update[];
 
 typedef struct EPD_2IN9F_t {
   int reset_pin;

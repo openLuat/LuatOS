7829d103ad46f7a445fec88fcc3a8f69564d5a18
Wendal Chen <wendal1985@gmail.com>
2021-01-31 21:48:33 +0800

    add: 添加单步调试的配置提示

--- bsp/air640w/demo/199.dbg/main.lua
+++ bsp/air640w/demo/199.dbg/main.lua
@@ -1,4 +1,27 @@
 
+
+--[[
+本demo用于演示调试功能
+
+提示: 需要最新代码, 编译最新固件, 需要Luatools 2.1.3以上, vscode找LuatOS debug插件
+
+
+local.ini需要配置:
+MAIN_LUA_DEBUG = 1
+LUA_DEBUG = 1
+
+否则因为没有调试信息, 导致无法触发断点
+
+luatools 配置 config/global.ini
+确保有如下配置
+
+[luatdbg]
+enable = 1
+
+
+
+
+]]
 dbg.wait()
 
 local sys = require("sys")

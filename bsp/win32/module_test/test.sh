5825b522dd43ec5d029d7ece238bb0761cf64a03
chenxuuu <lolicon@papapoi.com>
2021-04-09 14:04:36 +0800

    fix: zbuff，unpack漏了一个占位的

--- bsp/win32/module_test/test.sh
+++ bsp/win32/module_test/test.sh
@@ -1,4 +1,4 @@
-echo -e "\e[1;33m=====test start=====\e[0m"
+echo -e "\e[1;33m===== test start =====\e[0m"
 
 result=0
 
@@ -8,12 +8,18 @@ do
     script_output=$(./luatos.exe $file)
     if [ $? -eq 0 ]
     then
-        echo -e "\e[1;32mpass:\e[0m $file"
+        echo -e "[\e[1;32mpass\e[0m] $file"
     else
-        echo -e "\e[1;31mfail:\e[0m $file"
+        echo -e "[\e[1;31mfail\e[0m] $file"
         echo "$script_output"
         result=1
     fi
 done
-echo -e "\e[1;33m====all done, exit with $result====\e[0m"
+if [ $result -eq 0 ]
+then
+    echo -e "\e[1;33m==== all tests passed ====\e[0m"
+else
+    echo -e "\e[1;31m==== not pass ====\e[0m"
+fi
+
 exit $result

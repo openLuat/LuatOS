f998a099bd4eb987fdfd0da39a4139a79db82ed8
Wendal Chen <wendal1985@gmail.com>
2021-04-12 19:23:25 +0800

    move: 移动freertos和mbedtls到顶层的components目录

---
+++ bsp/win32/main.lua
@@ -0,0 +1,16 @@
+
+
+local sys = require "sys"
+
+log.info("sys", "from win32")
+
+sys.taskInit(function ()
+    while true do
+        log.info("hi", os.date())
+        log.info("sys", rtos.meminfo("sys"))
+        log.info("lua", rtos.meminfo("lua"))
+        sys.wait(1000)
+    end
+end)
+
+sys.run()

298cbf8f0c450f53bfcad81a53776ba04d15b874
Wendal Chen <wendal1985@gmail.com>
2021-06-03 23:07:01 +0800

    update: lvgl.init支持用zbuff作为缓冲区

--- components/lvgl/binding/luat_lib_lvgl7.c
+++ components/lvgl/binding/luat_lib_lvgl7.c
@@ -2,11 +2,10 @@
 #include "luat_base.h"
 #include "luat_lvgl.h"
 #include "luat_malloc.h"
-
+#include "luat_zbuff.h"
 
 static lv_disp_t* my_disp = NULL;
 static lv_disp_buf_t disp_buff = {0};
-static lv_color_t first_buff[LV_HOR_RES_MAX * 10] = {0};
 
 static void disp_flush(struct _disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
     //-----
@@ -29,24 +28,30 @@ int luat_lv_init(lua_State *L) {
         lua_pushboolean(L, 0);
         return 1;
     }
-    //LLOGD(">>%s %d", __func__, __LINE__);
-    lv_disp_buf_init(&disp_buff, first_buff, NULL, LV_HOR_RES_MAX * 10);
+    lv_color_t *fbuffer = NULL; 
+    size_t fbuff_size = LV_HOR_RES_MAX * 10;
+    if (lua_isuserdata(L, 1)) {
+        luat_zbuff *zbuff = tozbuff(L);
+        fbuffer = (lv_color_t *)zbuff->addr;
+        fbuff_size = zbuff->len / sizeof(lv_color_t);
+    }
+    else {
+        fbuffer = luat_heap_malloc(fbuff_size * sizeof(lv_color_t));
+    }
+    lv_disp_buf_init(&disp_buff, fbuffer, NULL, fbuff_size);
 
-    //LLOGD(">>%s %d", __func__, __LINE__);
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
 
-    // LLOGD(">>%s %d", __func__, __LINE__);
     disp_drv.flush_cb = disp_flush;
 
     disp_drv.hor_res = 480;
     disp_drv.ver_res = 320;
     disp_drv.buffer = &disp_buff;
-    LLOGD(">>%s %d", __func__, __LINE__);
+    //LLOGD(">>%s %d", __func__, __LINE__);
     my_disp = lv_disp_drv_register(&disp_drv);
     //LLOGD(">>%s %d", __func__, __LINE__);
-    //lua_pushboolean(L, my_disp != NULL ? 1 : 0);
+    lua_pushboolean(L, my_disp != NULL ? 1 : 0);
     //LLOGD(">>%s %d", __func__, __LINE__);
-    //return 1;
-    return 0;
+    return 1;
 }

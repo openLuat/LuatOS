9b038811e72a854139adb8cd54929a1e35321857
Dozingfiretruck <1473454180@qq.com>
2021-06-17 17:37:27 +0800

    add:添加st7735驱动

--- luat/lcd/luat_lcd.c
+++ luat/lcd/luat_lcd.c
@@ -1,6 +1,7 @@
-
 #include "luat_base.h"
 #include "luat_lcd.h"
+#include "luat_gpio.h"
+#include "luat_spi.h"
 
 #define LUAT_LCD_CONF_COUNT (2)
 static luat_lcd_conf_t* confs[LUAT_LCD_CONF_COUNT] = {0};
@@ -63,4 +64,71 @@ int luat_lcd_display_on(luat_lcd_conf_t* conf) {
 
 int luat_lcd_display_off(luat_lcd_conf_t* conf){
     return conf->opts->display_off(conf);
+}
+
+
+int lcd_write_cmd(const uint8_t cmd, luat_lcd_conf_t* conf)
+{
+    size_t len;
+    luat_gpio_set(conf->pin_dc, Luat_GPIO_LOW);
+    len = luat_spi_send(conf->port, &cmd, 1);
+    if (len != 1)
+    {
+        LLOGI("lcd_write_cmd error. %d", len);
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int lcd_write_data(const uint8_t data, luat_lcd_conf_t* conf)
+{
+    size_t len;
+    luat_gpio_set(conf->pin_dc, Luat_GPIO_HIGH);
+    len = luat_spi_send(conf->port, &data, 1);
+    if (len != 1)
+    {
+        LLOGI("lcd_write_data error. %d", len);
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int lcd_write_half_word(const uint16_t da, luat_lcd_conf_t* conf)
+{
+    size_t len = 0;
+    char data[2] = {0};
+    data[0] = da >> 8;
+    data[1] = da;
+    luat_gpio_set(conf->pin_dc, Luat_GPIO_HIGH);
+    len = luat_spi_send(conf->port, data, 2);
+    if (len != 2)
+    {
+        LLOGI("lcd_write_half_word error. %d", len);
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void lcd_address_set(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, luat_lcd_conf_t* conf)
+{
+    lcd_write_cmd(0x2a, conf);
+    lcd_write_data(x1 >> 8, conf);
+    lcd_write_data(x1, conf);
+    lcd_write_data(x2 >> 8, conf);
+    lcd_write_data(x2, conf);
+    lcd_write_cmd(0x2b, conf);
+    lcd_write_data(y1 >> 8, conf);
+    lcd_write_data(y1, conf);
+    lcd_write_data(y2 >> 8, conf);
+    lcd_write_data(y2, conf);
+    lcd_write_cmd(0x2C, conf);
 }
---
+++ luat/lcd/luat_lcd_st7735.c
@@ -0,0 +1,231 @@
+#include "luat_lcd.h"
+#include "luat_gpio.h"
+#include "luat_spi.h"
+#include "luat_malloc.h"
+#include "luat_timer.h"
+
+#define LUAT_LOG_TAG "lcd"
+#include "luat_log.h"
+
+#define LCD_W 128
+#define LCD_H 160
+#define LCD_CLEAR_SEND_NUMBER LCD_W*LCD_H/10//5760
+
+
+static int st7735_init(luat_lcd_conf_t* conf) {
+    // 配置SPI
+    luat_spi_t lcd_spi = {0};
+    lcd_spi.id = 0;
+    lcd_spi.CPHA = 0;
+    lcd_spi.CPOL = 0;
+    lcd_spi.dataw = 8;
+    lcd_spi.bit_dict = 1;
+    lcd_spi.master = 1;
+    lcd_spi.mode = 1;
+    lcd_spi.bandrate = 40 * 1000 * 1000;
+    lcd_spi.cs = conf->pin_cs;
+    LLOGI("cs_pin=%d",lcd_spi.cs);
+    luat_spi_setup(&lcd_spi);
+
+    if (conf->w == 0)
+        conf->w = LCD_W;
+    if (conf->h == 0)
+        conf->h = LCD_H;
+
+    // 配置CS脚的GPIO
+    luat_gpio_mode(conf->pin_cs, Luat_GPIO_OUTPUT, Luat_GPIO_PULLUP, 0);
+    luat_gpio_mode(conf->pin_dc, Luat_GPIO_OUTPUT, Luat_GPIO_PULLUP, 0); // DC
+    luat_gpio_mode(conf->pin_pwr, Luat_GPIO_OUTPUT, Luat_GPIO_PULLUP, 0); // POWER
+    luat_gpio_mode(conf->pin_rst, Luat_GPIO_OUTPUT, Luat_GPIO_PULLUP, 0); // RST
+
+    int pin_dc = conf->pin_dc;
+    int pin_pwr = conf->pin_pwr;
+
+    // 发送初始化命令
+    //------------------------------------ST7735S Frame Rate-----------------------------------------//
+    lcd_write_cmd(0xB1, conf);
+    lcd_write_data(0x05, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_cmd(0xB2, conf);
+    lcd_write_data(0x05, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_cmd(0xB3, conf);
+    lcd_write_data(0x05, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_data(0x05, conf);
+    lcd_write_data(0x3C, conf);
+    lcd_write_data(0x3C, conf);
+    //------------------------------------End ST7735S Frame Rate---------------------------------//
+    lcd_write_cmd(0xB4, conf);
+    lcd_write_data(0x03, conf);
+	//------------------------------------ST7735S Power Sequence---------------------------------//
+    lcd_write_cmd(0xC0, conf);
+    lcd_write_data(0x28, conf);
+    lcd_write_data(0x08, conf);
+    lcd_write_data(0x04, conf);
+    lcd_write_cmd(0xC1, conf);
+    lcd_write_data(0XC0, conf);
+    lcd_write_cmd(0xC2, conf);
+    lcd_write_data(0x0D, conf);
+    lcd_write_data(0x00, conf);
+    cd_write_cmd(0xC3, conf);
+    lcd_write_data(0x8D, conf);
+    lcd_write_data(0x2A, conf);
+    cd_write_cmd(0xC4, conf);
+    lcd_write_data(0x8D, conf);
+    lcd_write_data(0xEE, conf);
+	//---------------------------------End ST7735S Power Sequence-------------------------------------//
+    lcd_write_cmd(0xC5, conf);
+    lcd_write_data(0x1A, conf);
+    lcd_write_cmd(0x36, conf);
+    lcd_write_data(0x00, conf);
+	//------------------------------------ST7735S Gamma Sequence---------------------------------//
+	lcd_write_cmd(0xE0, conf);
+    lcd_write_data(0x04, conf);
+    lcd_write_data(0x22, conf);
+    lcd_write_data(0x07, conf);
+    lcd_write_data(0x0A, conf);
+    lcd_write_data(0x2E, conf);
+    lcd_write_data(0x30, conf);
+    lcd_write_data(0x25, conf);
+    lcd_write_data(0x2A, conf);
+    lcd_write_data(0x28, conf);
+    lcd_write_data(0x26, conf);
+    lcd_write_data(0x2E, conf);
+    lcd_write_data(0x3A, conf);
+    lcd_write_data(0x00, conf);
+    lcd_write_data(0x01, conf);
+    lcd_write_data(0x03, conf);
+    lcd_write_data(0x13, conf);
+    lcd_write_cmd(0xE1, conf);
+    lcd_write_data(0x04, conf);
+    lcd_write_data(0x16, conf);
+    lcd_write_data(0x06, conf);
+    lcd_write_data(0x0D, conf);
+    lcd_write_data(0x2D, conf);
+    lcd_write_data(0x26, conf);
+    lcd_write_data(0x23, conf);
+    lcd_write_data(0x27, conf);
+    lcd_write_data(0x27, conf);
+    lcd_write_data(0x25, conf);
+    lcd_write_data(0x2D, conf);
+    lcd_write_data(0x3B, conf);
+    lcd_write_data(0x00, conf);
+    lcd_write_data(0x01, conf);
+    lcd_write_data(0x04, conf);
+    lcd_write_data(0x13, conf);
+
+    lcd_write_cmd(0x3A, conf);
+    lcd_write_data(0x05, conf);
+    lcd_write_cmd(0x29, conf);
+    return 0;
+};
+
+static int st7735_close(luat_lcd_conf_t* conf) {
+    luat_gpio_set(conf->pin_pwr, Luat_GPIO_LOW);
+    return 0;
+}
+
+// 暂时用不上
+static int st7735_drawPoint(luat_lcd_conf_t* conf, uint16_t x, uint16_t y, uint32_t color) {
+    return 0;
+}
+
+// 暂时用不上
+static int st7735_fill(luat_lcd_conf_t* conf, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint32_t color) {
+    return 0;
+}
+
+// TODO 这里的color是ARGB, 需要转为lcd所需要的格式
+static int st7735_draw(luat_lcd_conf_t* conf, uint16_t x_start, uint16_t y_start, uint16_t x_end, uint16_t y_end, uint32_t* color) {
+    uint16_t i = 0, j = 0;
+    uint32_t size = 0, size_remain = 0;
+    uint8_t *fill_buf = NULL;
+    size = (x_end - x_start+1) * (y_end - y_start+1) * 2;
+    if (size > LCD_CLEAR_SEND_NUMBER)
+    {
+        /* the number of remaining to be filled */
+        size_remain = size - LCD_CLEAR_SEND_NUMBER;
+        size = LCD_CLEAR_SEND_NUMBER;
+    }
+    lcd_address_set(x_start, y_start, x_end, y_end, conf);
+    fill_buf = (uint8_t *)luat_heap_malloc(size);
+    if (fill_buf)
+    {
+        /* fast fill */
+        while (1)
+        {
+            for (i = 0; i < size / 2; i++)
+            {
+                fill_buf[2 * i] = color[i]>>8;
+                //color >> 8;
+                fill_buf[2 * i + 1] = color[i];
+            }
+            luat_gpio_set(conf->pin_dc, Luat_GPIO_HIGH);
+            luat_spi_send(conf->port, fill_buf, size);
+            /* Fill completed */
+            if (size_remain == 0)
+                break;
+            /* calculate the number of fill next time */
+            if (size_remain > LCD_CLEAR_SEND_NUMBER)
+            {
+                size_remain = size_remain - LCD_CLEAR_SEND_NUMBER;
+            }
+            else
+            {
+                size = size_remain;
+                size_remain = 0;
+            }
+        }
+        luat_heap_free(fill_buf);
+    }
+    else
+    {
+        for (i = y_start; i <= y_end; i++)
+        {
+            for (j = x_start; j <= x_end; j++)lcd_write_half_word(color[i], conf);
+        }
+    }
+    return 0;
+}
+
+static int st7735_sleep(luat_lcd_conf_t* conf) {
+    luat_gpio_set(conf->pin_pwr, Luat_GPIO_LOW);
+    luat_timer_mdelay(5);
+    lcd_write_cmd(0x10, conf);
+    return 0;
+}
+
+static int st7735_wakeup(luat_lcd_conf_t* conf) {
+    luat_gpio_set(conf->pin_pwr, Luat_GPIO_HIGH);
+    luat_timer_mdelay(5);
+    lcd_write_cmd(0x11, conf);
+    //luat_timer_mdelay(120); // 外部休眠就好了吧
+    return 0;
+}
+
+static int st7735_display_on(luat_lcd_conf_t* conf) {
+    luat_gpio_set(conf->pin_pwr, Luat_GPIO_HIGH);
+    lcd_write_cmd(0x29, conf);
+}
+
+static int st7735_display_off(luat_lcd_conf_t* conf) {
+    luat_gpio_set(conf->pin_pwr, Luat_GPIO_LOW);
+}
+
+const luat_lcd_opts_t lcd_opts_st7735 = {
+    .name = "st7735",
+    .init = st7735_init,
+    .close = st7735_close,
+    .drawPoint = st7735_drawPoint,
+    .fill = st7735_fill,
+    .draw = st7735_draw,
+    .sleep = st7735_sleep,
+    .wakeup = st7735_wakeup,
+    .display_on = st7735_display_on,
+    .display_off = st7735_display_off,
+};
+

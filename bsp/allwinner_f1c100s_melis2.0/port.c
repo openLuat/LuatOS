079c2399744600404c0bdaa9045717e8b4f48ffc
alienwalker <alienwalker@sina.com>
2021-06-23 10:17:32 +0800

    add:增加触摸屏，键盘和内核消息接收转换，还需要大量的修改

--- bsp/allwinner_f1c100s_melis2.0/port.c
+++ bsp/allwinner_f1c100s_melis2.0/port.c
@@ -2,8 +2,44 @@
 #include "stdint.h"
 #include "lcd_cfg.h"
 #define FB_CNT  2
-
+#define TP_EVENT_CNT    (128)
+#define KEY_EVENT_CNT   (128)
+#define SRV_EVENT_CNT   (128)
 void luatos_main_entry(void);
+
+typedef struct
+{
+	uint32_t id;
+	uint32_t param1;
+	uint32_t param2;
+	uint32_t param3;
+    uint32_t lock;
+}__event_t;
+
+typedef struct
+{
+    __krnl_event_t  *msg_queue;
+    __event_t *event_buf;
+    uint32_t event_pos;
+}__event_queue_t;
+
+typedef struct
+{
+    __krnl_event_t  *psys_msg_queue;				// msg 消息队列
+    __event_t tp_event_buf[TP_EVENT_CNT];
+    __event_t key_event_buf[KEY_EVENT_CNT];
+    __event_t srv_event_buf[SRV_EVENT_CNT];
+    uint32_t tp_event_pos;
+    uint32_t key_event_pos;
+    uint32_t srv_event_pos;
+	__hdle  		h_tpGraber;
+	__hdle  		h_keyGraber;
+    __u8 			ksrv_th_id;
+}kernel_ctrlstruct;
+
+
+
+
 
 typedef struct
 {
@@ -28,6 +64,579 @@ typedef union
 
 
 static display_ctrlstruct g_display;
+static kernel_ctrlstruct prv_kernel;
+
+
+/**
+ * 通过回调函数的方式取触摸屏消息
+ */
+static __s32 tp_msg_cb(void *msg)
+{
+	__event_t	*pmsg;
+	__u8 error;
+
+	__u32 i;
+	__input_event_packet_t *pPacket;
+	__input_event_t        *pEventFrame;
+	__input_event_t        pEvent_type;
+	__input_event_t        pEvent_x;
+	__input_event_t        pEvent_y;
+	__input_event_t        pEvent_speed_dir;
+	__input_event_t        pEvent_speed_val;
+
+	if (msg == NULL)
+	{
+		__msg("invalid argument for call back\n");
+		return EPDK_FAIL;
+	}
+
+	/* dump packet events */
+	pPacket = (__input_event_packet_t *)msg;
+	if (pPacket->event_cnt <= 0 || pPacket->event_cnt > INPUT_MAX_EVENT_NR)
+	{
+		__msg("invalid event count number\n");
+		return EPDK_FAIL;
+	}
+	//__inf("\n------------------------------------------------\n");
+	for (i = 0; i < pPacket->event_cnt; i++)
+	{
+		pEventFrame = &(pPacket->events[i]);
+		/*__inf("Event %d: type = %d, code = %d, value = %d\n", i + 1,
+																 pEventFrame->type,
+																 pEventFrame->code,
+																 pEventFrame->value
+																 );*/
+		if(pEventFrame->type == EV_ABS)
+		{
+			if(pEventFrame->code == ABS_MISC)
+			{
+				eLIBs_memcpy(&pEvent_type, pEventFrame, sizeof(__input_event_t));
+			}
+			else if(pEventFrame->code == ABS_X)
+			{
+				eLIBs_memcpy(&pEvent_x, pEventFrame, sizeof(__input_event_t));
+			}
+			else if(pEventFrame->code == ABS_Y)
+			{
+				eLIBs_memcpy(&pEvent_y, pEventFrame, sizeof(__input_event_t));
+			}
+			else if(pEventFrame->code == ABS_RUDDER)
+			{
+				eLIBs_memcpy(&pEvent_speed_dir, pEventFrame, sizeof(__input_event_t));
+			}
+			else if(pEventFrame->code == ABS_BRAKE)
+			{
+				eLIBs_memcpy(&pEvent_speed_val, pEventFrame, sizeof(__input_event_t));
+			}
+		}
+		else if(pEventFrame->type == EV_SYN)
+		{
+			break;
+		}
+	}
+	//__inf("\n------------------------------------------------\n");
+
+	esKRNL_SemPend(emit_ctr.p_array_sem, 0, &error);
+	pmsg = get_msg_buf(&emit_ctr);
+	esKRNL_SemPost(emit_ctr.p_array_sem);
+
+	pmsg->id = GUI_MSG_TOUCH;
+	switch( pEvent_type.value)
+	{
+		case EV_TP_PRESS_START:
+		{
+			pmsg->id 	= GUI_MSG_TOUCH_DOWN;
+			pmsg->data 	= (pEvent_y.value<<16) + pEvent_x.value;
+			pmsg->reserved = (pEvent_speed_dir.value<<16) + pEvent_speed_val.value;
+			last_touch_action = GUI_MSG_TOUCH_DOWN;
+			break;
+		}
+		case EV_TP_PRESS_MOVE:
+			pmsg->id 	= GUI_MSG_TOUCH_MOVE;
+			pmsg->data 	= (pEvent_y.value<<16) + pEvent_x.value;
+			pmsg->reserved = (pEvent_speed_dir.value<<16) + pEvent_speed_val.value;
+			last_touch_action = GUI_MSG_TOUCH_MOVE;
+			break;
+		case EV_TP_PINCH_IN:
+			pmsg->id 	= GUI_MSG_TOUCH_ZOOMIN;
+			pmsg->data 	= (pEvent_y.value<<16) + pEvent_x.value;
+			pmsg->reserved = (pEvent_speed_dir.value<<16) + pEvent_speed_val.value;
+			last_touch_action = GUI_MSG_TOUCH_ZOOMIN;
+			break;
+		case EV_TP_PINCH_OUT:
+			pmsg->id 	= GUI_MSG_TOUCH_ZOOMOUT;
+			pmsg->data 	= (pEvent_y.value<<16) + pEvent_x.value;
+			pmsg->reserved = (pEvent_speed_dir.value<<16) + pEvent_speed_val.value;
+			last_touch_action = GUI_MSG_TOUCH_ZOOMOUT;
+			break;
+		case EV_TP_PRESS_HOLD:
+			pmsg->id 	= last_touch_action;
+			pmsg->data 	= (pEvent_y.value<<16) + pEvent_x.value;
+			pmsg->reserved = (pEvent_speed_dir.value<<16) + pEvent_speed_val.value;
+			break;
+
+		case EV_TP_ACTION_NULL:
+		case EV_TP_ACTION_CLICK:
+		case EV_TP_ACTION_DOWN:
+		case EV_TP_ACTION_UP:
+		case EV_TP_ACTION_LEFT:
+		case EV_TP_ACTION_RIGHT:
+		case EV_TP_ACTION_ANTICLOCKWISE:
+		case EV_TP_ACTION_CLOCKWISE:
+		case EV_TP_ACTION_LD2RU:
+		case EV_TP_ACTION_RU2LD:
+		case EV_TP_ACTION_LU2RD:
+		case EV_TP_ACTION_RD2LU:
+			pmsg->id 	= GUI_MSG_TOUCH_UP;
+			pmsg->data 	= (pEvent_y.value<<16) + pEvent_x.value;
+			pmsg->reserved = (pEvent_speed_dir.value<<16) + pEvent_speed_val.value;
+			break;
+		default:
+			__msg("cannot identify message: %x\n", pEvent_type.value);
+			return EPDK_FALSE;
+	}
+	__msg("------------------------------------------------\n");
+	__msg("pmsg->type = %d, pmsg->id = %d\n", pmsg->type, pmsg->id);
+	__msg("x = %d, y = %d\n", LOSWORD(pmsg->data), HISWORD(pmsg->data));
+	__msg("------------------------------------------------\n");
+	esKRNL_QPost(emit_ctr.psys_msg_queue, pmsg);
+
+	return EPDK_TRUE;
+}
+
+static __s32 key_msg_cb(void *msg)
+{
+	__msrv_msg_t	*pmsg;
+	__u8 error;
+
+	__u32 i;
+	__input_event_packet_t *pPacket;
+	__input_event_t        *pEventFrame;
+
+	if (msg == NULL)
+	{
+		__msg("invalid argument for call back\n");
+		return EPDK_FAIL;
+	}
+
+	__inf("1------------------------------------------------\n");
+
+	//dump packet events
+	pPacket = (__input_event_packet_t *)msg;
+	if (pPacket->event_cnt > INPUT_MAX_EVENT_NR)
+	{
+		__msg("invalid event count number\n");
+		return EPDK_FAIL;
+	}
+	__inf("2------------------------------------------------\n");
+	for (i = 0; i < pPacket->event_cnt; i++)
+	{
+		pEventFrame = &(pPacket->events[i]);
+		__msg("Event %d: type = %d, code = %d, value = %d\n", i + 1,
+																 pEventFrame->type,
+																 pEventFrame->code,
+																 pEventFrame->value
+																 );
+		if(pEventFrame->type == EV_KEY)
+		{
+			__msg("pEventFrame->type == EV_KEY\n");
+
+			esKRNL_SemPend(emit_ctr.p_array_sem, 0, &error);
+			pmsg = get_msg_buf(&emit_ctr);
+			esKRNL_SemPost(emit_ctr.p_array_sem);
+
+
+			switch(pEventFrame->code)
+			{
+				case KPAD_MENU:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_MENU;
+					break;
+				}
+				case KPAD_UP:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_UP;
+					break;
+				}
+				case KPAD_DOWN:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_DOWN;
+					break;
+				}
+				case KPAD_LEFT:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_LEFT;
+					break;
+				}
+				case KPAD_RIGHT:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_RIGHT;
+					break;
+				}
+				case KPAD_VOICEUP:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_VADD;
+					break;
+				}
+				case KPAD_VOICEDOWN:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_VDEC;
+					break;
+				}
+				case KPAD_ENTER:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_ENTER;
+					break;
+				}
+				case KPAD_RETURN:
+				case KPAD_HOME:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_ESCAPE;
+					break;
+				}
+				case KPAD_POWER:
+				{
+					pmsg->type 	= GUI_MSG_KEY;
+					pmsg->id 	= GUI_MSG_KEY_POWER;
+					break;
+				}
+				case KPAD_POWEROFF:
+				{
+					pmsg->type 	= DSK_MSG_POWER_OFF;
+					break;
+				}
+				default:
+					break;
+			}
+			pmsg->data 	= pEventFrame->value;
+
+			__msg("GUI_MSG_KEY_ val=%d, pmsg->type=%d\n", pmsg->id, pmsg->type);
+
+			esKRNL_QPost(emit_ctr.psys_msg_queue, pmsg);
+		}
+	}
+	__inf("3------------------------------------------------\n");
+
+	return EPDK_TRUE;
+}
+
+/**
+ * 从系统消息队列取消息
+ */
+static void ksrv_msg_thread(void *arg)
+{
+	__memit_ctr_t *emit = (__memit_ctr_t *)arg;
+	__u8  error;
+
+	/* 清空按键消息队列 */
+	while(1)
+	{
+		__u32           usrmsg;
+
+		//usrmsg = esKSRV_GetLowMsg();
+		usrmsg = esKSRV_GetMsg();
+		if( usrmsg == 0 )
+			break;
+	}
+
+	while(1)
+	{
+		__u32           usrmsg;
+		__msrv_msg_t	*pmsg;
+
+		while(1)
+		{
+			if( esKRNL_TDelReq(OS_PRIO_SELF) == OS_TASK_DEL_REQ )
+			{
+				esKRNL_TDel(OS_PRIO_SELF);
+			}
+
+			usrmsg = esKSRV_GetMsg();				// 系统消息队列
+			if(usrmsg)
+				break;
+			esKRNL_TimeDly(2);
+		}
+
+		esKRNL_SemPend(emit->p_array_sem, 0, &error);
+		pmsg = get_msg_buf(emit);
+		//eLIBs_memset(pmsg, 0, sizeof(__msrv_msg_t));
+		esKRNL_SemPost(emit->p_array_sem);
+
+		if( (usrmsg & 0xffff0000) == KMSG_USR_CLS_KEY) 	// key 按键消息
+		{
+			switch (usrmsg)
+			{
+
+			#if( EPDK_CASE == EPDK_LIVE_BOX )
+				case KMSG_USR_KEY_POWER:				/* standby	*/
+					 pmsg->type = DSK_MSG_STANDBY;
+					 break;
+
+				case KMSG_USR_KEY_REPEATE:				/* 切换到色差输出 */
+					pmsg->type = DSK_MSG_SWITCH_YPBPR;
+					break;
+
+				case KMSG_USR_KEY_CLEAR:				/* 切换到cvbs输出 */
+					pmsg->type = DSK_MSG_SWITCH_CVBS;
+					break;
+
+				case KMSG_USR_KEY_DISPLAY:				/* 切换到hdmi输出 */
+					pmsg->type = DSK_MSG_SWITCH_HDMI;
+					break;
+
+				case KMSG_USR_KEY_SHIFT:
+					 pmsg->type = GUI_MSG_KEY;
+					 pmsg->id	= GUI_MSG_KEY_SHIFT;
+					break;
+
+				case KMSG_USR_KEY_SEL:
+					 pmsg->type = GUI_MSG_KEY;
+					 pmsg->id	= GUI_MSG_KEY_SEL;
+					break;
+
+				case KMSG_USR_KEY_NUM4:
+					pmsg->type = DSK_MSG_SWITCH_AUDIO_IF;
+					pmsg->data = AUDIO_DEV_IF_CODEC;
+					break;
+
+				case KMSG_USR_KEY_NUM5:
+					pmsg->type = DSK_MSG_SWITCH_AUDIO_IF;
+					pmsg->data = AUDIO_DEV_IF_IIS;
+					break;
+
+				case KMSG_USR_KEY_NUM6:
+					pmsg->type = DSK_MSG_SWITCH_AUDIO_IF;
+					pmsg->data = AUDIO_DEV_IF_SPDIF;
+					break;
+
+			#endif
+
+			#if( EPDK_CASE == EPDK_LIVE_TOUCH )
+				case KMSG_USR_KEY_NUM8:					/* hold 按键 */
+					 pmsg->type = DSK_MSG_HOLD;
+					 break;
+
+				case KMSG_USR_KEY_NUM7:								// 禁音功能
+					pmsg->type = DSK_MSG_BAN_VOLUME;
+					pmsg->data = 0;
+					break;
+
+			#endif
+
+				case KMSG_USR_KEY_GOTO:
+					pmsg->type = DSK_MSG_APP_EXIT;		/* 一键回主界面 */
+					break;
+
+				case KMSG_USR_KEY_POWEROFF:				// 关机消息
+					 pmsg->type = DSK_MSG_POWER_OFF;
+					 break;
+
+				case KMSG_USR_KEY_NUM9:
+					 pmsg->type = GUI_MSG_KEY;
+					 pmsg->id	= GUI_MSG_KEY_ESCAPE;
+					 break;
+
+				case KMSG_USR_KEY_VOICE_UP:
+					 pmsg->type = GUI_MSG_KEY;
+					 pmsg->id	= GUI_MSG_KEY_VADD;
+					 break;
+
+				case KMSG_USR_KEY_VOICE_DOWN:
+					 pmsg->type    = GUI_MSG_KEY;
+					 pmsg->id	   = GUI_MSG_KEY_VDEC;
+					 break;
+
+				case KMSG_USR_KEY_UP:
+					 pmsg->type    = GUI_MSG_KEY;
+					 pmsg->id	   = GUI_MSG_KEY_UP;
+					 break;
+
+				case KMSG_USR_KEY_DOWN  :
+					 pmsg->type    = GUI_MSG_KEY;
+					 pmsg->id	   = GUI_MSG_KEY_DOWN;
+					 break;
+
+				case KMSG_USR_KEY_LEFT  :
+					 pmsg->type    	= GUI_MSG_KEY;
+					 pmsg->id	    = GUI_MSG_KEY_LEFT;
+					 break;
+
+				case KMSG_USR_KEY_RIGHT :
+					 pmsg->type    	= GUI_MSG_KEY;
+					 pmsg->id	    = GUI_MSG_KEY_RIGHT;
+					 break;
+
+				case KMSG_USR_KEY_ENTER:
+					 pmsg->type    	= GUI_MSG_KEY;
+					 pmsg->id	    = GUI_MSG_KEY_ENTER;
+					break;
+
+				case KMSG_USR_KEY_RISE:
+					pmsg->type    	= GUI_MSG_KEY;
+					pmsg->id	    = GUI_MSG_KEY_RISE;
+					break;
+
+				default:
+					continue;
+			}
+		}
+		else	/* system 消息 */
+		{
+			if( (usrmsg & 0x0000ffff) == KMSG_USR_SYSTEM_FS_PLUGIN )
+			{
+				pmsg->type = DSK_MSG_FS_PART_PLUGIN;
+				pmsg->data = (usrmsg & KMSG_USR_SYSTEM_FS_PARA_MASK)>>16;
+			}
+			else if( (usrmsg & 0x0000ffff) == KMSG_USR_SYSTEM_FS_PLUGOUT )
+			{
+				pmsg->type = DSK_MSG_FS_PART_PLUGOUT;
+				pmsg->data = (usrmsg & KMSG_USR_SYSTEM_FS_PARA_MASK)>>16;
+			}
+			else if((usrmsg & 0x0000ffff) == KMSG_USR_SYSTEM_WLAN_PLUGIN)
+			{
+				tp_ad("00DSK_MSG_WIFI_PART_PLUGIN\n");
+				pmsg->type = DSK_MSG_WIFI_PART_PLUGIN;
+			}
+			else if((usrmsg & 0x0000ffff) == KMSG_USR_SYSTEM_WLAN_PLUGOUT)
+			{
+				tp_ad("00DSK_MSG_WIFI_PART_PLUGOUT\n");
+				pmsg->type = DSK_MSG_WIFI_PART_PLUGOUT;
+			}
+			else
+			{
+				__msg("*************usrmsg = %d***************\n", usrmsg);
+				switch (usrmsg)
+				{
+					case KMSG_USR_SYSTEM_USBD_PLUGIN:		// usb device plug in
+						 __msg("case KMSG_USR_SYSTEM_USBD_PLUGIN\n");
+						 pmsg->type = DSK_MSG_USBD_PLUG_IN;
+						 break;
+
+					case KMSG_USR_SYSTEM_USBD_PLUGOUT:    	// usb device plug out
+						 pmsg->type = DSK_MSG_USBD_PLUG_OUT;
+						 __msg("case KMSG_USR_SYSTEM_USBD_PLUGOUT\n");
+						 break;
+
+					case KMSG_USR_SYSTEM_SDMMC_PLUGOUT:
+						 pmsg->type = DSK_MSG_APP_EXIT;
+						 break;
+
+					case KMSG_USR_SYSTEM_USBH_PLUGOUT:		// usb host 设备拔出
+						 pmsg->type = DSK_MSG_APP_EXIT;
+						 break;
+
+					case KMSG_USR_SYSTEM_TVDAC_PLUGIN:
+						__msg("**********KMSG_USR_SYSTEM_TVDAC_PLUGIN***********\n");
+						 pmsg->type = DSK_MSG_TVDAC_PLUGIN;
+						 break;
+
+					case KMSG_USR_SYSTEM_TVDAC_PLUGOUT:
+						__msg("***********KMSG_USR_SYSTEM_TVDAC_PLUGOUT***************\n");
+						 pmsg->type = DSK_MSG_TVDAC_PLUGOUT;
+						 break;
+
+					case KMSG_USR_SYSTEM_HDMI_PLUGIN:
+						__msg("**********KMSG_USR_SYSTEM_HDMI_PLUGIN***********\n");
+						 pmsg->type = DSK_MSG_HDMI_PLUGIN;
+						 break;
+
+					case KMSG_USR_SYSTEM_HDMI_PLUGOUT:
+						__msg("***********KMSG_USR_SYSTEM_HDMI_PLUGOUT***************\n");
+						 pmsg->type = DSK_MSG_HDMI_PLUGOUT;
+						 break;
+
+					case KMSG_USR_SYSTEM_WEBCAM_PLUGIN:
+						 eLIBs_printf("***********KMSG_USR_SYSTEM_WEBCAM_PLUGIN***************\n");
+		    		     pmsg->type = DSK_MSG_UVC_DEVICE_PLUGIN;
+		                 break;
+
+					case KMSG_USR_SYSTEM_WEBCAM_PLUGOUT:
+						 eLIBs_printf("***********KMSG_USR_SYSTEM_WEBCAM_PLUGOUT***************\n");
+		    		     pmsg->type = DSK_MSG_UVC_DEVICE_PLUGOUT;
+		                 break;
+					#if( EPDK_CASE == EPDK_LIVE_TOUCH )
+					case KMSG_USR_SYSTEM_WAKEUP:
+						 __here__
+						pmsg->type = DSK_MSG_STANDBY_WAKE_UP;
+					 break;
+					#endif
+
+					default:
+						continue;
+				}
+			}
+		}
+
+		esKRNL_QPost(emit->psys_msg_queue, pmsg);
+	}
+}
+
+/**
+ *
+ */
+__s32 msg_srv_get_message(__memit_ctr_t *emit, __msrv_msg_t *p_msg)
+{
+	__u8 error;
+	__msrv_msg_t *tmp;
+
+	tmp = (__msrv_msg_t *)esKRNL_QAccept( emit->psys_msg_queue, &error);
+
+	if( tmp!= NULL)
+	{
+		memcpy( p_msg, tmp, sizeof(__msrv_msg_t) );
+		return EPDK_OK;
+	}
+	else
+	{
+		return EPDK_FAIL;
+	}
+
+}
+
+
+static void app_init(void)
+{
+    ES_FILE 	*pHwsc;
+    __s32  LdevID;
+    esDEV_Plugin("\\drv\\audio.drv", 0, 0, 1);
+    esDEV_Plugin("\\drv\\matrixkey.drv", 0, 0, 1);
+	esDEV_Plugin("\\drv\\ir.drv", 0, 0, 1);
+    esDEV_Plugin("\\drv\\touchpanel.drv", 0, 0, 1);
+
+    pHwsc = eLIBs_fopen("b:\\HWSC\\hwsc", "rb+");
+    if(pHwsc)
+    {
+        eLIBs_fioctrl(pHwsc, DEV_IOC_USR_HWSC_ENABLE_MONITOR, 0, NULL);
+        eLIBs_fclose(pHwsc);
+    }
+    else
+    {
+        __err("try to open b:\\HWSC\\hwsc failed!\n");
+    }
+    eLIBs_memset(&prv_kernel, 0, sizeof(prv_kernel));
+    prv_kernel.psys_msg_queue  = esKRNL_QCreate(TP_EVENT_CNT + KEY_EVENT_CNT + SRV_EVENT_CNT);
+    prv_kernel.h_tpGraber = esINPUT_LdevGrab(INPUT_LTS_DEV_NAME, (__pCBK_t)tp_msg_cb, 0, 0);
+    prv_kernel.h_keyGraber = esINPUT_LdevGrab(INPUT_LKEYBOARD_DEV_NAME, (__pCBK_t)key_msg_cb, 0, 0);
+
+    LdevID = esINPUT_GetLdevID(prv_kernel.h_keyGraber);
+	if (LdevID != -1)
+	{
+        if (esINPUT_LdevCtl(LdevID, INPUT_SET_REP_PERIOD, 200, NULL) != EPDK_OK)
+        {
+            DBG("logical device ioctl failed\n");
+        }
+	}
+    prv_kernel.ksrv_th_id = esKRNL_TCreate(ksrv_msg_thread, (void *)&prv_kernel, 0x400, KRNL_priolevel3);
+}
 //初始化显示
 static void disp_lcd_init(void)
 {	
@@ -146,6 +755,7 @@ static void disp_lcd_test(void)
 
 static void port_thread(void *arg)
 {
+    app_init();
     disp_lcd_init();
     disp_lcd_test();
     //luatos_main_entry();

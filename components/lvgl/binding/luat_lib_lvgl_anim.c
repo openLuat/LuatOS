90e6680bf564cbc37d0906069cddec28f6d15132
Wendal Chen <wendal1985@gmail.com>
2021-07-14 22:57:29 +0800

    add: 添加lvgl.anim_set_path_str方法,设置动画路径

--- components/lvgl/binding/luat_lib_lvgl_anim.c
+++ components/lvgl/binding/luat_lib_lvgl_anim.c
@@ -31,3 +31,50 @@ int luat_lv_anim_free(lua_State *L) {
     }
     return 0;
 }
+
+
+/*
+设置动画路径方式
+@api lvgl.anim_set_path_str(anim, tp)
+@userdata 动画指针
+@string 类型, 支持 linear/ease_in/ease_out/ease_in_out/overshoot/bounce/step
+@return nil 无返回值
+*/
+int luat_lv_anim_set_path_str(lua_State *L) {
+    lv_anim_t* anim = lua_touserdata(L, 1);
+    if (anim == NULL) {
+        LLOGW("obj is NULL when set event cb");
+        return 0;
+    }
+    const char* tp = luaL_checkstring(L, 2);
+    lv_anim_path_t path;
+    lv_anim_path_init(&path);
+    if (!strcmp("linear", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_linear);
+    }
+    else if (!strcmp("ease_in", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_ease_in);
+    }
+    else if (!strcmp("ease_out", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_ease_out);
+    }
+    else if (!strcmp("ease_in_out", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_ease_in_out);
+    }
+    else if (!strcmp("overshoot", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_overshoot);
+    }
+    else if (!strcmp("bounce", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_bounce);
+    }
+    else if (!strcmp("step", tp)) {
+        lv_anim_path_set_cb(&path, lv_anim_path_step);
+    }
+    else {
+        //lv_anim_path_set_cb(&path, NULL);
+        return 0;
+    }
+    lv_anim_set_path(anim, &path);
+    return 0;
+}
+

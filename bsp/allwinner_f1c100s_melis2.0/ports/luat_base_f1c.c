5aca781b717674132d591b0ff3480a8d02389c33
Wendal Chen <wendal1985@gmail.com>
2021-07-16 17:05:22 +0800

    update: 解决f1c编译的问题

--- bsp/allwinner_f1c100s_melis2.0/ports/luat_base_f1c.c
+++ bsp/allwinner_f1c100s_melis2.0/ports/luat_base_f1c.c
@@ -17,19 +17,19 @@ void luatos_main_entry(void) {
 
 
 static const luaL_Reg loadedlibs[] = {
-  // {"_G", luaopen_base}, // _G
-//   {LUA_LOADLIBNAME, luaopen_package}, // require
-//   {LUA_COLIBNAME, luaopen_coroutine}, // coroutine协程库
-//   {LUA_TABLIBNAME, luaopen_table},    // table库,操作table类型的数据结构
-//   {LUA_IOLIBNAME, luaopen_io},        // io库,操作文件
-//   {LUA_OSLIBNAME, luaopen_os},        // os库,已精简
-//   {LUA_STRLIBNAME, luaopen_string},   // string库,字符串操作
-//   {LUA_MATHLIBNAME, luaopen_math},    // math 数值计算
-// //  {LUA_UTF8LIBNAME, luaopen_utf8},
-//   {LUA_DBLIBNAME, luaopen_debug},     // debug库,已精简
-// #if defined(LUA_COMPAT_BITLIB)
-//   {LUA_BITLIBNAME, luaopen_bit32},    // 不太可能启用
-// #endif
+  {"_G", luaopen_base}, // _G
+  {LUA_LOADLIBNAME, luaopen_package}, // require
+  {LUA_COLIBNAME, luaopen_coroutine}, // coroutine协程库
+  {LUA_TABLIBNAME, luaopen_table},    // table库,操作table类型的数据结构
+  {LUA_IOLIBNAME, luaopen_io},        // io库,操作文件
+  {LUA_OSLIBNAME, luaopen_os},        // os库,已精简
+  {LUA_STRLIBNAME, luaopen_string},   // string库,字符串操作
+  {LUA_MATHLIBNAME, luaopen_math},    // math 数值计算
+//  {LUA_UTF8LIBNAME, luaopen_utf8},
+  {LUA_DBLIBNAME, luaopen_debug},     // debug库,已精简
+#if defined(LUA_COMPAT_BITLIB)
+  {LUA_BITLIBNAME, luaopen_bit32},    // 不太可能启用
+#endif
 //   {"rtos", luaopen_rtos},             // rtos底层库, 核心功能是队列和定时器
 //   {"log", luaopen_log},               // 日志库
 //   {"timer", luaopen_timer},           // 延时库
@@ -51,18 +51,18 @@ static const luaL_Reg loadedlibs[] = {
 };
 
 // 按不同的rtconfig加载不同的库函数
-// void luat_openlibs(lua_State *L) {
-//     // 初始化队列服务
-//     luat_msgbus_init();
-//     //print_list_mem("done>luat_msgbus_init");
-//     // 加载系统库
-//     const luaL_Reg *lib;
-//     /* "require" functions from 'loadedlibs' and set results to global table */
-//     for (lib = loadedlibs; lib->func; lib++) {
-//         luaL_requiref(L, lib->name, lib->func, 1);
-//         lua_pop(L, 1);  /* remove lib */
-//     }
-// }
+void luat_openlibs(lua_State *L) {
+    // 初始化队列服务
+    luat_msgbus_init();
+    //print_list_mem("done>luat_msgbus_init");
+    // 加载系统库
+    const luaL_Reg *lib;
+    /* "require" functions from 'loadedlibs' and set results to global table */
+    for (lib = loadedlibs; lib->func; lib++) {
+        luaL_requiref(L, lib->name, lib->func, 1);
+        lua_pop(L, 1);  /* remove lib */
+    }
+}
 
 void luat_os_reboot(int code) {
 
@@ -84,12 +84,46 @@ void luat_os_exit_cri(void) {
 
 }
 
-#include "lvgl.h"
-void luat_lv_user_data_free(lv_obj_t * obj) {
-    // if (obj == NULL || (obj->user_data.event_cb_ref == 0 && obj->user_data.signal_cb_ref)) return;
-    // rtos_msg_t msg = {0};
-    // msg.handler = lv_func_unref;
-    // msg.arg1 = obj->user_data.event_cb_ref;
-    // msg.arg2 = obj->user_data.signal_cb_ref;
-    // luat_msgbus_put(&msg, 0);
+void* luat_heap_alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+    // if (0) {
+    //     if (ptr) {
+    //         if (nsize) {
+    //             // 缩放内存块
+    //             LLOGD("realloc %p from %d to %d", ptr, osize, nsize);
+    //         }
+    //         else {
+    //             // 释放内存块
+    //             LLOGD("free %p ", ptr);
+    //             brel(ptr);
+    //             return NULL;
+    //         }
+    //     }
+    //     else {
+    //         // 申请内存块
+    //         ptr = bget(nsize);
+    //         LLOGD("malloc %p type=%d size=%d", ptr, osize, nsize);
+    //         return ptr;
+    //     }
+    // }
+
+    if (nsize)
+    {
+    	void* ptmp = bgetr(ptr, nsize);
+    	if(ptmp == NULL && osize >= nsize)
+    	{
+    		return ptr;
+    	}
+        return ptmp;
+    }
+    brel(ptr);
+    return NULL;
+}
+
+void luat_meminfo_luavm(size_t *total, size_t *used, size_t *max_used) {
+	long curalloc, totfree, maxfree;
+	unsigned long nget, nrel;
+	bstats(&curalloc, &totfree, &maxfree, &nget, &nrel);
+	*used = curalloc;
+	*max_used = bstatsmaxget();
+    *total = curalloc + totfree;
 }

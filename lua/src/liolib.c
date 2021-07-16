027f57b27d60dbe7959c073af3f0519666669290
Wendal Chen <wendal1985@gmail.com>
2021-07-16 15:26:48 +0800

    change: 按temp分支的修正,清理stdio相关操作,适配全志f1c环境

--- lua/src/liolib.c
+++ lua/src/liolib.c
@@ -659,20 +659,20 @@ static int g_write (lua_State *L, FILE *f, int arg) {
   int nargs = lua_gettop(L) - arg;
   int status = 1;
   for (; nargs--; arg++) {
-    if (lua_type(L, arg) == LUA_TNUMBER) {
-      /* optimization: could be done exactly as for strings */
-      int len = lua_isinteger(L, arg)
-                ? fprintf(f, LUA_INTEGER_FMT,
-                             (LUAI_UACINT)lua_tointeger(L, arg))
-                : fprintf(f, LUA_NUMBER_FMT,
-                             (LUAI_UACNUMBER)lua_tonumber(L, arg));
-      status = status && (len > 0);
-    }
-    else {
+    // if (lua_type(L, arg) == LUA_TNUMBER) {
+    //   /* optimization: could be done exactly as for strings */
+    //   int len = lua_isinteger(L, arg)
+    //             ? fprintf(f, LUA_INTEGER_FMT,
+    //                          (LUAI_UACINT)lua_tointeger(L, arg))
+    //             : fprintf(f, LUA_NUMBER_FMT,
+    //                          (LUAI_UACNUMBER)lua_tonumber(L, arg));
+    //   status = status && (len > 0);
+    // }
+    // else {
       size_t l;
       const char *s = luaL_checklstring(L, arg, &l);
       status = status && (fwrite(s, sizeof(char), l, f) == l);
-    }
+    // }
   }
   if (status) return 1;  /* file handle already on stack top */
   else return luaL_fileresult(L, status, NULL);
@@ -728,7 +728,11 @@ static int f_setvbuf (lua_State *L) {
 
 
 static int f_flush (lua_State *L) {
+#ifdef LUA_USE_WINDOWS
   return luaL_fileresult(L, fflush(tofile(L)) == 0, NULL);
+#else
+  return 0;
+#endif
 }
 
 #include "luat_malloc.h"

28676f06015bd32bf2a3edc73801d94f2c8eb740
Dozingfiretruck <1473454180@qq.com>
2021-08-13 14:59:59 +0800

    add:修改gauge_set_needle_count更还原原生api

--- bsp/win32/test/lvgl_demo_test/gauge_demo.lua
+++ bsp/win32/test/lvgl_demo_test/gauge_demo.lua
@@ -3,10 +3,10 @@ local gauge_demo = {}
 --demo1
 function gauge_demo.demo1()
     --Describe the color for the needles
-
+    local needle_colors = {lvgl.color_make(0x00, 0x00, 0xFF), lvgl.color_make(0xFF, 0xA5, 0x00), lvgl.color_make(0x80, 0x00, 0x80)}
     --Create a gauge*/
     local gauge1 = lvgl.gauge_create(lvgl.scr_act(), nil);
-    lvgl.gauge_set_needle_count(gauge1, 3, lvgl.color_make(0x00, 0x00, 0xFF), lvgl.color_make(0xFF, 0xA5, 0x00), lvgl.color_make(0x80, 0x00, 0x80));
+    lvgl.gauge_set_needle_count(gauge1, 3, needle_colors);
     lvgl.obj_set_size(gauge1, 200, 200);
     lvgl.obj_align(gauge1, nil, lvgl.ALIGN_CENTER, 0, 0);
 

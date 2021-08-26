a6ff913f32541144827d04a8973f7502d14ea3f0
Wendal Chen <wendal1985@gmail.com>
2021-08-26 18:11:00 +0800

    change: lvgl库引用的lcd配置信息应该是static的

--- components/lvgl/binding/luat_lib_lvgl7.c
+++ components/lvgl/binding/luat_lib_lvgl7.c
@@ -9,12 +9,12 @@
 static lv_disp_t* my_disp = NULL;
 static lv_disp_buf_t my_disp_buff = {0};
 //static lv_disp_drv_t my_disp_drv;
-luat_lcd_conf_t* conf;
+static luat_lcd_conf_t* lcd_conf;
 
 static void disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
     //-----
-    if (conf != NULL) {
-        conf->opts->draw(conf, area->x1, area->y1, area->x2, area->y2, color_p);
+    if (lcd_conf != NULL) {
+        lcd_conf->opts->draw(conf, area->x1, area->y1, area->x2, area->y2, color_p);
     }
     // LLOGD("CALL disp_flush (%d, %d, %d, %d)", area->x1, area->y1, area->x2, area->y2);
     lv_disp_flush_ready(disp_drv);
@@ -45,12 +45,14 @@ int luat_lv_init(lua_State *L) {
     lua_pushboolean(L, 1);
     return 1;
     #else
-    conf = luat_lcd_get_default();
+    lcd_conf = luat_lcd_get_default();
+    if (lcd_conf == NULL)
+        return 0;
     lv_color_t *fbuffer = NULL;
     size_t fbuff_size = 0;
 
-    int w = luaL_optinteger(L, 1, conf->w);
-    int h = luaL_optinteger(L, 2, conf->h);
+    int w = luaL_optinteger(L, 1, lcd_conf->w);
+    int h = luaL_optinteger(L, 2, lcd_conf->h);
 
     fbuff_size = w * h / 10;
 

a0fd297a7863d573ea1668d83ada08cb7127f606
Dozingfiretruck <1473454180@qq.com>
2021-09-01 14:37:05 +0800

    add:增加rtc demo

---
+++ bsp/air101/demo/rtc/main.lua
@@ -0,0 +1,31 @@
+--- 模块功能：rtcdemo
+-- @module rtc
+-- @author Dozingfiretruck
+-- @release 2021.01.25
+
+-- LuaTools需要PROJECT和VERSION这两个信息
+PROJECT = "rtcdemo"
+VERSION = "1.0.1"
+
+-- sys库是标配
+_G.sys = require("sys")
+
+sys.taskInit(function()
+    log.info("os.date()", os.date())
+    local t = rtc.get()
+    log.info("rtc", json.encode(t))
+    sys.wait(2000)
+    rtc.set({year=2021,mon=8,day=31,hour=17,min=8,sec=43})
+    log.info("os.date()", os.date())
+    -- rtc.timerStart(0, {year=2021,mon=9,day=1,hour=17,min=8,sec=43})
+    -- rtc.timerStop(0)
+    while 1 do
+        log.info("os.date()", os.date())
+        local t = rtc.get()
+        log.info("rtc", json.encode(t))
+        sys.wait(1000)
+    end
+end)
+
+-- 主循环, 必须加
+sys.run()

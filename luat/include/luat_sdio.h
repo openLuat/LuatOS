c9428a2eee1ebdfbefde23447ec58211898174ba
Dozingfiretruck <1473454180@qq.com>
2021-09-02 09:33:51 +0800

    add:初始化需修改实参，使用指针

--- luat/include/luat_sdio.h
+++ luat/include/luat_sdio.h
@@ -4,5 +4,5 @@
 int luat_sdio_init(int id);
 int luat_sdio_sd_read(int id, int rca, char* buff, size_t offset, size_t len);
 int luat_sdio_sd_write(int id, int rca, char* buff, size_t offset, size_t len);
-int luat_sdio_sd_mount(int id, int rca, int auto_format);
+int luat_sdio_sd_mount(int id, int *rca, int auto_format);
 int luat_sdio_sd_format(int id, int rca);

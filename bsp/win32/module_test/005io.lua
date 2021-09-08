799a4eaafdb4cff601d08af195a345e0c419a511
Wendal Chen <wendal1985@gmail.com>
2021-09-08 21:34:13 +0800

    update: lfs2的测试用例加上seek的检验

--- bsp/win32/module_test/005io.lua
+++ bsp/win32/module_test/005io.lua
@@ -6,23 +6,26 @@ sys.taskInit(function()
     sys.wait(100) -- 特意的,检验sys.run在运行
 
     -- 读写文件1000次
-    for i = 1, 1000, 1 do
+    for i = 1, 100, 1 do
         local f = io.open("T", "wb")
         assert(f)
-        f:write(os.date())
+        local data = os.date()
+        f:write(data)
         f:close()
         f = io.open("T", "rb")
         assert(f)
+        assert(f:read("a") == data)
         f:close()
     end
 
     -- 快捷读写文件1000次
-    for i = 1, 1000, 1 do
+    for i = 1, 100, 1 do
         os.remove("T")
         io.writeFile("T", os.date())
         io.readFile("T")
     end
 
+    log.info("sys", "all done")
     os.exit(0)
 end)
 

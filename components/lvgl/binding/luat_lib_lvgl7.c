fc19dabf04451007bfbe630815a8f135b37ca88a
Dozingfiretruck <1473454180@qq.com>
2021-10-12 14:05:56 +0800

    add：disp_flush win32下不编译

--- components/lvgl/binding/luat_lib_lvgl7.c
+++ components/lvgl/binding/luat_lib_lvgl7.c
@@ -9,6 +9,8 @@
 static lv_disp_t* my_disp = NULL;
 static lv_disp_buf_t my_disp_buff = {0};
 //static lv_disp_drv_t my_disp_drv;
+
+#if !defined (LUA_USE_WINDOWS) && !defined (LUA_USE_LINUX)
 static luat_lcd_conf_t* lcd_conf;
 
 static void disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
@@ -19,6 +21,7 @@ static void disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_colo
     // LLOGD("CALL disp_flush (%d, %d, %d, %d)", area->x1, area->y1, area->x2, area->y2);
     lv_disp_flush_ready(disp_drv);
 }
+#endif
 
 #ifdef LUA_USE_WINDOWS
 #include <windows.h>

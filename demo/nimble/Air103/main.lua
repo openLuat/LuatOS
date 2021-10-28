4b897273de157451c2419053850dc4bd849aebb3
Wendal Chen <wendal1985@gmail.com>
2021-10-28 20:31:47 +0800

    add: 添加nimble库测试ble功能的demo

---
+++ demo/nimble/Air103/main.lua
@@ -0,0 +1,45 @@
+
+-- LuaTools需要PROJECT和VERSION这两个信息
+PROJECT = "nimbledemo"
+VERSION = "1.0.0"
+
+log.info("main", PROJECT, VERSION)
+
+-- 一定要添加sys.lua !!!!
+local sys = require "sys"
+
+--添加硬狗防止程序卡死
+if wdt then
+    wdt.init(15000)--初始化watchdog设置为15s
+    sys.timerLoopStart(wdt.feed, 10000)--10s喂一次狗
+end
+
+-- 监听BLE主适配的状态变化
+sys.subscribe("BLE_STATE_INC", function(state)
+    log.info("ble", "ble state changed", state)
+    if state == 1 then
+        nimble.server_init()
+    else
+        nimble.server_deinit()
+    end
+end)
+
+-- 监听GATT服务器的WRITE_CHR
+sys.subscribe("BLE_GATT_WRITE_CHR", function(info, data)
+    -- info 是个table, 但当前没有数据
+    log.info("ble", "data got!!", data:toHex())
+end)
+
+-- TODO 支持传数据(read)和推送数据(notify)
+
+sys.taskInit(function()
+    sys.wait(2000)
+    nimble.debug(6)
+    nimble.init()
+end)
+
+
+-- 用户代码已结束---------------------------------------------
+-- 结尾总是这一句
+sys.run()
+-- sys.run()之后后面不要加任何语句!!!!!

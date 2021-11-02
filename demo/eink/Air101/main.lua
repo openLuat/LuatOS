da0915cc4386e212d5bd343f11258775a902b352
Dozingfiretruck <1473454180@qq.com>
2021-11-02 16:13:40 +0800

    add:添加Air101墨水屏demo

--- demo/eink/Air101/main.lua
+++ demo/eink/Air101/main.lua
@@ -7,17 +7,13 @@ VERSION = "1.0.0"
 _G.sys = require("sys")
 
 --[[
-本DEMO需要V0005或2020-12-14及之后的源码才支持
-]]
-
---[[
-显示屏为佳显 1.54寸,200x200,快刷屏
+显示屏为佳显 1.54寸v2,200x200,快刷屏
 硬件接线
-显示屏SPI          --> Air302 SPI
-显示屏 Pin_BUSY        (GPIO18)
-显示屏 Pin_RES         (GPIO7)
-显示屏 Pin_DC          (GPIO9)
-显示屏 Pin_CS          (GPIO16)
+显示屏SPI          --> Air101 SPI0
+显示屏 Pin_BUSY        (PB1)
+显示屏 Pin_RES         (PA1)
+显示屏 Pin_DC          (PA4)
+显示屏 Pin_CS          (PB4)
 ]]
 
 function eink154_update()
@@ -35,26 +31,17 @@ function eink154_update()
     eink.show()
 end
 
-
-
-sys.taskInit(function()
-
-    log.info("eink", "begin setup")
-    -- 初始化必要的参数
-    eink.setup(1, 0)
-    -- 设置视窗大小
+    eink.model(eink.MODEL_1in54_V2)
+    eink.setup(1, 0,17,1,4,20)
     eink.setWin(200, 200, 0)
     log.info("eink", "end setup")
-
     -- 稍微等一会,免得墨水屏没初始化完成
     sys.wait(1000)
     while 1 do
         log.info("e-paper 1.54", "Testing Go\r\n")
         eink154_update()
         log.info("e-paper 1.54", "Testing End\r\n")
-
-        sys.wait(3000) -- 3秒刷新一次
+        sys.wait(5000) -- 3秒刷新一次
     end
-end)
 
 sys.run()

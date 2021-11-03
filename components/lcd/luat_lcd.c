d977191af9b7392289cc8cf8a8c3858cfd21e452
chenxuuu <lolicon@papapoi.com>
2021-11-03 16:19:28 +0800

    fix: lcd矩形显示问题

--- components/lcd/luat_lcd.c
+++ components/lcd/luat_lcd.c
@@ -257,18 +257,19 @@ int luat_lcd_draw_line(luat_lcd_conf_t* conf,uint16_t x1, uint16_t y1, uint16_t 
     {
         /* fast draw transverse line */
         luat_lcd_set_address(conf,x1, y1, x2, y2);
-        uint8_t line_buf[480] = {0};
-        for (i = 0; i < x2 - x1; i++)
+        uint8_t* line_buf = (uint8_t*)luat_heap_malloc((x2 - x1 + 1) * 2);
+        for (i = 0; i < x2 - x1 + 1; i++)
         {
             line_buf[2 * i] = color >> 8;
             line_buf[2 * i + 1] = color;
         }
         luat_gpio_set(conf->pin_dc, Luat_GPIO_HIGH);
         if (conf->port == LUAT_LCD_SPI_DEVICE){
-            luat_spi_device_send((luat_spi_device_t*)(conf->userdata), line_buf, (x2 - x1) * 2);
+            luat_spi_device_send((luat_spi_device_t*)(conf->userdata), line_buf, (x2 - x1 + 1) * 2);
         }else{
-            luat_spi_send(conf->port, line_buf, (x2 - x1) * 2);
+            luat_spi_send(conf->port, line_buf, (x2 - x1 + 1) * 2);
         }
+        luat_heap_free(line_buf);
         return 0;
     }
 

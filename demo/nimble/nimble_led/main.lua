8000587ce45ca2265425062d80c58e1de6050af4
Wendal Chen <wendal1985@gmail.com>
2021-11-08 22:04:58 +0800

    update: 完善一下nimble_led库

--- demo/nimble/nimble_led/main.lua
+++ demo/nimble/nimble_led/main.lua
@@ -27,16 +27,33 @@ elseif rtos.bsp() == "air103" then -- 与w806等价
     leds["a"] = gpio.setup(16, 0, gpio.PULLUP) -- PB0,输出模式
     leds["b"] = gpio.setup(17, 0, gpio.PULLUP) -- PB1,输出模式
     leds["c"] = gpio.setup(18, 0, gpio.PULLUP) -- PB2,输出模式
+else
+    log.info("gpio", "pls add gpio.setup for you board")
 end
-local ble_display = nil
+
+if lcd then
+    spi_lcd = spi.deviceSetup(0,20,0,0,8,20*1000*1000,spi.MSB,1,1)
+    lcd.setColor(0x0000,0xFFFF)
+    log.info("lcd.init",
+    lcd.init("st7735s",{port = "device",pin_dc = 17, pin_pwr = 7,pin_rst = 19,direction = 2,w = 160,h = 80,xoffset = 1,yoffset = 26},spi_lcd))
+    lcd.clear()
+    lcd.setFont(lcd.font_opposansm12_chinese)
+    lcd.drawStr(30,15,"nimbledemo",0X07FF)
+    lcd.drawStr(50,35,"监听中",0x001F)
+else
+    log.info("lcd", "lcd not found, display is off")
+end
+
 -- 注册一个命令列表
 cmds = {
+    -- 控制led的命令
     led = function(id, val)
         local led = leds[id]
         if led then
             led(val == "on" and 1 or 0)
         end
     end,
+    -- 重启板子的命令
     reboot = function()
         sys.taskInit(function()
             log.info("ble", "cmd reboot, after 5s")
@@ -44,25 +61,17 @@ cmds = {
             rtos.reboot()
         end)
     end,
-    display = function()
-        if lcd then
-            ble_display = 1
-            spi_lcd = spi.deviceSetup(0,20,0,0,8,20*1000*1000,spi.MSB,1,1)
-            lcd.setColor(0x0000,0xFFFF)
-            log.info("lcd.init",
-            lcd.init("st7735s",{port = "device",pin_dc = 17, pin_pwr = 7,pin_rst = 19,direction = 2,w = 160,h = 80,xoffset = 1,yoffset = 26},spi_lcd))
-            lcd.clear()
-            lcd.setFont(lcd.font_opposansm12_chinese)
-            lcd.drawStr(30,15,"nimbledemo",0X07FF)
-            lcd.drawStr(50,35,"监听中",0x001F)
-        end
+    -- 显示屏输出内容的命令
+    display = function(text)
+        -- TODO 清空第二行
+        -- lcd.fill(50, 35, 12, 160, 0x07FF)
+        lcd.drawStr(50 , 35, text ,0x001F)
     end,
 }
 
--- 监听BLE主适配的状态变化
+-- 监听BLE主适配的状态变化,需要nimble库
 if nimble then
-
-
+    -- BLE初始化成功或失败会产生该事件
     sys.subscribe("BLE_STATE_INC", function(state)
         log.info("ble", "ble state changed", state)
         if state == 1 then
@@ -72,7 +81,7 @@ if nimble then
         end
     end)
     
-    -- 监听GATT服务器的WRITE_CHR
+    -- 监听GATT服务器的WRITE_CHR事件
     sys.subscribe("BLE_GATT_WRITE_CHR", function(info, data)
         -- info 是个table, 但当前没有数据
         log.info("ble", "data got!!", data:toHex())
@@ -85,7 +94,7 @@ if nimble then
         -- led,a,off 对应 6c65642c612c6f6666
         -- led,b,off 对应 6c65642c622c6f6666
         -- led,c,off 对应 6c65642c632c6f6666
-        -- display 对应 646973706C6179
+        -- display,xxx 对应 646973706C6179xxx, 支持中文
         local cmd = data:split(",")
         if cmd[1] and cmds[cmd[1]] then
             cmds[cmd[1]](table.unpack(cmd, 2))
@@ -106,15 +115,31 @@ if nimble then
     -- 2. 该小程序是开源的, 每次write会自动分包为16字节
 
     sys.taskInit(function()
-        sys.wait(2000)
-        nimble.debug(6)
-        nimble.init()
+        sys.wait(2000) -- 为了能看到日志,休眠2秒
+        nimble.debug(6) -- 开启日志
+        nimble.init() -- 初始化nimble, 会产生事件BLE_STATE_INC
     end)
 else
+    -- 没有nimble, 那就闪灯吧
     sys.taskInit(function()
+        if not leds["a"] then
+            return
+        end
+        local count = 0
+        local uid = ""
+        if mcu then
+            uid = mcu.unique_id():toHex()
+        elseif nbiot then
+            uid = nbiot.imei()
+        end
+        log.warn("ble", "no nimble found, just led")
         while true do
+            leds["a"](count & 0x01 == 0x01 and 1 or 0)
+            leds["b"](count & 0x02 == 0x02 and 1 or 0)
+            leds["c"](count & 0x03 == 0x03 and 1 or 0)
+            log.info("gpio", "Go Go Go", uid, count)
             sys.wait(1000)
-            log.warn("ble", "this demo need nimble lib")
+            count = count + 1
         end
     end)
 end

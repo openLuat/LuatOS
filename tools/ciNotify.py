8f0a49347e85d5b4352b3e8fcbb700252a6770b6
chenxuuu <lolicon@papapoi.com>
2021-11-25 12:50:23 +0800

    add: ci报错加上最后一次commit的信息

--- tools/ciNotify.py
+++ tools/ciNotify.py
@@ -1,13 +1,15 @@
 import sys
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+from git import Repo
 
 client = mqtt.Client()
+repo = Repo("./")
 try:
     #服务器请自行修改，需要传入参数
     client.connect(sys.argv[1], int(sys.argv[2]), 60)
     #topic请根据需要自行修改，需要传入参数
-    pub = client.publish(sys.argv[3],sys.argv[4])
+    pub = client.publish(sys.argv[3],sys.argv[4]+"\r\n"+repo.head.commit.name+"-"+repo.head.commit.author.message)
     pub.wait_for_publish()
     client.disconnect()
     print("sent")

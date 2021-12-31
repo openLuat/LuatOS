e95014b26ee97add9f7957d7c9c336a12faad304
Dozingfiretruck <1473454180@qq.com>
2021-12-31 14:32:57 +0800

    add:u8g2添加屏幕旋转方向参数

--- luat/packages/u8g2/luat_u8g2.h
+++ luat/packages/u8g2/luat_u8g2.h
@@ -1,5 +1,6 @@
 
 #include "luat_base.h"
+#include "u8g2.h"
 
 typedef struct luat_u8g2_conf
 {
@@ -16,6 +17,7 @@ typedef struct luat_u8g2_conf
     size_t pin7;
     char* cname; // 控制器名称, 例如SSD1306
     void* ptr;
+    u8g2_cb_t* direction;//方向 
 } luat_u8g2_conf_t;
 
 int luat_u8g2_setup(luat_u8g2_conf_t *conf);

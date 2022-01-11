d9795841ac598936c76ad67bc144976e1bc4c2a7
Wendal Chen <wendal1985@gmail.com>
2022-01-11 15:57:12 +0800

    fix: rtt改API了,导致air640w编译报错, 提示缺方法, 修正之

--- bsp/air640w/rtt/rtconfig.h
+++ bsp/air640w/rtt/rtconfig.h
@@ -13,6 +13,7 @@
 #define RT_TICK_PER_SECOND 1000
 #define RT_USING_OVERFLOW_CHECK
 #define RT_USING_HOOK
+#define RT_HOOK_USING_FUNC_PTR
 #define RT_USING_IDLE_HOOK
 #define RT_IDLE_HOOK_LIST_SIZE 4
 #define IDLE_THREAD_STACK_SIZE 512
@@ -38,6 +39,7 @@
 
 #define RT_USING_MEMPOOL
 #define RT_USING_SMALL_MEM
+#define RT_USING_SMALL_MEM_AS_HEAP
 #define RT_USING_HEAP
 
 /* Kernel Device Object */
@@ -88,7 +90,6 @@
 /* Device Drivers */
 
 #define RT_USING_DEVICE_IPC
-#define RT_PIPE_BUFSZ 512
 #define RT_USING_SYSTEM_WORKQUEUE
 #define RT_SYSTEM_WORKQUEUE_STACKSIZE 2048
 #define RT_SYSTEM_WORKQUEUE_PRIORITY 23
@@ -96,143 +97,35 @@
 #define RT_USING_SERIAL_V1
 #define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 512
-#define RT_USING_HWTIMER
-#define RT_USING_I2C
-#define RT_USING_I2C_BITOPS
 #define RT_USING_PIN
-#define RT_USING_PWM
-#define RT_USING_RTC
-#define RT_USING_SOFT_RTC
-#define RT_USING_SPI
-#define RT_USING_WDT
-#define RT_USING_HWCRYPTO
-#define RT_HWCRYPTO_DEFAULT_NAME "hwcryto"
-#define RT_HWCRYPTO_IV_MAX_SIZE 16
-#define RT_HWCRYPTO_KEYBIT_MAX_SIZE 256
-#define RT_HWCRYPTO_USING_AES
-#define RT_HWCRYPTO_USING_AES_ECB
-#define RT_HWCRYPTO_USING_AES_CBC
-#define RT_HWCRYPTO_USING_AES_CTR
-#define RT_HWCRYPTO_USING_DES
-#define RT_HWCRYPTO_USING_DES_ECB
-#define RT_HWCRYPTO_USING_3DES
-#define RT_HWCRYPTO_USING_3DES_ECB
-#define RT_HWCRYPTO_USING_RC4
-#define RT_HWCRYPTO_USING_MD5
-#define RT_HWCRYPTO_USING_SHA1
-#define RT_HWCRYPTO_USING_RNG
-#define RT_HWCRYPTO_USING_CRC
-#define RT_HWCRYPTO_USING_CRC_07
-#define RT_HWCRYPTO_USING_CRC_8005
-#define RT_HWCRYPTO_USING_CRC_1021
-#define RT_HWCRYPTO_USING_CRC_04C11DB7
-#define RT_HWCRYPTO_USING_BIGNUM
-#define RT_HWCRYPTO_USING_BIGNUM_EXPTMOD
-#define RT_HWCRYPTO_USING_BIGNUM_MULMOD
-#define RT_USING_WIFI
-#define RT_WLAN_DEVICE_STA_NAME "wlan0"
-#define RT_WLAN_DEVICE_AP_NAME "wlan1"
-#define RT_WLAN_SSID_MAX_LENGTH 32
-#define RT_WLAN_PASSWORD_MAX_LENGTH 32
-#define RT_WLAN_DEV_EVENT_NUM 2
-#define RT_WLAN_MANAGE_ENABLE
-#define RT_WLAN_SCAN_WAIT_MS 10000
-#define RT_WLAN_CONNECT_WAIT_MS 10000
-#define RT_WLAN_SCAN_SORT
-#define RT_WLAN_MSH_CMD_ENABLE
-#define RT_WLAN_AUTO_CONNECT_ENABLE
-#define AUTO_CONNECTION_PERIOD_MS 2000
-#define RT_WLAN_CFG_ENABLE
-#define RT_WLAN_CFG_INFO_MAX 3
-#define RT_WLAN_PROT_ENABLE
-#define RT_WLAN_PROT_NAME_LEN 8
-#define RT_WLAN_PROT_MAX 2
-#define RT_WLAN_DEFAULT_PROT "lwip"
-#define RT_WLAN_PROT_LWIP_ENABLE
-#define RT_WLAN_PROT_LWIP_NAME "lwip"
-#define RT_WLAN_WORK_THREAD_ENABLE
-#define RT_WLAN_WORKQUEUE_THREAD_NAME "wlan_job"
-#define RT_WLAN_WORKQUEUE_THREAD_SIZE 2048
-#define RT_WLAN_WORKQUEUE_THREAD_PRIO 22
 
 /* Using USB */
 
 
 /* POSIX layer and C standard library */
 
-#define RT_USING_LIBC
-#define RT_LIBC_USING_TIME
 #define RT_LIBC_DEFAULT_TIMEZONE 8
 
 /* POSIX (Portable Operating System Interface) layer */
 
 #define RT_USING_POSIX_FS
 #define RT_USING_POSIX_POLL
-#define RT_USING_POSIX_SELECT
+
+/* Interprocess Communication (IPC) */
+
+
+/* Socket is in the 'Network' category */
 
 /* Network */
 
 /* Socket abstraction layer */
 
-#define RT_USING_SAL
-#define SAL_INTERNET_CHECK
-
-/* protocol stack implement */
-
-#define SAL_USING_LWIP
-#define SAL_USING_POSIX
 
 /* Network interface device */
 
-#define RT_USING_NETDEV
-#define NETDEV_USING_AUTO_DEFAULT
-#define NETDEV_IPV4 1
-#define NETDEV_IPV6 0
 
 /* light weight TCP/IP stack */
 
-#define RT_USING_LWIP
-#define RT_USING_LWIP202
-#define RT_LWIP_MEM_ALIGNMENT 4
-#define RT_LWIP_IGMP
-#define RT_LWIP_ICMP
-#define RT_LWIP_DNS
-#define RT_LWIP_DHCP
-#define IP_SOF_BROADCAST 1
-#define IP_SOF_BROADCAST_RECV 1
-
-/* Static IPv4 Address */
-
-#define RT_LWIP_IPADDR "192.168.1.30"
-#define RT_LWIP_GWADDR "192.168.1.1"
-#define RT_LWIP_MSKADDR "255.255.255.0"
-#define RT_LWIP_UDP
-#define RT_LWIP_TCP
-#define RT_LWIP_RAW
-#define RT_MEMP_NUM_NETCONN 8
-#define RT_LWIP_PBUF_NUM 16
-#define RT_LWIP_RAW_PCB_NUM 4
-#define RT_LWIP_UDP_PCB_NUM 4
-#define RT_LWIP_TCP_PCB_NUM 4
-#define RT_LWIP_TCP_SEG_NUM 40
-#define RT_LWIP_TCP_SND_BUF 8196
-#define RT_LWIP_TCP_WND 8196
-#define RT_LWIP_TCPTHREAD_PRIORITY 10
-#define RT_LWIP_TCPTHREAD_MBOX_SIZE 8
-#define RT_LWIP_TCPTHREAD_STACKSIZE 2048
-#define LWIP_NO_RX_THREAD
-#define LWIP_NO_TX_THREAD
-#define RT_LWIP_ETHTHREAD_PRIORITY 12
-#define RT_LWIP_ETHTHREAD_STACKSIZE 1024
-#define RT_LWIP_ETHTHREAD_MBOX_SIZE 8
-#define LWIP_NETIF_STATUS_CALLBACK 1
-#define LWIP_NETIF_LINK_CALLBACK 1
-#define SO_REUSE 1
-#define LWIP_SO_RCVTIMEO 1
-#define LWIP_SO_SNDTIMEO 1
-#define LWIP_SO_RCVBUF 1
-#define LWIP_SO_LINGER 0
-#define LWIP_NETIF_LOOPBACK 0
 
 /* AT commands */
 
@@ -242,9 +135,6 @@
 
 /* Utilities */
 
-#define RT_USING_RYM
-#define YMODEM_USING_CRC_TABLE
-#define YMODEM_USING_FILE_TRANSFER
 #define RT_USING_ULOG
 #define ULOG_OUTPUT_LVL_D
 #define ULOG_OUTPUT_LVL 7
@@ -273,16 +163,6 @@
 
 /* Wiced WiFi */
 
-#define PKG_USING_NETUTILS
-#define PKG_NETUTILS_NTP
-#define NTP_USING_AUTO_SYNC
-#define NTP_AUTO_SYNC_FIRST_DELAY 30
-#define NTP_AUTO_SYNC_PERIOD 3600
-#define NETUTILS_NTP_HOSTNAME "cn.ntp.org.cn"
-#define NETUTILS_NTP_HOSTNAME2 "ntp.rt-thread.org"
-#define NETUTILS_NTP_HOSTNAME3 "edu.ntp.org.cn"
-#define PKG_USING_NETUTILS_V131
-#define PKG_NETUTILS_VER_NUM 0x10301
 
 /* IoT Cloud */
 
@@ -301,12 +181,22 @@
 /* u8g2: a monochrome graphic library */
 
 
+/* PainterEngine: A cross-platform graphics application framework written in C language */
+
+
 /* tools packages */
 
 
 /* system packages */
 
+
+/* enhanced kernel services */
+
+
 /* acceleration: Assembly language or algorithmic acceleration packages */
+
+
+/* CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
 
 
 /* Micrium: Micrium software products porting for RT-Thread */
@@ -346,26 +236,6 @@
 #define BSP_USING_UART1
 #define WM_UART1_BAUDRATE 115200
 #define BSP_USING_PIN
-#define BSP_USING_WIFI
-#define BSP_USING_HWTIMER
-#define BSP_USING_PWM
-#define USING_PWM_CH1
-#define WM_PWM_CH1_PIN -1
-#define USING_PWM_CH2
-#define WM_PWM_CH2_PIN -1
-#define USING_PWM_CH3
-#define WM_PWM_CH3_PIN -1
-#define USING_PWM_CH4
-#define WM_PWM_CH4_PIN -1
-#define USING_PWM_CH5
-#define WM_PWM_CH5_PIN -1
-#define BSP_USING_I2C
-#define WM_HW_I2C_FREQ 200000
-#define BSP_USING_SPI
-#define WM_SPI_BUS_NAME "spi0"
-#define BSP_USING_WDT
-#define BSP_USING_CRYPTO
-#define BSP_USING_STANDBY
 
 /* External Libraries */
 

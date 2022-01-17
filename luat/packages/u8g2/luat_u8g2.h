b04e372ea03467055b44a9efd18117c1a252610c
Wendal Chen <wendal1985@gmail.com>
2022-01-18 00:09:02 +0800

    fix: make air302 happy, at u8g2 lib

--- luat/packages/u8g2/luat_u8g2.h
+++ luat/packages/u8g2/luat_u8g2.h
@@ -17,7 +17,7 @@ typedef struct luat_u8g2_conf
     size_t pin7;
     char* cname; // 控制器名称, 例如SSD1306
     void* ptr;
-    u8g2_cb_t* direction;//方向 
+    const u8g2_cb_t* direction;//方向 
 } luat_u8g2_conf_t;
 
 int luat_u8g2_setup(luat_u8g2_conf_t *conf);

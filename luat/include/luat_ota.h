f35789057cb89a9a657a8cf866f23561efd9b2e4
Dozingfiretruck <1473454180@qq.com>
2022-01-28 08:53:22 +0800

    add:自定义升级文件

--- luat/include/luat_ota.h
+++ luat/include/luat_ota.h
@@ -4,13 +4,17 @@
 #ifndef LUAT_OTA_H
 #define LUAT_OTA_H
 
+#ifndef UPDATE_TGZ_PATH
+#define UPDATE_TGZ_PATH "/update.tgz"
+#endif
+
+#ifndef UPDATE_BIN_PATH
 #define UPDATE_BIN_PATH "/update.bin"
+#endif
+
 #define ROLLBACK_MARK_PATH "/rollback_mark"
 #define UPDATE_MARK "/update_mark"
 #define FLASHX_PATH "/flashx.bin"
-
-#define UPDATE_TGZ_PATH "/update.tgz"
-#define UPDATE_BIN_PATH "/update.bin"
 
 #ifndef LUAT_OTA_MODE
 #define LUAT_OTA_MODE 1

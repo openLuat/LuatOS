55e1dc3ec0e71f8b28038742a079f7391eb0c8f6
Dozingfiretruck <1473454180@qq.com>
2022-03-15 23:30:49 +0800

    add:示例去掉无用代码

--- script/libs/bh1750/bh1750.lua
+++ script/libs/bh1750/bh1750.lua
@@ -13,7 +13,6 @@ i2c_speed = i2c.FAST
 sys.taskInit(function()
     i2c.setup(i2cid,i2c_speed)
     bh1750.init(i2cid)--初始化,传入i2c_id
-    bh1750.power_on()
     while 1 do
         local bh1750_data = bh1750.read_light()
         log.info("bh1750_read_light", bh1750_data)

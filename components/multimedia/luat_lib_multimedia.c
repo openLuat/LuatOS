ce6abc3abbe58630eb6b06877cbb604cf87d88e4
alienwalker <alienwalker@sina.com>
2022-03-16 10:36:02 +0800

    fix:mp3解码数据不正常

--- components/multimedia/luat_lib_multimedia.c
+++ components/multimedia/luat_lib_multimedia.c
@@ -278,7 +278,7 @@ static int l_codec_get_audio_data(lua_State *L) {
 			{
 				result = mp3dec_decode_frame(coder->mp3_decoder, in_buff->addr + pos, in_buff->used - pos, out_buff->addr + out_buff->used, &info);
 //				LLOGD("result %u,%u,%u,%u", result, info.frame_bytes, pos, coder->mp3_decoder->reserv);
-				out_buff->used += (result * info.channels);
+				out_buff->used += (result * info.channels * 2);
 				if (result)
 				{
 					pos += info.frame_bytes;

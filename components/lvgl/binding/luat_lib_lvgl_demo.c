67bfe282d647ada25ce3e1182c0ad72d8d5fa956
Dozingfiretruck <1473454180@qq.com>
2022-03-28 13:34:25 +0800

    add:整理一下lvgl demos默认不开启

--- components/lvgl/binding/luat_lib_lvgl_demo.c
+++ components/lvgl/binding/luat_lib_lvgl_demo.c
@@ -8,9 +8,59 @@
 #include "luat_base.h"
 #include "luat_lvgl.h"
 #include "lvgl.h"
+#include "../lv_demos/lv_ex_conf.h"
 #include "luat_malloc.h"
 
-int luat_lv_demo(lua_State *L) {
+int luat_lv_demo_benchmark(lua_State *L){
+#if LV_USE_DEMO_BENCHMARK
     lv_demo_benchmark();
-    return 1;
+#else
+    LLOGW("please defined LV_USE_DEMO_BENCHMARK");
+#endif
+    return 0;
+}
+
+int luat_lv_demo_keypad_encoder(lua_State *L){
+#if LV_USE_DEMO_KEYPAD_AND_ENCODER
+    lv_demo_keypad_encoder();
+#else
+    LLOGW("please defined LV_USE_DEMO_KEYPAD_AND_ENCODER");
+#endif
+    return 0;
+}
+
+int luat_lv_demo_music(lua_State *L){
+#if LV_USE_DEMO_MUSIC
+    lv_demo_music();
+#else
+    LLOGW("please defined LV_USE_DEMO_MUSIC");
+#endif
+    return 0;
+}
+
+int luat_lv_demo_printer(lua_State *L){
+#if LV_USE_DEMO_PRINTER
+    lv_demo_printer();
+#else
+    LLOGW("please defined LV_USE_DEMO_PRINTER");
+#endif
+    return 0;
+}
+
+int luat_lv_demo_stress(lua_State *L){
+#if LV_USE_DEMO_STRESS
+    lv_demo_stress();
+#else
+    LLOGW("please defined LV_USE_DEMO_STRESS");
+#endif
+    return 0;
+}
+
+int luat_lv_demo_widgets(lua_State *L){
+#if LV_USE_DEMO_WIDGETS
+    lv_demo_widgets();
+#else
+    LLOGW("please defined LV_USE_DEMO_WIDGETS");
+#endif
+    return 0;
 }
--- components/lvgl/binding/luat_lvgl_demo.h
+++ components/lvgl/binding/luat_lvgl_demo.h
@@ -5,8 +5,18 @@
 #include "luat_base.h"
 #include "lvgl.h"
 
-int luat_lv_demo(lua_State *L);
+int luat_lv_demo_benchmark(lua_State *L);
+int luat_lv_demo_keypad_encoder(lua_State *L);
+int luat_lv_demo_music(lua_State *L);
+int luat_lv_demo_printer(lua_State *L);
+int luat_lv_demo_stress(lua_State *L);
+int luat_lv_demo_widgets(lua_State *L);
 
-#define LUAT_LV_DEMO_RLT {"demo", ROREG_FUNC(luat_lv_demo)},\
+#define LUAT_LV_DEMO_RLT {"demo_benchmark", ROREG_FUNC(luat_lv_demo_benchmark)},\
+{"demo_keypad_encoder", ROREG_FUNC(luat_lv_demo_keypad_encoder)},\
+{"demo_music", ROREG_FUNC(luat_lv_demo_music)},\
+{"demo_printer", ROREG_FUNC(luat_lv_demo_printer)},\
+{"demo_stress", ROREG_FUNC(luat_lv_demo_stress)},\
+{"demo_widgets", ROREG_FUNC(luat_lv_demo_widgets)},\
 
 #endif

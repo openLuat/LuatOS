fe4682ebd1dd3f3ac668f7cb1efa156124f8947f
Dozingfiretruck <1473454180@qq.com>
2022-04-28 00:15:49 +0800

    add:配合sysplus.lua改动

--- demo/network/Air105/lan_test.lua
+++ demo/network/Air105/lan_test.lua
@@ -1,12 +1,12 @@
 local libnet = require "libnet"
-
+local sysplus = require "sysplus"
 
 --下面演示用阻塞方式做串口透传远程服务器，简单的串口DTU
 local d1Online = false
 local com_buff = zbuff.create(4096)
 local d1Name = "D1_TASK"
 local function dtu_cb(msg)
-	sys.info("未处理消息", msg[1], msg[2], msg[3], msg[4])
+	log.info("未处理消息", msg[1], msg[2], msg[3], msg[4])
 end
 
 local function demo_dtu(uart_id)
@@ -26,16 +26,16 @@ local function demo_dtu(uart_id)
 	end)
 	while true do
 		result = libnet.waitLink(d1Name, 0, netc)
-		result = libnet.connect(d1Name, 5000, netc, "10.0.0.3", 12000)
-		sys.info(rtos.meminfo("sys"))
+		result = libnet.connect(d1Name, 5000, netc, "112.125.89.8", 37518)
+		log.info(rtos.meminfo("sys"))
 		d1Online = result
 		if result then
-			sys.info("服务器连上了")
+			log.info("服务器连上了")
 		end
 		while result do
 			is_err, param, ip, port = network.rx(netc, rx_buff)
 			if is_err then
-				sys.info("服务器断开了", is_err, param, ip, port)
+				log.info("服务器断开了", is_err, param, ip, port)
 				break
 			end
 			if rx_buff:used() > 0 then
@@ -48,7 +48,7 @@ local function demo_dtu(uart_id)
 				result, param = libnet.tx(d1Name, 5000, netc, tx_buff)
 			end
 			if not result then
-				sys.info("发送失败了", result, param)
+				log.info("发送失败了", result, param)
 				break
 			end
 			tx_buff:del()
@@ -61,20 +61,20 @@ local function demo_dtu(uart_id)
 			if rx_buff:len() > 10240 then
 				rx_buff:resize(10240)
 			end
-			sys.info(rtos.meminfo("sys"))
+			log.info(rtos.meminfo("sys"))
 			result, param = libnet.wait(d1Name, 5000, netc)
 			if not result then
-				sys.info("服务器断开了", result, param)
+				log.info("服务器断开了", result, param)
 				break
 			end
 		end
 		d1Online = false
 		libnet.close(d1Name, 5000, netc)
-		sys.info(rtos.meminfo("sys"))
+		log.info(rtos.meminfo("sys"))
 		sys.wait(1000)
 	end
 end
 
 function demo1(uart_id)
-	sys.taskInitV2(demo_dtu, d1Name, dtu_cb, uart_id)
+	sysplus.taskInitEx(demo_dtu, d1Name, dtu_cb, uart_id)
 end

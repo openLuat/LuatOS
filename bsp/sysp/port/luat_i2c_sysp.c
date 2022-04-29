2f1a8f541aecfcd29126ce375937a34c4c5dce5c
Wendal Chen <wendal1985@gmail.com>
2022-04-29 13:32:47 +0800

    fix: 修正bsp/sysp编译错误

--- bsp/sysp/port/luat_i2c_sysp.c
+++ bsp/sysp/port/luat_i2c_sysp.c
@@ -10,7 +10,7 @@ int luat_i2c_setup(int id, int speed, int slaveaddr) {
 int luat_i2c_close(int id) {
     return 0;
 }
-int luat_i2c_send(int id, int addr, void* buff, size_t len) {
+int luat_i2c_send(int id, int addr, void* buff, size_t len, uint8_t stop) {
     return 0;
 }
 int luat_i2c_recv(int id, int addr, void* buff, size_t len) {

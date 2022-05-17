4086086132400fca17947f91f4289a8f5f47e1d9
hanxiang <pojdd@qq.com>
2022-05-17 17:27:28 +0800

    修复D1在开启O3优化后无法翻页的问题，支持在luat_conf_bsp.h中定义宏LV_IMG_CACHE_DEF_SIZE。

--- components/lvgl/lv_conf.h
+++ components/lvgl/lv_conf.h
@@ -270,7 +270,9 @@ typedef void * lv_fs_drv_user_data_t;
  * With complex image decoders (e.g. PNG or JPG) caching can save the continuous open/decode of images.
  * However the opened images might consume additional RAM.
  * Set it to 0 to disable caching */
+#ifndef LV_IMG_CACHE_DEF_SIZE
 #define LV_IMG_CACHE_DEF_SIZE       1
+#endif
 
 /*Declare the type of the user data of image decoder (can be e.g. `void *`, `int`, `struct`)*/
 typedef void * lv_img_decoder_user_data_t;

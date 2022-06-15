a9232aee78d2c93dfe2a62769ab7c3adddfbadbc
Dozingfiretruck <1473454180@qq.com>
2022-06-15 15:37:16 +0800

    add:更新版本号

--- bsp/win32/include/luat_conf_bsp.h
+++ bsp/win32/include/luat_conf_bsp.h
@@ -2,7 +2,7 @@
 #ifndef LUAT_CONF_BSP
 #define LUAT_CONF_BSP
 
-#define LUAT_BSP_VERSION "V0002"
+#define LUAT_BSP_VERSION "V0003"
 #define LUAT_USE_CMDLINE_ARGS 1
 // 启用64位虚拟机
 // #define LUAT_CONF_VM_64bit

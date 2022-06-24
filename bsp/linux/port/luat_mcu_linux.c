558d7dbc025803300a08081307e77c269a435cfe
Dozingfiretruck <1473454180@qq.com>
2022-06-24 23:01:53 +0800

    fix:名写错了...

--- bsp/linux/port/luat_mcu_linux.c
+++ bsp/linux/port/luat_mcu_linux.c
@@ -1,7 +1,8 @@
 #include "luat_base.h"
 #include "luat_mcu.h"
 #include "stdio.h"
+#include "time.h"
 
 long luat_mcu_ticks(void) {
-    return clock()*1000/CLOCKS_PRE_SEC;
+    return clock()*1000/CLOCKS_PER_SEC;
 }

16c02ba339a96b848b79b4378ab470d26a8cf62a
Dozingfiretruck <1473454180@qq.com>
2022-07-08 20:19:34 +0800

    add:添加接线注释

--- script/libs/mcp2515/mcp2515.lua
+++ script/libs/mcp2515/mcp2515.lua
@@ -9,6 +9,13 @@
 -- 用法实例
 local mcp2515 = require "mcp2515"
 
+-- mcp2515    mcu
+-- csk      spi_sck
+-- si       spi_mosi
+-- so       spi_miso
+-- cs       spi_cs
+-- int      gpio
+
 sys.subscribe("mcp2515", function(len,buff,config)
     print("mcp2515", len,buff:byte(1,len))
     for k, v in pairs(config) do
@@ -20,17 +27,14 @@ sys.taskInit(function()
     local mcp2515_spi= 0
     local mcp2515_cs= pin.PB04
     local mcp2515_int= pin.PB01
-    spi_mcp2515 = spi.setup(mcp2515_spi,nil,0,0,8,20*1000*1000,spi.MSB,1,0)
+    spi_mcp2515 = spi.setup(mcp2515_spi,nil,0,0,8,10*1000*1000,spi.MSB,1,0)
     mcp2515.init(mcp2515_spi,mcp2515_cs,mcp2515_int,mcp2515.CAN_500Kbps)
 
     mcp2515.send_buffer({id = 0x7FF,ide = false,rtr = false},0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07)--标准帧,数据帧
     mcp2515.send_buffer({id = 0x1FFFFFE6,ide = true,rtr = false},0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07)--扩展帧,数据帧
     mcp2515.send_buffer({id = 0x7FF,ide = false,rtr = true},0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07)--标准帧,远程帧
     mcp2515.send_buffer({id = 0x1FFFFFE6,ide = true,rtr = true},0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07)--扩展帧,远程帧
-    
-    while 1 do
-        sys.wait(1000)
-    end
+
 end)
 ]]
 

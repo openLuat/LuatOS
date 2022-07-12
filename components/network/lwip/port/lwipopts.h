fdae3af1fa7c9055449194661ce139a5b82aa1ee
alienwalker <alienwalker@sina.com>
2022-07-12 09:30:18 +0800

    fix:DAC用16bit数据，所以数据长度需要减半

--- components/network/lwip/port/lwipopts.h
+++ components/network/lwip/port/lwipopts.h
@@ -16,7 +16,7 @@
 #define SYS_LIGHTWEIGHT_PROT            1	//平台锁，保护关键区域内缓存的分配与释放，目前lwip不允许放在中断处理，不需要额外的平台锁
 
 //mem管理
-#define MEMP_MEM_MALLOC 0
+#define MEMP_MEM_MALLOC 1
 #define MEM_LIBC_MALLOC 1
 #define mem_clib_free luat_heap_free
 #define mem_clib_malloc luat_heap_malloc
@@ -202,7 +202,7 @@
 #define MLD6_STATS                      (LWIP_IPV6 && LWIP_IPV6_MLD)
 #define ND6_STATS                       (LWIP_IPV6)
 #define MIB2_STATS                      0
-#define LWIP_CHECKSUM_CTRL_PER_NETIF    0
+#define LWIP_CHECKSUM_CTRL_PER_NETIF    1
 #define CHECKSUM_GEN_IP                 1
 #define CHECKSUM_GEN_UDP                1
 #define CHECKSUM_GEN_TCP                1
@@ -253,7 +253,7 @@
 #define LWIP_DHCP6_GET_NTP_SRV          0
 #define LWIP_DHCP6_MAX_NTP_SERVERS      0
 #define LWIP_DHCP6_MAX_DNS_SERVERS      DNS_MAX_SERVERS
-
+//#define LWIP_USING_NAT
 /* TODO: check hooks */
 
 #define LWIP_DBG_MIN_LEVEL              LWIP_DBG_LEVEL_ALL

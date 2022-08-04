060ca0f57cdb3b7ae453d92a1710105dcd354487
Wendal Chen <wendal1985@gmail.com>
2022-08-04 18:20:10 +0800

    fix: lvgl的lv_font_glyph_dsc_t一定要填0

--- components/lvgl/src/lv_font/lv_font.c
+++ components/lvgl/src/lv_font/lv_font.c
@@ -73,7 +73,7 @@ bool lv_font_get_glyph_dsc(const lv_font_t * font_p, lv_font_glyph_dsc_t * dsc_o
  */
 uint16_t lv_font_get_glyph_width(const lv_font_t * font, uint32_t letter, uint32_t letter_next)
 {
-    lv_font_glyph_dsc_t g;
+    lv_font_glyph_dsc_t g = {0};
     bool ret;
     ret = lv_font_get_glyph_dsc(font, &g, letter, letter_next);
     if(ret) return g.adv_w;

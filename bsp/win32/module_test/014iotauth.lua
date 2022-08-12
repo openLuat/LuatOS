af3a81e7444c479d1b6f46429dbff5aa1af2d561
Dozingfiretruck <1473454180@qq.com>
2022-08-12 15:22:43 +0800

    add:添加iotauth测试

--- bsp/win32/module_test/014iotauth.lua
+++ bsp/win32/module_test/014iotauth.lua
@@ -12,10 +12,23 @@ print(_VERSION)
 
 sys.taskInit(function()
     assert(iotauth, "iotauth exist")
-    local clientid, user, passwd = iotauth.onenet("qDPGh8t81z", "45463968338A185E", "MTIzNDU2")
+    local client_id,user_name,password = iotauth.onenet("qDPGh8t81z", "45463968338A185E", "MTIzNDU2")
     log.info("onenet", clientid, user, passwd)
 
+    local client_id,user_name,password = iotauth.iotda("6203cc94c7fb24029b110408_88888888","123456789")
+    log.info("iotda",client_id,user_name,password)
 
+    local client_id,user_name,password = iotauth.tuya(" 6c95875d0f5ba69607nzfl","fb803786602df760")
+    log.info("tuya",client_id,user_name,password)
+
+    local client_id,user_name,password = iotauth.baidu("abcd123","mydevice","ImSeCrEt0I1M2jkl")
+    log.info("baidu",client_id,user_name,password)
+
+    local client_id,user_name,password = iotauth.qcloud("LD8S5J1L07","test","acyv3QDJrRa0fW5UE58KnQ==")
+    log.info("qcloud",client_id,user_name,password)
+
+    local client_id,user_name,password = iotauth.aliyun("123456789","abcdefg","Y877Bgo8X5owd3lcB5wWDjryNPoB")
+    log.info("aliyun",client_id,user_name,password)
 
 
     

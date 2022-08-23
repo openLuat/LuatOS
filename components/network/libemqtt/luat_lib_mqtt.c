8fb97177c6317d13ab6db23085b2aeb4556d839c
Wendal Chen <wendal1985@gmail.com>
2022-08-23 16:10:28 +0800

    update: 审阅lib_mqtt后给出修改意见

--- components/network/libemqtt/luat_lib_mqtt.c
+++ components/network/libemqtt/luat_lib_mqtt.c
@@ -5,22 +5,25 @@
 #include "libemqtt.h"
 #include "luat_rtos.h"
 #include "luat_zbuff.h"
+
 #define LUAT_LOG_TAG "mqtt"
 #include "luat_log.h"
 
 #define MQTT_RECV_BUF_LEN_MAX 4096
 typedef struct
 {
-	mqtt_broker_handle_t *broker;
+	mqtt_broker_handle_t *broker; // TODO 这里没必要分开malloc
 	network_ctrl_t *netc;
 	luat_ip_addr_t *ip_addr;
-	const char *ip;
-	uint8_t mqtt_packet_buffer[MQTT_RECV_BUF_LEN_MAX];
-	uint8_t mqtt_id;
-	uint16_t remote_port;
-	uint32_t keepalive;
-	uint8_t adapter_index;
-	uint8_t mqtt_state;
+	const char *ip; // 这是host?
+	uint16_t buffer_offset; // 用于标识mqtt_packet_buffer当前有多少数据
+	uint8_t mqtt_packet_buffer[MQTT_RECV_BUF_LEN_MAX + 4];
+	uint8_t mqtt_id; // 对应mqtt_cbs的索引, TODO, 既然要存function的ref_id, 为啥不直接存呢
+	uint16_t remote_port; // 远程端口号
+	uint32_t keepalive;   // 心跳时长
+	uint8_t adapter_index; // 适配器索引号, 似乎并没有什么用
+	uint8_t mqtt_state;    // mqtt状态
+	// TODO 记录最后一次数据交互的时间,方便判断是否真的发送ping请求
 }luat_mqtt_ctrl_t;
 
 #define MAX_MQTT_COUNT 32
@@ -81,27 +84,87 @@ static int packet_parse(luat_mqtt_ctrl_t *mqtt_ctrl,uint8_t *buff_in,uint32_t rx
 	return ret;
 }
 
+static int mqtt_parse(luat_mqtt_ctrl_t *mqtt_ctrl) {
+	if (mqtt_ctrl->buffer_offset < 2) {
+		LLOGD("wait more data");
+		return 0;
+	}
+	mqtt_ctrl->mqtt_packet_buffer[mqtt_ctrl->buffer_offset] = 0x00;
+	// 判断数据长度, 前几个字节能判断出够不够读出mqtt的头
+	uint8_t rem_len_bytes = mqtt_num_rem_len_bytes(mqtt_ctrl->mqtt_packet_buffer);
+	if (rem_len_bytes > mqtt_ctrl->buffer_offset - 1) {
+		LLOGD("wait more data for mqtt head");
+		return 0;
+	}
+	// 判断数据总长, 这里rem_len只包含mqtt头部之外的数据
+	uint16_t rem_len = mqtt_parse_rem_len(mqtt_ctrl->mqtt_packet_buffer);
+	if (rem_len > mqtt_ctrl->buffer_offset - rem_len_bytes - 1) {
+		LLOGD("wait more data for mqtt head");
+		return 0;
+	}
+
+	// 至此, mqtt包是完整的
+
+	// TODO 解析类型, 处理之
+	mqtt_msg_cb(mqtt_ctrl);
+
+	// 处理完成后, 如果还有数据, 移动数据, 继续处理
+	mqtt_ctrl->buffer_offset -= (1 + rem_len_bytes + rem_len);
+	return 0;
+}
+
 static int mqtt_read_packet(luat_mqtt_ctrl_t *mqtt_ctrl){
 	int ret = -1;
-	uint8_t *read_buff;
-	uint32_t total_len;
-	uint32_t rx_len;
-	memset(mqtt_ctrl->mqtt_packet_buffer, 0, MQTT_RECV_BUF_LEN_MAX);
+	uint8_t *read_buff = NULL;
+	uint32_t total_len = 0;
+	uint32_t rx_len = 0;
+	// 这里获取长度的逻辑貌似没有用了, 还是说network需要先读一次呢?
 	int result = network_rx(mqtt_ctrl->netc, NULL, 0, 0, NULL, NULL, &total_len);
-	if (total_len){
-		read_buff = (uint8_t *)luat_heap_malloc(total_len);
+	if (total_len > 0xFFF) {
+		LLOGE("too many data wait for recv %d", total_len);
+		// TODO close socket, clean up mqtt context
+		return 0;
 	}
-	result = network_rx(mqtt_ctrl->netc, read_buff, total_len, 0, NULL, NULL, &rx_len);
-	if (rx_len>=2){
-		uint16_t rem_len = mqtt_parse_rem_len(read_buff);
-		uint8_t rem_len_bytes = mqtt_num_rem_len_bytes(read_buff);
-		LLOGD("mqtt_read_packet rem_len:%d rem_len_bytes:%d rx_len:%d",rem_len,rem_len_bytes,rx_len);
-		if(rem_len+rem_len_bytes+1<=rx_len){
-			ret = packet_parse(mqtt_ctrl,read_buff,rx_len);
+	if (total_len == 0) {
+		LLOGD("rx event but NO data wait for recv");
+		return 0;
+	}
+	if (MQTT_RECV_BUF_LEN_MAX - mqtt_ctrl->buffer_offset) {
+		LLOGD("buff is FULL, mqtt packet too big");
+		// TODO 关闭socket
+		return 0;
+	}
+	#define MAX_READ (1024)
+	int recv_want = 0;
+
+	// 读取数据, 直至没有数据可读
+	while (MQTT_RECV_BUF_LEN_MAX - mqtt_ctrl->buffer_offset > 0) {
+		if (MAX_READ > (MQTT_RECV_BUF_LEN_MAX - mqtt_ctrl->buffer_offset)) {
+			recv_want = MQTT_RECV_BUF_LEN_MAX - mqtt_ctrl->buffer_offset;
+		}
+		else {
+			// 
+			recv_want = MAX_READ;
+		}
+		// 从网络接收数据
+		result = network_rx(mqtt_ctrl->netc, mqtt_ctrl->mqtt_packet_buffer + mqtt_ctrl->buffer_offset, recv_want, 0, NULL, NULL, &rx_len);
+		// TODO 判断 result, 虽然通常没问题
+		if (rx_len == 0) {
+			break;
+		}
+		// 收到数据了, 传给处理函数继续处理
+		// 数据的长度变更, 触发传递
+		mqtt_ctrl->buffer_offset += rx_len;
+		result = mqtt_parse(mqtt_ctrl);
+		if (result == 0) {
+			// 处理OK
+		}
+		else {
+			// TODO 数据肯定有问题,关闭socket
+			break;
 		}
 	}
-	luat_heap_free(read_buff);
-	return ret;
+	return 0;
 }
 
 static int32_t l_mqtt_callback(lua_State *L, void* ptr){
@@ -148,6 +211,7 @@ static int32_t l_mqtt_callback(lua_State *L, void* ptr){
 				if (lua_isfunction(L, -1)) {
 					lua_pushlightuserdata(L, mqtt_ctrl);
 					lua_pushstring(L, "sent");
+					// TODO 需要解出pkgid,作为参数传过去
 					lua_call(L, 2, 0);
 				}
             }
@@ -164,7 +228,7 @@ static int32_t l_mqtt_callback(lua_State *L, void* ptr){
 
 
 static int mqtt_msg_cb(luat_mqtt_ctrl_t *mqtt_ctrl) {
-	rtos_msg_t msg;
+	rtos_msg_t msg = {0};
     msg.handler = l_mqtt_callback;
     uint8_t msg_tp = MQTTParseMessageType(mqtt_ctrl->mqtt_packet_buffer);
     switch (msg_tp) {
@@ -281,8 +345,10 @@ static int32_t luat_lib_mqtt_callback(void *data, void *param){
 
 static int mqtt_send_packet(void* socket_info, const void* buf, unsigned int count){
     luat_mqtt_ctrl_t * mqtt_ctrl = (luat_mqtt_ctrl_t *)socket_info;
-	uint32_t tx_len;
+	uint32_t tx_len = 0;
 	network_tx(mqtt_ctrl->netc, buf, count, 0, mqtt_ctrl->ip_addr->is_ipv6?NULL:&(mqtt_ctrl->ip_addr), NULL, &tx_len, 0);
+	// TODO 判断network_tx的返回值
+	return 0;
 }
 
 static int luat_socket_connect(luat_mqtt_ctrl_t *mqtt_ctrl, const char *hostname, uint16_t port, uint16_t keepalive){
@@ -297,7 +363,7 @@ static int luat_socket_connect(luat_mqtt_ctrl_t *mqtt_ctrl, const char *hostname
 }
 
 static int l_mqtt_subscribe(lua_State *L) {
-	size_t len;
+	size_t len = 0;
 	luat_mqtt_ctrl_t * mqtt_ctrl = (luat_mqtt_ctrl_t *)lua_touserdata(L, 1);
 	const char * topic = luaL_checklstring(L, 2, &len);
 	uint8_t qos = luaL_optinteger(L, 3, 0);
@@ -306,7 +372,7 @@ static int l_mqtt_subscribe(lua_State *L) {
 }
 
 static int l_mqtt_unsubscribe(lua_State *L) {
-	size_t len;
+	size_t len = 0;
 	luat_mqtt_ctrl_t * mqtt_ctrl = (luat_mqtt_ctrl_t *)lua_touserdata(L, 1);
 	const char * topic = luaL_checklstring(L, 2, &len);
 	int subscribe_state = mqtt_unsubscribe(mqtt_ctrl->broker, topic, NULL);
@@ -317,14 +383,15 @@ static int l_mqtt_unsubscribe(lua_State *L) {
 static int l_mqtt_create(lua_State *L) {
 	int adapter_index = luaL_optinteger(L, 1, network_get_last_register_adapter());
 	if (adapter_index < 0 || adapter_index >= NW_ADAPTER_QTY){
-		lua_pushnil(L);
-		return 1;
+		// lua_pushnil(L);
+		return 0;
 	}
 	luat_mqtt_ctrl_t *mqtt_ctrl = (luat_mqtt_ctrl_t *)lua_newuserdata(L, sizeof(luat_mqtt_ctrl_t));
 	if (!mqtt_ctrl){
-		lua_pushnil(L);
-		return 1;
+		// lua_pushnil(L);
+		return 0;
 	}
+	memset(mqtt_ctrl, 0, sizeof(luat_mqtt_ctrl_t));
 
 	mqtt_ctrl->adapter_index = adapter_index;
 	mqtt_ctrl->netc = network_alloc_ctrl(adapter_index);
@@ -346,12 +413,12 @@ static int l_mqtt_create(lua_State *L) {
 	//后面再加tls
 	network_deinit_tls(mqtt_ctrl->netc);
 
-	int packet_length;
-	uint16_t msg_id, msg_id_rcv;
+	int packet_length = 0;
+	uint16_t msg_id = 0, msg_id_rcv = 0;
 	mqtt_ctrl->broker = (mqtt_broker_handle_t *)luat_heap_malloc(sizeof(mqtt_broker_handle_t));
 	
 	const char *ip;
-	size_t ip_len;
+	size_t ip_len = 0;
 
 	mqtt_ctrl->ip_addr = (luat_ip_addr_t *)luat_heap_malloc(sizeof(luat_ip_addr_t));
 	mqtt_ctrl->ip_addr->is_ipv6 = 0xff;
@@ -425,21 +492,22 @@ static int l_mqtt_connect(lua_State *L) {
 }
 
 static int l_mqtt_publish(lua_State *L) {
-	uint16_t message_id;
+	uint16_t message_id = 0;
 	luat_mqtt_ctrl_t * mqtt_ctrl = get_mqtt_ctrl(L);
 	const char * topic = luaL_checkstring(L, 2);
 	const char * payload = luaL_checkstring(L, 3);
 	uint8_t qos = luaL_optinteger(L, 4, 0);
 	uint8_t retain = luaL_optinteger(L, 5, 0);
-	mqtt_publish_with_qos(mqtt_ctrl->broker, topic, payload, retain, qos, message_id);
-	if (qos==0){
-		rtos_msg_t msg;
+	mqtt_publish_with_qos(mqtt_ctrl->broker, topic, payload, retain, qos, &message_id);
+	// TODO 判断一下mqtt_publish_with_qos的返回值哦
+	if (qos == 0){
+		rtos_msg_t msg = {0};
     	msg.handler = l_mqtt_callback;
 		msg.ptr = mqtt_ctrl;
 		msg.arg1 = MQTT_MSG_PUBACK;
 		luat_msgbus_put(&msg, 0);
 	}
-	lua_pushinteger(L,message_id);
+	lua_pushinteger(L, message_id);
 	return 1;
 }
 

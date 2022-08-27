7fc4cc638eaf48a23f5d23af30a67ee966495731
Wendal Chen <wendal1985@gmail.com>
2022-08-27 11:56:55 +0800

    remove: components/lvgl_v8 先移除了,以后再考虑吧

--- components/lvgl_v8/binding/luat_lvgl_img_ext.h
+++
@@ -1,9 +0,0 @@
-#include "luat_base.h"
-#include "luat_msgbus.h"
-#include "luat_lvgl.h"
-
-int luat_lv_img_set_src(lua_State *L);
-
-#define LUAT_LV_IMG_EX_RTL {"img_set_src", luat_lv_img_set_src, 0},\
-
-
--- components/lvgl_v8/binding/luat_lvgl_userdata.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef LUAT_LVGL_USERDATA
-#define LUAT_LVGL_USERDATA
-
-#include "lvgl.h"
-void luat_lv_user_data_free(lv_obj_t * obj);
-
-#endif

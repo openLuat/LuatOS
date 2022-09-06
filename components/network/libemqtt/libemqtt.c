9e8fb6b5cafd38887186e35e359f88908f78da7a
Dozingfiretruck <1473454180@qq.com>
2022-09-06 17:48:26 +0800

    fix:防止信息\0截止的问题

--- components/network/libemqtt/libemqtt.c
+++ components/network/libemqtt/libemqtt.c
@@ -309,13 +309,13 @@ int mqtt_ping(mqtt_broker_handle_t* broker) {
 	return 1;
 }
 
-int mqtt_publish(mqtt_broker_handle_t* broker, const char* topic, const char* msg, uint8_t retain) {
-	return mqtt_publish_with_qos(broker, topic, msg, retain, 0, NULL);
+int mqtt_publish(mqtt_broker_handle_t* broker, const char* topic, const char* msg, uint16_t msg_len, uint8_t retain) {
+	return mqtt_publish_with_qos(broker, topic, msg, msg_len, retain, 0, NULL);
 }
 
-int mqtt_publish_with_qos(mqtt_broker_handle_t* broker, const char* topic, const char* msg, uint8_t retain, uint8_t qos, uint16_t* message_id) {
+int mqtt_publish_with_qos(mqtt_broker_handle_t* broker, const char* topic, const char* msg, uint16_t msg_len, uint8_t retain, uint8_t qos, uint16_t* message_id) {
 	uint16_t topiclen = strlen(topic);
-	uint16_t msglen = strlen(msg);
+	uint16_t msglen = msg_len;
 
 	uint8_t qos_flag = MQTT_QOS0_FLAG;
 	uint8_t qos_size = 0; // No QoS included

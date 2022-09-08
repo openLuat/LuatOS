ee8600f7c0caaff58f80cc11ef486f1f39b150c9
Dozingfiretruck <1473454180@qq.com>
2022-09-08 18:20:32 +0800

    del:去掉luaopen_http2

--- luat/include/luat_libs.h
+++ luat/include/luat_libs.h
@@ -51,7 +51,6 @@ LUAMOD_API int luaopen_sfud( lua_State *L );
 LUAMOD_API int luaopen_mqtt( lua_State *L );
 /** http库*/
 LUAMOD_API int luaopen_http( lua_State *L );
-LUAMOD_API int luaopen_http2( lua_State *L );
 /** pack库*/
 LUAMOD_API int luaopen_pack( lua_State *L );
 /** mqttcore库*/

068d7b5cd0416a8b1f5f423149cc7d7704c9e73d
Dozingfiretruck <1473454180@qq.com>
2022-09-23 14:16:28 +0800

    fix:修复注释错误

--- components/miniz/luat_lib_miniz.c
+++ components/miniz/luat_lib_miniz.c
@@ -15,7 +15,7 @@ if cdata then
     log.info("miniz", "cdata as hex", cdata:toHex())
 
     -- 解压, 得到原文
-    local udata = miniz.compress(cdata)
+    local udata = miniz.uncompress(cdata)
     log.info("miniz", "udata", udata)
 end
 */
@@ -80,7 +80,7 @@ if cdata then
     log.info("miniz", "before", #bigdata, "after", #cdata)
     log.info("miniz", "cdata as hex", cdata:toHex())
 
-    local udata = miniz.compress(cdata)
+    local udata = miniz.uncompress(cdata)
     log.info("miniz", "udata", udata)
 end
 */

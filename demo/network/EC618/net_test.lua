c0f0c778c3254f8cac846f2d16bf89400de4dcff
alienwalker <alienwalker@sina.com>
2022-11-11 21:23:06 +0800

    update:network的demo换成EC618

--- demo/network/EC618/net_test.lua
+++ demo/network/EC618/net_test.lua
@@ -1,5 +1,6 @@
-network.setDNS(nil,1,"223.5.5.5")
-network.setDNS(nil,2,"114.114.114.114")
+-- 如果运营商自带的DNS不好用，可以用下面的公用DNS
+-- network.setDNS(nil,1,"223.5.5.5")	
+-- network.setDNS(nil,2,"114.114.114.114")
 require "dtu_demo"
 dtuDemo(2, "112.125.89.8", 37553)
 require "ntp_demo"

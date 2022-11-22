357c5618615f1900f39f21bdfad0a8bf53da7603
liyuanlong <1398051498@qq.com>
2022-11-22 11:44:40 +0800

    add:增加了一些注释和日志

--- demo/socket/EC618/mqtt_demo.lua
+++ demo/socket/EC618/mqtt_demo.lua
@@ -1,56 +1,55 @@
 local libnet = require "libnet"
 
-
+--根据自己的服务器修改以下参数
 local mqtt_host = "lbsmqtt.airm2m.com"
-local mqtt_port = 1883
-local client_id = "123456"
-local user_name = "username"
+local mqtt_port = 1884
+local mqtt_isssl = false
+local client_id = "abc"
+local user_name = "user"
 local password = "password"
 
 local mqttc = nil
 
-
-function recv(topic, payload)
-    local tjson, r = json.decode(payload)
-    log.info("result", r, ",tjson", tjson)
-    if r then
-        log.info("topic", topic)
-        for k, v in pairs(tjson) do
-            log.info("key & value", k, v)
-        end
-    end
-end
+-- function recv(topic, payload)
+--     local tjson, r = json.decode(payload)
+--     log.info("result", r, ",tjson", tjson)
+--     if r then
+--         log.info("topic", topic)
+--         for k, v in pairs(tjson) do
+--             log.info("key & value", k, v)
+--         end
+--     end
+-- end
 
 sys.taskInit(function()
 	sys.wait(3000)
-    mqttc = mqtt.create(nil,mqtt_host, mqtt_port,false)
 
-    mqttc:auth(client_id,user_name,password)    -- mqtt三元组配置
-    mqttc:keepalive(30)     -- 默认值240s
-    mqttc:autoreconn(true, 3000)    -- 自动重连机制
+    mqttc = mqtt.create(nil,mqtt_host, mqtt_port,mqtt_isssl)  --mqtt客户端创建
 
-    mqttc:on(function(mqtt_client, event, topic, payload)
+    mqttc:auth(client_id,user_name,password) --mqtt三元组配置
+    mqttc:keepalive(30) -- 默认值240s
+    mqttc:autoreconn(true, 3000) -- 自动重连机制
+
+    mqttc:on(function(mqtt_client, event, data, payload)  --mqtt回调注册
         -- 用户自定义代码
-        log.info("mqtt", "event", event, mqtt_client, topic, payload)
-        if event == "conack" then       -- 连接事件
+        log.info("mqtt", "event", event, mqtt_client, data, payload)
+        if event == "conack" then
             sys.publish("mqtt_conack")
             mqtt_client:subscribe("/luatos/123456")
-        elseif event == "recv" then     -- 接收事件
-            recv(topic, payload)
-            log.info("mqtt", "downlink", "topic", topic, "payload", payload)
-        elseif event == "sent" then     -- 发送事件
-            log.info("mqtt", "sent", "pkgid", topic)
+        elseif event == "recv" then
+            log.info("mqtt", "downlink", "topic", data, "payload", payload)
+        elseif event == "sent" then
+            log.info("mqtt", "sent", "pkgid", data)
         end
     end)
 
     mqttc:connect()
-
+    log.info("mqtt连接成功")
 	sys.waitUntil("mqtt_conack")
     while true do
         -- mqttc自动处理重连
         local ret, topic, data, qos = sys.waitUntil("mqtt_pub", 30000)
         if ret then
-            log.info("mqtt-topic_data_qos",ret, topic, data, qos)
             if topic == "close" then break end
             mqttc:publish(topic, data, qos)
         end
@@ -59,15 +58,16 @@ sys.taskInit(function()
     mqttc = nil
 end)
 
--- publish消息
 sys.taskInit(function()
 	local topic = "/luatos/123456"
-	local payload = "{\"cn\": \"合宙\", \" luatos\": \"yyds\"}"
+	local payload = "123"
 	local qos = 1
     while true do
         sys.wait(5000)
         if mqttc and mqttc:ready() then
-            local pkgid = mqttc:publish(topic, payload, qos)
+            log.info("mqtt的topic",topic,payload,qos)
+            local pkgid = mqttc:publish(topic, payload, qos) --发送一条消息
         end
     end
 end)
+ 

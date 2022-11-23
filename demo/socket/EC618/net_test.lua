463b7e93b9c165880d89e6f44bf788bc740374cf
liyuanlong <1398051498@qq.com>
2022-11-23 11:01:57 +0800

    fix：修改了socket_demo的名字，require改变了文件名字

--- demo/socket/EC618/net_test.lua
+++ demo/socket/EC618/net_test.lua
@@ -1,9 +1,9 @@
 -- 如果运营商自带的DNS不好用，可以用下面的公用DNS
 -- socket.setDNS(nil,1,"223.5.5.5")	
 -- socket.setDNS(nil,2,"114.114.114.114")
-require "dtu_demo"
-dtuDemo(1, "112.125.89.8", 33107)
-require "ntp_demo"
-ntpDemo()
-require "http_demo"
-require "mqtt_demo"
+require "socket_demo"
+dtuDemo(1, "112.125.89.8", 35009)
+-- require "ntp_demo"
+-- ntpDemo()
+-- require "http_demo"
+-- require "mqtt_demo"

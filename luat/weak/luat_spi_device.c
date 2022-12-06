d9cf58efb3b87e72c4c5b3fb040132a3e55dfb6a
Dozingfiretruck <1473454180@qq.com>
2022-12-06 15:41:14 +0800

    add:半双工时不使用transfer

--- luat/weak/luat_spi_device.c
+++ luat/weak/luat_spi_device.c
@@ -22,10 +22,16 @@ LUAT_WEAK int luat_spi_device_close(luat_spi_device_t* spi_dev) {
 
 //收发SPI数据，返回接收字节数
 LUAT_WEAK int luat_spi_device_transfer(luat_spi_device_t* spi_dev, const char* send_buf, size_t send_length, char* recv_buf, size_t recv_length) {
+    int ret = -1;
     luat_spi_device_config(spi_dev);
     if (spi_dev->spi_config.cs != 255)
         luat_gpio_set(spi_dev->spi_config.cs, LUAT_SPI_CS_SELECT);
-    int ret = luat_spi_transfer(spi_dev->bus_id, send_buf, send_length, recv_buf, recv_length);
+    if (spi_dev->spi_config.mode){
+        ret = luat_spi_transfer(spi_dev->bus_id, send_buf, send_length, recv_buf, recv_length);
+    }else{
+        ret = luat_spi_send(spi_dev->bus_id, send_buf, send_length);
+        ret = luat_spi_recv(spi_dev->bus_id, recv_buf, recv_length);
+    }
     if (spi_dev->spi_config.cs != 255)
         luat_gpio_set(spi_dev->spi_config.cs, LUAT_SPI_CS_CLEAR);
     return ret;

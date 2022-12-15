ec7d74c7760abc73f56a4be686f0ac1cc3ade03b
Wendal Chen <wendal1985@gmail.com>
2022-12-15 10:53:53 +0800

    fix: fatfs库补充部分API注释 https://gitee.com/openLuat/LuatOS/issues/I65ZIN

--- components/fatfs/luat_lib_fatfs.c
+++ components/fatfs/luat_lib_fatfs.c
@@ -1,4 +1,14 @@
 
+/*
+@module  fatfs
+@summary 读写fatfs格式
+@version 1.0
+@date    2020.07.03
+@demo    fatfs
+@tag LUAT_USE_FATFS
+@usage
+-- 通常只使用fatfs.mount挂载tf/sd卡,其他操作走io库就可以了
+*/
 #include "luat_base.h"
 #include "luat_spi.h"
 #include "luat_timer.h"
@@ -29,6 +39,39 @@ DRESULT diskio_open_spitf(BYTE pdrv, void* userdata);
 extern const struct luat_vfs_filesystem vfs_fs_fatfs;
 #endif
 
+/*
+挂载fatfs
+@api fatfs.mount(mount_point, spiid_or_spidevice, spi_cs, spi_speed)
+@string fatfs挂载点, 通常填""或者"SD", 底层会映射到vfs的 /sd 路径
+@int 传入spi device指针,或者spi的id
+@int 片选脚的GPIO 号, 若前一个参数传的是spi device,这个参数就不需要传
+@int SPI最高速度,默认10M, 若前2个参数传的是spi device,这个参数就不需要传
+@return bool 成功返回true, 否则返回nil或者false
+@return string 失败的原因
+@usage
+-- 方法1, 使用SPI模式
+    local spiId = 2
+    local result = spi.setup(
+        spiId,--串口id
+        255, -- 不使用默认CS脚
+        0,--CPHA
+        0,--CPOL
+        8,--数据宽度
+        400*1000  -- 初始化时使用较低的频率
+    )
+    local TF_CS = pin.PB3
+    gpio.setup(TF_CS, 1)
+    --fatfs.debug(1) -- 若挂载失败,可以尝试打开调试信息,查找原因
+	-- 提醒, 若TF/SD模块带电平转换, 通常不支持10M以上的波特率!!
+    fatfs.mount("SD", spiId, TF_CS, 24000000)
+    local data, err = fatfs.getfree("SD")
+    if data then
+        log.info("fatfs", "getfree", json.encode(data))
+    else
+        log.info("fatfs", "err", err)
+    end
+	-- 往下的操作, 使用 io.open("/sd/xxx", "w+") 等io库的API就可以了
+*/
 static int fatfs_mount(lua_State *L)
 {
 	if (FATFS_DEBUG)
@@ -134,6 +177,27 @@ static int fatfs_mkfs(lua_State *L) {
 	return 1;
 }
 
+/**
+获取可用空间信息
+@api fatfs.getfree(mount_point)
+@string 挂载点, 需要跟fatfs.mount传入的值一致
+@return table 若成功会返回table,否则返回nil
+@return int 导致失败的底层返回值
+@usage
+-- table包含的内容有
+-- total_sectors 总扇区数量
+-- free_sectors 空闲扇区数量
+-- total_kb 总字节数,单位kb
+-- free_kb 空闲字节数, 单位kb
+-- 注意,当前扇区大小固定在512字节
+
+    local data, err = fatfs.getfree("SD")
+    if data then
+        log.info("fatfs", "getfree", json.encode(data))
+    else
+        log.info("fatfs", "err", err)
+    end
+ */
 static int fatfs_getfree(lua_State *L)
 {
 	DWORD fre_clust, fre_sect, tot_sect;

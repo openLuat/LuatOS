81824d421f477ec0b44c6f1b68bdae448a6883a7
alienwalker <alienwalker@sina.com>
2022-12-21 17:28:25 +0800

    fix:fota注释的有点问题

--- luat/modules/luat_lib_fota.c
+++ luat/modules/luat_lib_fota.c
@@ -20,7 +20,7 @@
 @int/string fota数据存储的起始位置<br>如果是int，则是由芯片平台具体判断<br>如果是string，则存储在文件系统中<br>如果为nil，则由底层决定存储位置
 @int 数据存储的最大空间
 @userdata param1，如果数据存储在spiflash时,为spi_device
-@return 成功返回true, 失败返回false
+@return boolean 成功返回true, 失败返回false
 @usage
 -- 初始化fota流程
 local result = mcu.fotaInit(0, 0x00300000, spi_device)	--由于105的flash从0x01000000开始，所以0就是外部spiflash
@@ -54,8 +54,7 @@ static int l_fota_init(lua_State* L)
 等待底层fota流程准备好，目前只有105能使用
 @api fota.fotaWait()
 @boolean 是否完整走完流程，true 表示正确走完流程了
-@return
-@boolean 准备好返回true
+@return boolean 准备好返回true
 @usage
 local isDone = mcu.fotaWait()
 */
@@ -69,13 +68,11 @@ static int l_fota_wait(lua_State* L)
 写入fota数据，目前只有105能使用
 @api fota.fotaRun(buff)
 @zbuff/string fota数据，尽量用zbuff，如果传入的是zbuff，写入成功后，自动清空zbuff内的数据
-@return
-@boolean 有异常返回true
-@boolean 接收到最后一块返回true
-@int 还未写入的数据量，超过64K必须做等待
+@return boolean 有异常返回true
+@return boolean 接收到最后一块返回true
+@return int 还未写入的数据量，超过64K必须做等待
 @usage
--- 写入fota流程
-local isError, isDone, cache = mcu.fotaRun(buf)
+local isError, isDone, cache = fota.fotaRun(buf) -- 写入fota流程
 */
 static int l_fota_write(lua_State* L)
 {
@@ -115,9 +112,8 @@ static int l_fota_write(lua_State* L)
 等待底层fota流程完成，目前只有105能使用
 @api fota.fotaDone()
 @boolean 是否完整走完流程，true 表示正确走完流程了
-@return
-@boolean 有异常返回true
-@boolean 写入到最后一块返回true
+@return boolean 有异常返回true
+@return boolean 写入到最后一块返回true
 @usage
 local isError, isDone = mcu.fotaDone()
 */
@@ -146,7 +142,7 @@ static int l_fota_done(lua_State* L)
 结束fota流程，目前只有105能使用
 @api fota.fotaEnd(is_ok)
 @boolean 是否完整走完流程，true 表示正确走完流程了
-@return 成功返回true, 失败返回false
+@return boolean 成功返回true, 失败返回false
 @usage
 -- 结束fota流程
 local result = mcu.fotaEnd(true)

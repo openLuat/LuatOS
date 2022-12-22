a1cd843a9d8222395dafbfee1ae2ba38359e7bd9
alienwalker <alienwalker@sina.com>
2022-12-22 14:09:52 +0800

    update:完善USB插拔检测demo

--- demo/gpio/EC618_usbconnect/main.lua
+++ demo/gpio/EC618_usbconnect/main.lua
@@ -1,19 +1,21 @@
 
 -- LuaTools需要PROJECT和VERSION这两个信息
-PROJECT = "pwrkey_demo"
+PROJECT = "usb_connect_demo"
 VERSION = "1.0.0"
 
 log.info("main", PROJECT, VERSION)
 
 -- sys库是标配
 _G.sys = require("sys")
-
+led = gpio.setup(24, 1) --如果真的把USB拔出，可能无法打印出信息，所以拿个IO输出和USB一样状态的电平
 
 gpio.setup(33, function()
     log.info("usb", gpio.get(33))
+    led(gpio.get(33))   --IO输出和USB一样的状态
 end, gpio.PULLUP, gpio.BOTH)
-gpio.debounce(33, 2000, 1)  --加入消抖是为了能看到输出
+gpio.debounce(33, 500, 1)  --加入消抖是为了尽量能看到输出
 log.info("usb", gpio.get(33))
+led(gpio.get(33)) --IO输出和USB一样的状态
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
 sys.run()

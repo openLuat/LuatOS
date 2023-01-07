2f2d9c71737da029b83c1c2ec3108122eb374556
Wendal Chen <wendal1985@gmail.com>
2023-01-07 22:21:29 +0800

    fix: ssl_write_client_hello的打印语句补充修改

--- components/mbedtls/library/ssl_cli.c
+++ components/mbedtls/library/ssl_cli.c
@@ -1183,8 +1183,14 @@ static int ssl_write_client_hello( mbedtls_ssl_context *ssl )
                                       ssl->conf->max_minor_ver ) != 0 )
             continue;
 
-        MBEDTLS_SSL_DEBUG_MSG( 3, ( "client hello, add ciphersuite: %#04x (%s)",
-                                    (unsigned int)ciphersuites[i], ciphersuite_info->name ) );
+#if (defined __LUATOS__) || (defined __USER_CODE__)
+        MBEDTLS_SSL_DEBUG_MSG( 3, ( "client hello, add ciphersuite: %04x (%s)",
+                                            (unsigned int)ciphersuites[i], ciphersuite_info->name ) );
+#else
+
+         MBEDTLS_SSL_DEBUG_MSG( 3, ( "client hello, add ciphersuite: %#04x (%s)",
+                                     (unsigned int)ciphersuites[i], ciphersuite_info->name ) );
+#endif
 
 #if defined(MBEDTLS_ECDH_C) || defined(MBEDTLS_ECDSA_C) || \
     defined(MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED)

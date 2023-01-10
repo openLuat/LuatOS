4397c5d9bae06abe04393304f3fe4e688b46d27c
Dozingfiretruck <1473454180@qq.com>
2023-01-10 15:13:51 +0800

    update:更新adc demo

--- demo/adc/main.lua
+++ demo/adc/main.lua
@@ -4,12 +4,12 @@
 2. Air101，Air103模块上的ADC1脚-PA4, 0~2.4v,不要超过范围使用!!!
 3. Air103 模块上的ADC2脚-PA2, 0~2.4v,不要超过范围使用!!! 
 4. Air103 模块上的ADC3脚-PA3, 0~2.4v,不要超过范围使用!!! 
-5. Air101,Air103 ADC10 为内部温度 ，11为VBAT
+5. Air101,Air103 adc.CH_CPU 为内部温度 ，adc.CH_VBAT为VBAT
 6. Air105 adc参考电压是1.88V，所有通道一致，
 7. Air105内部分压没有隔离措施，在开启内部分压后，量程有所变化，具体看寄存器手册，1~5分压后能测到3.6，6通道能接近5V，但是不能直接测5V，可以测4.2V 0通道是始终开启无法关闭分压。
 8. Air780E内部ADC接口为12bits 外部直流分压为0-3.4V
 9. Air780E内部具有2个ADC接口，ADC0 -- AIO3 ADC1 -- AIO4 
-10. 特殊通道, CPU内部温度Temp -- 10 主供电脚电压 VBAT -- 11
+10. 特殊通道, CPU内部温度Temp -- adc.CH_CPU 主供电脚电压 VBAT -- adc.CH_VBAT
 ]]
 
 -- LuaTools需要PROJECT和VERSION这两个信息
@@ -30,19 +30,19 @@ end
 local rtos_bsp = rtos.bsp()
 function adc_pin() -- 根据不同开发板，设置ADC编号
     if rtos_bsp == "AIR101" then -- Air101开发板ADC编号
-        return 0,1,255,255,adc.CH_CPU or 10,adc.CH_VBAT or 11
+        return 0,1,255,255,adc.CH_CPU ,adc.CH_VBAT 
     elseif rtos_bsp == "AIR103" then -- Air103开发板ADC编号
-        return 0,1,2,3,adc.CH_CPU or 10,adc.CH_VBAT or 11
+        return 0,1,2,3,adc.CH_CPU ,adc.CH_VBAT 
     elseif rtos_bsp == "AIR105" then -- Air105开发板ADC编号
         return 0,5,6,255,255,255
     elseif rtos_bsp == "ESP32C3" then -- ESP32C3开发板ADC编号
-        return 0,1,2,3,adc.CH_CPU or 10, 255
+        return 0,1,2,3,adc.CH_CPU , 255
     elseif rtos_bsp == "ESP32C2" then -- ESP32C2开发板ADC编号
-        return 0,1,2,3,adc.CH_CPU or 10, 255
+        return 0,1,2,3,adc.CH_CPU , 255
     elseif rtos_bsp == "ESP32S3" then -- ESP32S3开发板ADC编号
-        return 0,1,2,3,adc.CH_CPU or 10, 255
+        return 0,1,2,3,adc.CH_CPU , 255
     elseif rtos_bsp == "EC618" then --Air780E开发板ADC编号
-        return 0,1,255,255,adc.CH_CPU or 10,adc.CH_VBAT or 11
+        return 0,1,255,255,adc.CH_CPU ,adc.CH_VBAT 
     else
         log.info("main", "define ADC pin in main.lua")
         return 0, 0,0,0,0,0

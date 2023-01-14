2c624ca36499fb302827cc85cf62b5551f59f79c
alienwalker <alienwalker@sina.com>
2023-01-14 13:09:17 +0800

    add:amrnb编码功能

--- components/multimedia/amr_decode/src/amrnb_wrapper.c
+++ components/multimedia/amr_decode/src/amrnb_wrapper.c
@@ -19,12 +19,11 @@
 #define AMRNB_WRAPPER_INTERNAL
 #include <sp_dec.h>
 #include <amrdecode.h>
-//#include <amrencode.h>
+#include <amrencode.h>
 #include "interf_dec.h"
-//#include "interf_enc.h"
+#include "interf_enc.h"
 #include <stdlib.h>
 
-#ifndef DISABLE_AMRNB_DECODER
 
 void* Decoder_Interface_init(void) {
 	void* ptr = NULL;
@@ -41,32 +40,30 @@ void Decoder_Interface_Decode(void* state, const unsigned char* in, short* out, 
 	in++;
 	AMRDecode(state, (enum Frame_Type_3GPP) type, (UWord8*) in, out, MIME_IETF);
 }
-#endif
 
-//#ifndef DISABLE_AMRNB_ENCODER
-//struct encoder_state {
-//	void* encCtx;
-//	void* pidSyncCtx;
-//};
-//
-//void* Encoder_Interface_init(int dtx) {
-//	struct encoder_state* state = (struct encoder_state*) malloc(sizeof(struct encoder_state));
-//	AMREncodeInit(&state->encCtx, &state->pidSyncCtx, dtx);
-//	return state;
-//}
-//
-//void Encoder_Interface_exit(void* s) {
-//	struct encoder_state* state = (struct encoder_state*) s;
-//	AMREncodeExit(&state->encCtx, &state->pidSyncCtx);
-//	free(state);
-//}
-//
-//int Encoder_Interface_Encode(void* s, enum Mode mode, const short* speech, unsigned char* out, int forceSpeech) {
-//	struct encoder_state* state = (struct encoder_state*) s;
-//	enum Frame_Type_3GPP frame_type = (enum Frame_Type_3GPP) mode;
-//	int ret = AMREncode(state->encCtx, state->pidSyncCtx, mode, (Word16*) speech, out, &frame_type, AMR_TX_IETF);
-//	out[0] |= 0x04;
-//	return ret;
-//}
-//#endif
+struct encoder_state {
+	void* encCtx;
+	void* pidSyncCtx;
+};
+
+void* Encoder_Interface_init(int dtx) {
+	struct encoder_state* state = (struct encoder_state*) malloc(sizeof(struct encoder_state));
+	AMREncodeInit(&state->encCtx, &state->pidSyncCtx, dtx);
+	return state;
+}
+
+void Encoder_Interface_exit(void* s) {
+	struct encoder_state* state = (struct encoder_state*) s;
+	AMREncodeExit(&state->encCtx, &state->pidSyncCtx);
+	free(state);
+}
+
+int Encoder_Interface_Encode(void* s, enum Mode mode, const short* speech, unsigned char* out, int forceSpeech) {
+	struct encoder_state* state = (struct encoder_state*) s;
+	enum Frame_Type_3GPP frame_type = (enum Frame_Type_3GPP) mode;
+	int ret = AMREncode(state->encCtx, state->pidSyncCtx, mode, (Word16*) speech, out, &frame_type, AMR_TX_IETF);
+	out[0] |= 0x04;
+	return ret;
+}
+
 

856da06e2ab80d90f23bd0ff66a371b6d97a58e7
alienwalker <alienwalker@sina.com>
2023-01-29 15:03:42 +0800

    fix:demo/mobile/main.lua里的默认apn拿不到了

--- demo/mobile/main.lua
+++ demo/mobile/main.lua
@@ -33,7 +33,7 @@ sys.taskInit(function()
         log.info("rsrp", mobile.rsrp())
         log.info("snr", mobile.snr())
         log.info("simid", mobile.simid()) -- 这里是获取当前SIM卡槽
-        log.info("apn", mobile.apn(0,2))
+        log.info("apn", mobile.apn(0,1))
         log.info("ip", socket.localIP())
         sys.wait(15000)
     end

56e9c95dc296c7b1421de15a3fe7e20510f50155
alienwalker <alienwalker@sina.com>
2023-01-30 13:09:59 +0800

    update:618可以开机就获取到model name了

--- demo/hmeta/main.lua
+++ demo/hmeta/main.lua
@@ -9,11 +9,11 @@ _G.sys = require("sys")
 
 sys.taskInit(function()
     while 1 do
-        sys.wait(3000)
         -- hmeta识别底层模组类型的
         -- 不同的模组可以使用相同的bsp,但根据封装的不同,根据内部数据仍可识别出具体模块
         log.info("hmeta", hmeta.model())
         log.info("bsp",   rtos.bsp())
+        sys.wait(3000)
     end
 end)
 

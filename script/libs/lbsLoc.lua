cf3805f1aa0a7a979d1095e058142dde6340c91f
Dozingfiretruck <1473454180@qq.com>
2023-01-31 21:12:53 +0800

    update:demo去掉定时执行

--- script/libs/lbsLoc.lua
+++ script/libs/lbsLoc.lua
@@ -9,9 +9,6 @@
 --用法实例
 PRODUCT_KEY = "VmhtOb81EgZau6YyuuZJzwF6oUNGCbXi"
 local lbsLoc = require("lbsLoc")
-local function reqLbsLoc()
-    lbsLoc.request(getLocCb)
-end
 -- 功能:获取基站对应的经纬度后的回调函数
 -- 参数:-- result：number类型，0表示成功，1表示网络环境尚未就绪，2表示连接服务器失败，3表示发送数据失败，4表示接收服务器应答超时，5表示服务器返回查询失败；为0时，后面的5个参数才有意义
 		-- lat：string类型，纬度，整数部分3位，小数部分7位，例如031.2425864
@@ -32,9 +29,8 @@ function getLocCb(result, lat, lng, addr, time, locType)
         log.info("服务器返回的时间", time:toHex())
         log.info("定位类型,基站定位成功返回0", locType)
     end
-    sys.timerStart(lbsLoc,20000)
 end
-reqLbsLoc()
+lbsLoc.request(getLocCb)
 ]]
 
 local sys = require "sys"

0f2eb9d5d64e5c2fe1b1777eb49ed89b085477d5
Dozingfiretruck <1473454180@qq.com>
2023-02-24 10:06:00 +0800

    del:去掉LUA_USE_WINDOWS判断

--- components/lua-cjson/lua_cjson.c
+++ components/lua-cjson/lua_cjson.c
@@ -109,7 +109,6 @@ static const u_char charmap[] = {
 	'\370', '\371', '\372', '\373', '\374', '\375', '\376', '\377',
 };
 
-#ifndef LUA_USE_WINDOWS
 int
 strcasecmp(const char *s1, const char *s2)
 {
@@ -140,7 +139,6 @@ strncasecmp(const char *s1, const char *s2, size_t n)
   }
 	return (0);
 }
-#endif
 
 #endif
 

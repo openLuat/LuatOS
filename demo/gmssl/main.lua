d0b460545f2e9442e6001bf9454e41d40e68756f
Wendal Chen <wendal1985@gmail.com>
2023-03-03 00:25:22 +0800

    change: 国密算法改名为gmssl, 毕竟是基于gmssl实现的, SM有点那个意思了-_-

--- demo/gmssl/main.lua
+++ demo/gmssl/main.lua
@@ -1,6 +1,6 @@
 
 -- LuaTools需要PROJECT和VERSION这两个信息
-PROJECT = "smdemo"
+PROJECT = "gmssldemo"
 VERSION = "1.0.0"
 
 log.info("main", PROJECT, VERSION)
@@ -17,40 +17,43 @@ end
 sys.taskInit(function()
 
     sys.wait(3000)
-    log.info("sm", "start")
+    log.info("gmssl", "start")
 
+    -- SM2 , 非对称加密, 类似于RSA,但属于椭圆算法
+    -- 就当前实现还是比较慢的
     local originStr = "encryption standard"
     local pkx = "191BFF8148006EEA72D857CB974DB9F4903B3CA3655D8D597AD4663F5044DCB1"
     local pky = "E2F7888AF1FCD8C653A8059CD2F379855389F71A7709E2C1EE1E914C855EF119"
     local private = "1649AB77A00637BD5E2EFE283FBF353534AA7F7CB89463F208DDBC2920BB0DA0"
     local rand = "4C62EEFD6ECFC2B95B92FD6C3D9575148AFA17425546D49018E5388D49DD7B4F"
-    local encodeStr = sm.sm2encrypt(pkx,pky,rand,originStr,private)
+    local encodeStr = gmssl.sm2encrypt(pkx,pky,rand,originStr,private)
     print(originStr,"encrypt",string.toHex(encodeStr))
-    log.info("testsm.sm2decrypt",sm.sm2decrypt(private,encodeStr,pkx,pky))
+    log.info("testsm.sm2decrypt",gmssl.sm2decrypt(private,encodeStr,pkx,pky))
 
-    encodeStr = sm.sm3update("lqlq666lqlq946")
+    -- SM3 算法, hash类
+    encodeStr = gmssl.sm3update("lqlq666lqlq946")
     log.info("testsm.sm3update",string.toHex(encodeStr))
 
-    originStr = "AES128 ECB ZeroPadding test"
+    -- SM4 算法, 对称加密
+    originStr = "SM4 ECB ZeroPadding test"
     --加密模式：ECB；填充方式：ZeroPadding；密钥：1234567890123456；密钥长度：128 bit
-    encodeStr = sm.sm4encrypt("ECB","ZERO",originStr,"1234567890123456")
+    encodeStr = gmssl.sm4encrypt("ECB","ZERO",originStr,"1234567890123456")
     print(originStr,"encrypt",string.toHex(encodeStr))
-    log.info("testsm.sm4decrypt",sm.sm4decrypt("ECB","ZERO",encodeStr,"1234567890123456"))
+    log.info("testsm.sm4decrypt",gmssl.sm4decrypt("ECB","ZERO",encodeStr,"1234567890123456"))
 
     originStr = "AES128 ECB Pkcs5Padding test"
     --加密模式：ECB；填充方式：Pkcs5Padding；密钥：1234567890123456；密钥长度：128 bit
-    encodeStr = sm.sm4encrypt("ECB","PKCS5",originStr,"1234567890123456")
+    encodeStr = gmssl.sm4encrypt("ECB","PKCS5",originStr,"1234567890123456")
     print(originStr,"encrypt",string.toHex(encodeStr))
-    log.info("testsm.sm4decrypt",sm.sm4decrypt("ECB","PKCS5",encodeStr,"1234567890123456"))
+    log.info("testsm.sm4decrypt",gmssl.sm4decrypt("ECB","PKCS5",encodeStr,"1234567890123456"))
 
     originStr = "AES256 CBC Pkcs5Padding test"
     --加密模式：CBC；填充方式：Pkcs5Padding；密钥：1234567890123456；密钥长度：256 bit；偏移量：1234567890666666
-    encodeStr = sm.sm4encrypt("CBC","PKCS5",originStr,"1234567890123456","1234567890666666")
+    encodeStr = gmssl.sm4encrypt("CBC","PKCS5",originStr,"1234567890123456","1234567890666666")
     print(originStr,"encrypt",string.toHex(encodeStr))
-    log.info("testsm.sm4decrypt",sm.sm4decrypt("CBC","PKCS5",encodeStr,"1234567890123456","1234567890666666"))
+    log.info("testsm.sm4decrypt",gmssl.sm4decrypt("CBC","PKCS5",encodeStr,"1234567890123456","1234567890666666"))
 
     log.info("sm", "ALL Done")
-    sys.wait(100000)
 end)
 
 -- 用户代码已结束---------------------------------------------

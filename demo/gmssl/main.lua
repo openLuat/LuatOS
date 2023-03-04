75a9e0b4723e7426ed1658dec77e09e5e0a22a1f
Wendal Chen <wendal1985@gmail.com>
2023-03-04 14:50:28 +0800

    fix: demo/gmssl的注释有误,应该是SM4,不是AES128

--- demo/gmssl/main.lua
+++ demo/gmssl/main.lua
@@ -43,24 +43,26 @@ sys.taskInit(function()
     end
 
     if gmssl.sm4encrypt then
+        local passwd = "1234567890123456"
+        local iv = "1234567890666666"
         -- SM4 算法, 对称加密
-        originStr = "SM4 ECB ZeroPadding test"
+        originStr = ">>SM4 ECB ZeroPadding test<<"
         --加密模式：ECB；填充方式：ZeroPadding；密钥：1234567890123456；密钥长度：128 bit
-        encodeStr = gmssl.sm4encrypt("ECB","ZERO",originStr,"1234567890123456")
+        encodeStr = gmssl.sm4encrypt("ECB","ZERO",originStr,passwd)
         print(originStr,"encrypt",string.toHex(encodeStr))
-        log.info("testsm.sm4decrypt",gmssl.sm4decrypt("ECB","ZERO",encodeStr,"1234567890123456"))
+        log.info("testsm.sm4decrypt",gmssl.sm4decrypt("ECB","ZERO",encodeStr,passwd))
 
-        originStr = "AES128 ECB Pkcs5Padding test"
+        originStr = ">>SM4 ECB Pkcs5Padding test<<"
         --加密模式：ECB；填充方式：Pkcs5Padding；密钥：1234567890123456；密钥长度：128 bit
-        encodeStr = gmssl.sm4encrypt("ECB","PKCS5",originStr,"1234567890123456")
+        encodeStr = gmssl.sm4encrypt("ECB","PKCS5",originStr,passwd)
         print(originStr,"encrypt",string.toHex(encodeStr))
-        log.info("testsm.sm4decrypt",gmssl.sm4decrypt("ECB","PKCS5",encodeStr,"1234567890123456"))
+        log.info("testsm.sm4decrypt",gmssl.sm4decrypt("ECB","PKCS5",encodeStr,passwd))
 
-        originStr = "AES256 CBC Pkcs5Padding test"
-        --加密模式：CBC；填充方式：Pkcs5Padding；密钥：1234567890123456；密钥长度：256 bit；偏移量：1234567890666666
-        encodeStr = gmssl.sm4encrypt("CBC","PKCS5",originStr,"1234567890123456","1234567890666666")
+        originStr = ">>SM4 CBC Pkcs5Padding test<<"
+        --加密模式：CBC；填充方式：Pkcs5Padding；密钥：1234567890123456；密钥长度：128 bit；偏移量：1234567890666666
+        encodeStr = gmssl.sm4encrypt("CBC","PKCS5",originStr,passwd,iv)
         print(originStr,"encrypt",string.toHex(encodeStr))
-        log.info("testsm.sm4decrypt",gmssl.sm4decrypt("CBC","PKCS5",encodeStr,"1234567890123456","1234567890666666"))
+        log.info("testsm.sm4decrypt",gmssl.sm4decrypt("CBC","PKCS5",encodeStr,passwd, iv))
     end
 
     log.info("sm", "ALL Done")

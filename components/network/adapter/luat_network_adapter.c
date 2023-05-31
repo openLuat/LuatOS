d9c1a0d691a9eeff4ec1d0759536165aebf672e0
alienwalker <alienwalker@sina.com>
2023-05-31 12:11:07 +0800

    fix:上次改动不完整

--- components/network/adapter/luat_network_adapter.c
+++ components/network/adapter/luat_network_adapter.c
@@ -1015,6 +1015,8 @@ static void network_default_statemachine(network_ctrl_t *ctrl, OS_EVENT *event, 
 			NW_UNLOCK;
 			if (ctrl->new_rx_flag && ctrl->user_callback)
 			{
+				event->ID = NW_WAIT_EVENT + EV_NW_RESULT_BASE;
+				event->Param1 = 0;
 				ctrl->user_callback(event, ctrl->user_data);
 			}
 			return ;

445620f9af325ff30fd25178fbe74a78e2a02abc
Dozingfiretruck <1473454180@qq.com>
2023-06-09 11:22:41 +0800

    update:腾讯云使用强校验，更安全

--- script/libs/iotcloud.lua
+++ script/libs/iotcloud.lua
@@ -11,6 +11,29 @@ iotcloud.key = "key"
 -- event
 iotcloud.connect = "connect"
 
+local tencent_ca_crt = [[-----BEGIN CERTIFICATE-----
+MIIDxTCCAq2gAwIBAgIJALM1winYO2xzMA0GCSqGSIb3DQEBCwUAMHkxCzAJBgNV
+BAYTAkNOMRIwEAYDVQQIDAlHdWFuZ0RvbmcxETAPBgNVBAcMCFNoZW5aaGVuMRAw
+DgYDVQQKDAdUZW5jZW50MRcwFQYDVQQLDA5UZW5jZW50IElvdGh1YjEYMBYGA1UE
+AwwPd3d3LnRlbmNlbnQuY29tMB4XDTE3MTEyNzA0MjA1OVoXDTMyMTEyMzA0MjA1
+OVoweTELMAkGA1UEBhMCQ04xEjAQBgNVBAgMCUd1YW5nRG9uZzERMA8GA1UEBwwI
+U2hlblpoZW4xEDAOBgNVBAoMB1RlbmNlbnQxFzAVBgNVBAsMDlRlbmNlbnQgSW90
+aHViMRgwFgYDVQQDDA93d3cudGVuY2VudC5jb20wggEiMA0GCSqGSIb3DQEBAQUA
+A4IBDwAwggEKAoIBAQDVxwDZRVkU5WexneBEkdaKs4ehgQbzpbufrWo5Lb5gJ3i0
+eukbOB81yAaavb23oiNta4gmMTq2F6/hAFsRv4J2bdTs5SxwEYbiYU1teGHuUQHO
+iQsZCdNTJgcikga9JYKWcBjFEnAxKycNsmqsq4AJ0CEyZbo//IYX3czEQtYWHjp7
+FJOlPPd1idKtFMVNG6LGXEwS/TPElE+grYOxwB7Anx3iC5ZpE5lo5tTioFTHzqbT
+qTN7rbFZRytAPk/JXMTLgO55fldm4JZTP3GQsPzwIh4wNNKhi4yWG1o2u3hAnZDv
+UVFV7al2zFdOfuu0KMzuLzrWrK16SPadRDd9eT17AgMBAAGjUDBOMB0GA1UdDgQW
+BBQrr48jv4FxdKs3r0BkmJO7zH4ALzAfBgNVHSMEGDAWgBQrr48jv4FxdKs3r0Bk
+mJO7zH4ALzAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQDRSjXnBc3T
+d9VmtTCuALXrQELY8KtM+cXYYNgtodHsxmrRMpJofsPGiqPfb82klvswpXxPK8Xx
+SuUUo74Fo+AEyJxMrRKlbJvlEtnpSilKmG6rO9+bFq3nbeOAfat4lPl0DIscWUx3
+ajXtvMCcSwTlF8rPgXbOaSXZidRYNqSyUjC2Q4m93Cv+KlyB+FgOke8x4aKAkf5p
+XR8i1BN1OiMTIRYhGSfeZbVRq5kTdvtahiWFZu9DGO+hxDZObYGIxGHWPftrhBKz
+RT16Amn780rQLWojr70q7o7QP5tO0wDPfCdFSc6CQFq/ngOzYag0kJ2F+O5U6+kS
+QVrcRBDxzx/G
+-----END CERTIFICATE-----]]
 
 local cloudc_table = {}
 
@@ -65,12 +88,12 @@ local function iotcloud_auto_enrol(iotcloudc)
             signature=signature,
         }
         local cloud_body_json = json.encode(cloud_body)
-        log.info("cloud_body_json", cloud_body_json)
+        -- log.info("cloud_body_json", cloud_body_json)
         local code, headers, body = http.request("POST","https://ap-guangzhou.gateway.tencentdevices.com/register/dev", 
                 {["Content-Type"]="application/json; charset=UTF-8"},
                 cloud_body_json
         ).wait()
-        log.info("http.post", code, headers, body)
+        -- log.info("http.post", code, headers, body)
         if code == 200 then
             local dat, result, errinfo = json.decode(body)
             if result then
@@ -89,6 +112,7 @@ local function iotcloud_auto_enrol(iotcloudc)
                 end
             end
         else
+            log.info("http.post", code, headers, body)
             return false
         end
     end
@@ -159,6 +183,7 @@ function iotcloud.new(cloud,iot_config,connect_config)
     end
 
     if ca_file then
+        ca_file.server_cert = tencent_ca_crt
         if ca_file.client_cert then
             ca_file.client_cert = ca_file.client_cert:gsub("\b", "\n")
         end
@@ -166,9 +191,6 @@ function iotcloud.new(cloud,iot_config,connect_config)
             ca_file.client_key = ca_file.client_key:gsub("\b", "\n")
         end
     end
-
-    ca_file.verify = 1
-
     iotcloudc.mqttc = mqtt.create(nil, iotcloudc.host, iotcloudc.ip, isssl and ca_file or nil)
     iotcloudc.mqttc:auth(iotcloudc.client_id,iotcloudc.user_name,iotcloudc.password)
     iotcloudc.mqttc:keepalive(300)                         -- 默认值240s

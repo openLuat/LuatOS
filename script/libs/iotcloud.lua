ce596c7ae59cfa7a34f8431c2053d12cf8503510
Dozingfiretruck <1473454180@qq.com>
2023-06-19 16:20:24 +0800

    add:添加iotcloud文档

--- script/libs/iotcloud.lua
+++ script/libs/iotcloud.lua
@@ -1,4 +1,12 @@
-
+--[[
+@module iotcloud
+@summary iotcloud 云平台库 (已支持: 腾讯云 阿里云 其他也会支持,有用到的提issue会加速支持)  
+@version 1.0
+@date    2023.06.19
+@author  Dozingfiretruck
+@usage
+--注意:因使用了sys.wait()所有api需要在协程中使用
+]]
 
 
 
@@ -230,7 +238,6 @@ end
 local function iotcloud_aliyun_config(iotcloudc,iot_config,connect_config)
     iotcloudc.cloud = iotcloud.ALIYUN
     iotcloudc.produt_id = iot_config.produt_id
-    iotcloudc.host = iotcloudc.produt_id..".iot-as-mqtt.cn-shanghai.aliyuncs.com"
     if iot_config.product_secret then                       -- 有product_secret说明是动态注册
         iotcloudc.product_secret = iot_config.product_secret
         if not fskv.get("iotcloud_aliyun") then 
@@ -255,10 +262,26 @@ local function iotcloud_aliyun_config(iotcloudc,iot_config,connect_config)
             return false
         end
     end
-
+    if connect_config then
+        iotcloudc.host = connect_config.host or iotcloudc.produt_id..".iot-as-mqtt.cn-shanghai.aliyuncs.com"
+        if connect_config.ip then iotcloudc.ip = connect_config.ip end
+    else
+        iotcloudc.host = iotcloudc.produt_id..".iot-as-mqtt.cn-shanghai.aliyuncs.com"
+    end
     return true
 end
 
+--[[
+创建云平台对象
+@api iotcloud.new(cloud,iot_config,connect_config)
+@string 云平台 iotcloud.TENCENT:腾讯云 iotcloud.ALIYUN:阿里云
+@table iot云平台配置, device_name:可选，默认为imei否则为unique_id iot_config.produt_id:产品id(阿里云则为产品key) iot_config.product_secret:产品密钥,有此项则为动态注册 iot_config.key:设备秘钥,有此项则为秘钥连接 
+@table mqtt配置, host:可选,默认为平台默认host ip:可选,默认为平台默认ip tls:加密,若有此项一般为产品认证
+@return table 云平台对象
+@usage
+-- 阿里云动态注册
+iotcloudc = iotcloud.new(iotcloud.ALIYUN,{produt_id = "xxx",product_secret = "xxx"})
+]]
 function iotcloud.new(cloud,iot_config,connect_config)
     local iotcloudc = setmetatable({
         cloud = nil,                                        -- 云平台
@@ -301,22 +324,54 @@ function iotcloud.new(cloud,iot_config,connect_config)
     return iotcloudc,error_code                             -- 错误返回待处理
 end 
 
+--[[
+云平台连接
+@api cloudc:connect()
+@usage
+iotcloudc:connect()
+]]
 function cloudc:connect()
     self.mqttc:connect()
 end
 
+--[[
+云平台断开
+@api cloudc:disconnect()
+@usage
+iotcloudc:disconnect()
+]]
 function cloudc:disconnect()
     self.mqttc:disconnect()
 end
 
+--[[
+云平台订阅
+@api cloudc:subscribe(topic, qos)
+@string/table 主题
+@number topic为string时生效 0/1/2 默认0
+]]
 function cloudc:subscribe(topic, qos)
     self.mqttc:subscribe(topic, qos)
 end
 
+--[[
+云平台发布
+@api cloudc:publish(topic,data,qos,retain)
+@string/table 主题
+@string 消息,必填,但长度可以是0
+@number 消息级别 0/1 默认0
+@number 是否存档, 0/1,默认0
+]]
 function cloudc:publish(topic,data,qos,retain)
     self.mqttc:publish(topic,data,qos,retain)
 end
 
+--[[
+云平台关闭
+@api cloudc:close()
+@usage
+iotcloudc:close()
+]]
 function cloudc:close()
     self.mqttc:close()
     for k, v in pairs(cloudc_table) do

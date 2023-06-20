58bb3b91aec611afe57e8cd02cd713e73edcf428
Dozingfiretruck <1473454180@qq.com>
2023-06-20 16:45:23 +0800

    add:http添加下载回调文档注释

--- components/network/libhttp/luat_lib_http.c
+++ components/network/libhttp/luat_lib_http.c
@@ -61,7 +61,7 @@ http客户端
 @string url地址, 支持 http和https, 支持域名, 支持自定义端口
 @tabal  请求头 可选 例如 {["Content-Type"] = "application/x-www-form-urlencoded"}
 @string body 可选, 对POST/PUT等请求方式有效
-@table  额外配置 可选 包含 timeout:超时时间单位ms 可选,默认10分钟,写0即永久等待 dst:下载路径,可选 adapter:选择使用网卡,可选 debug:是否打开debug信息,可选,ipv6:是否为ipv6 默认不是,可选
+@table  额外配置 可选 包含 timeout:超时时间单位ms 可选,默认10分钟,写0即永久等待 dst:下载路径,可选 adapter:选择使用网卡,可选 debug:是否打开debug信息,可选,ipv6:是否为ipv6 默认不是,可选 callback:下载回调函数,参数 content_len:总长度 body_len以下载长度,可选 
 @string 服务器ca证书数据, 可选, 一般不需要
 @string 客户端ca证书数据, 可选, 一般不需要, 双向https认证才需要
 @string 客户端私钥加密数据, 可选, 一般不需要, 双向https认证才需要

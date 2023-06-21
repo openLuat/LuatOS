34ff6c18f377115bd9085c9521cb8b57fee5594e
Dozingfiretruck <1473454180@qq.com>
2023-06-21 10:53:59 +0800

    add:http demo添加下载回调

--- demo/http/main.lua
+++ demo/http/main.lua
@@ -105,6 +105,12 @@ function demo_http_post_form()
     log.info("http.post.form", code, headers, body)
 end
 
+-- local function http_download_callback(content_len,body_len,userdata)
+--     print("http_download_callback",content_len,body_len,userdata)
+-- end
+
+-- local http_userdata = "123456789"
+
 function demo_http_download()
 
     -- POST and download, task内的同步操作
@@ -112,6 +118,13 @@ function demo_http_download()
     opts["dst"] = "/data.bin"       -- 下载路径,可选
     opts["timeout"] = 30000         -- 超时时长,单位ms,可选
     -- opts["adapter"] = socket.ETH0  -- 使用哪个网卡,可选
+    -- opts["callback"] = http_download_callback
+    -- opts["userdata"] = http_userdata
+
+    for k, v in pairs(opts) do
+        print("opts",k,v)
+    end
+    
     local code, headers, body = http.request("POST","http://site0.cn/api/httptest/simple/date",
             {}, -- 请求所添加的 headers, 可以是nil
             "", 
@@ -119,11 +132,11 @@ function demo_http_download()
     ).wait()
     log.info("http.post", code, headers, body) -- 只返回code和headers
 
-    local f = io.open("/data.bin", "rb")
-    if f then
-        local data = f:read("*a")
-        log.info("fs", "data", data, data:toHex())
-    end
+    -- local f = io.open("/data.bin", "rb")
+    -- if f then
+    --     local data = f:read("*a")
+    --     log.info("fs", "data", data, data:toHex())
+    -- end
     
     -- GET request, 开个task让它自行执行去吧, 不管执行结果了
     sys.taskInit(http.request("GET","http://site0.cn/api/httptest/simple/time").wait)
@@ -185,13 +198,13 @@ sys.taskInit(function()
         -- 演示GET请求
         -- demo_http_get()
         -- 表单提交
-        demo_http_post_form()
+        -- demo_http_post_form()
         -- POST一个json字符串
         -- demo_http_post_json()
         -- 上传文件, mulitform形式
         -- demo_http_post_file()
         -- 文件下载
-        -- demo_http_download()
+        demo_http_download()
 
         sys.wait(1000)
         -- 打印一下内存状态

ffff6c87dd9142394e93064fdf211676e5cbafdf
Wendal Chen <wendal1985@gmail.com>
2023-08-24 13:06:42 +0800

    update: demo/hmeta有hmeta库的时候才演示

--- demo/hmeta/main.lua
+++ demo/hmeta/main.lua
@@ -14,13 +14,14 @@ end
 
 
 sys.taskInit(function()
-    while 1 do
+    while hmeta do
         -- hmeta识别底层模组类型的
         -- 不同的模组可以使用相同的bsp,但根据封装的不同,根据内部数据仍可识别出具体模块
         log.info("hmeta", hmeta.model(), hmeta.hwver and hmeta.hwver())
         log.info("bsp",   rtos.bsp())
         sys.wait(3000)
     end
+    log.info("这个bsp不支持hmeta库哦")
 end)
 
 -- 用户代码已结束---------------------------------------------

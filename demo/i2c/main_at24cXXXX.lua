7d8237c6e428fa9746db5114493f6217e65b7828
wanglong <wanglongqyt@163.com>
2023-08-28 14:58:35 +0800

    添加i2c.writeReg的使用方法，添加at24cxxx EEPROM 的demo

---
+++ demo/i2c/main_at24cXXXX.lua
@@ -0,0 +1,29 @@
+-- 添加i2c.writeReg的使用方法，添加at24cxxx EEPROM 的demo 
+-- LuaTools需要PROJECT和VERSION这两个信息
+PROJECT = "i2c 24c02 demo"
+VERSION = "1.0.0"
+-- sys库是标配
+sys = require("sys")
+
+--1010 000x
+--7bit地址，不包含最后一位读写位
+local addr = 0x50
+-- 按照实际芯片更改编号哦
+local i2cid = 0
+
+sys.taskInit(function()
+    log.info("i2c initial",i2c.setup(i2cid))
+    while true do
+        i2c.writeReg(i2cid, addr, 0x01, "abcd1234")
+        sys.wait(100)
+        local data = i2c.readReg(i2cid, addr, 0x01, 8)
+        log.info("i2c", "data2",data:toHex(),data)
+        sys.wait(1000)
+    end
+
+end)
+
+-- 用户代码已结束---------------------------------------------
+-- 结尾总是这一句
+sys.run()
+-- sys.run()之后后面不要加任何语句!!!!!

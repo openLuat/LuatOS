15aebc8d459f3bf246cb1248e85209112acc2f9a
Dozingfiretruck <1473454180@qq.com>
2023-08-29 15:26:31 +0800

    fix:spi方式驱动u8g2失效 add:u8g2添加custom传参

--- components/u8g2/luat_u8g2.h
+++ components/u8g2/luat_u8g2.h
@@ -2,16 +2,23 @@
 #include "luat_base.h"
 #include "u8g2.h"
 
+typedef struct luat_u8g2_custom {
+    size_t init_cmd_count;
+    uint32_t *initcmd; // 实际命令长度与init_cmd_count相同
+}luat_u8g2_custom_t;
+
 typedef struct luat_u8g2_conf
 {
     size_t w;
     size_t h;
     int lua_ref;
     char* cname; // 控制器名称, 例如SSD1306
-    // void* ptr;
-    const u8g2_cb_t* direction;//方向 
+    u8g2_cb_t* direction;//方向 
     u8g2_t u8g2;
+    uint16_t sleepcmd;
+    uint16_t wakecmd;
     uint8_t* buff_ptr;
+    void* userdata;
 } luat_u8g2_conf_t;
 
 int luat_u8g2_setup(luat_u8g2_conf_t *conf);

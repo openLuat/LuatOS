a32c3e3d3ca85a08a1d5cc8ae789126fbbc4950b
Dozingfiretruck <1473454180@qq.com>
2023-09-07 22:12:17 +0800

    fix:MQTT_MSG_CLOSE重复上报

--- components/network/libemqtt/luat_mqtt_client.c
+++ components/network/libemqtt/luat_mqtt_client.c
@@ -103,18 +103,20 @@ int luat_mqtt_set_connopts(luat_mqtt_ctrl_t *mqtt_ctrl, luat_mqtt_connopts_t *op
 
 void luat_mqtt_close_socket(luat_mqtt_ctrl_t *mqtt_ctrl){
 	LLOGI("mqtt closing socket netc:%p mqtt_state:%d",mqtt_ctrl->netc,mqtt_ctrl->mqtt_state);
-	if (mqtt_ctrl->netc && mqtt_ctrl->mqtt_state){
-		network_force_close_socket(mqtt_ctrl->netc);
-		l_luat_mqtt_msg_cb(mqtt_ctrl, MQTT_MSG_DISCONNECT, mqtt_ctrl->error_state==0?MQTT_ERROR_STATE_SOCKET:mqtt_ctrl->error_state);
+	if (mqtt_ctrl->mqtt_state){
+		if (mqtt_ctrl->netc){
+			network_force_close_socket(mqtt_ctrl->netc);
+			l_luat_mqtt_msg_cb(mqtt_ctrl, MQTT_MSG_DISCONNECT, mqtt_ctrl->error_state==0?MQTT_ERROR_STATE_SOCKET:mqtt_ctrl->error_state);
+		}
+		luat_stop_rtos_timer(mqtt_ctrl->ping_timer);
+		if (mqtt_ctrl->reconnect && mqtt_ctrl->reconnect_time > 0){
+			luat_start_rtos_timer(mqtt_ctrl->reconnect_timer, mqtt_ctrl->reconnect_time, 0);
+		}else{
+			l_luat_mqtt_msg_cb(mqtt_ctrl, MQTT_MSG_CLOSE, 0);
+		}
 	}
-	luat_stop_rtos_timer(mqtt_ctrl->ping_timer);
 	mqtt_ctrl->buffer_offset = 0;
 	mqtt_ctrl->mqtt_state = 0;
-	if (mqtt_ctrl->reconnect && mqtt_ctrl->reconnect_time > 0){
-		luat_start_rtos_timer(mqtt_ctrl->reconnect_timer, mqtt_ctrl->reconnect_time, 0);
-	}else{
-		l_luat_mqtt_msg_cb(mqtt_ctrl, MQTT_MSG_CLOSE, 0);
-	}
 }
 
 void luat_mqtt_release_socket(luat_mqtt_ctrl_t *mqtt_ctrl){

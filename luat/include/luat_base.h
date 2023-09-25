9b43c7e330fc2f5a00e635c46c924b24547301e4
Dozingfiretruck <1473454180@qq.com>
2023-09-25 15:18:09 +0800

    update:luat_base.h里做lua剥离

--- luat/include/luat_base.h
+++ luat/include/luat_base.h
@@ -16,13 +16,18 @@
 
 //-------------------------------
 // 通用头文件
+
+#include "stdint.h"
+#include "string.h"
+#include "luat_types.h"
+
+#ifdef __LUATOS__ 
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 #include "lstate.h"
-#include "stdint.h"
-#include "string.h"
-#include "luat_types.h"
+// luaopen_xxx 代表各种库, 2021.09.26起独立一个头文件
+#include "luat_libs.h"
 
 /**
  * LuatOS主入口函数, 从这里开始就交由LuatOS控制了.
@@ -37,8 +42,19 @@ int luat_main (void);
  */
 void luat_openlibs(lua_State *L);
 
-// luaopen_xxx 代表各种库, 2021.09.26起独立一个头文件
-#include "luat_libs.h"
+// 自定义扩展库的初始化入口, 可以自行注册lua库, 或其他初始化操作.
+void luat_custom_init(lua_State *L);
+
+//c等待接口
+uint64_t luat_pushcwait(lua_State *L);
+//c等待接口，直接向用户返回错误的对象
+void luat_pushcwait_error(lua_State *L, int arg_num);
+//c等待接口，对指定id进行回调响应，并携带返回参数
+int luat_cbcwait(lua_State *L, uint64_t id, int arg_num);
+//c等待接口，无参数的回调，可不传入lua栈
+void luat_cbcwait_noarg(uint64_t id);
+
+#endif
 
 /** sprintf需要支持longlong值的打印, 提供平台无关的实现*/
 int l_sprintf(char *buf, size_t size, const char *fmt, ...);
@@ -70,17 +86,5 @@ void luat_timer_us_delay(size_t us);
 const char* luat_version_str(void);
 
 void luat_os_print_heapinfo(const char* tag);
-
-// 自定义扩展库的初始化入口, 可以自行注册lua库, 或其他初始化操作.
-void luat_custom_init(lua_State *L);
-
-//c等待接口
-uint64_t luat_pushcwait(lua_State *L);
-//c等待接口，直接向用户返回错误的对象
-void luat_pushcwait_error(lua_State *L, int arg_num);
-//c等待接口，对指定id进行回调响应，并携带返回参数
-int luat_cbcwait(lua_State *L, uint64_t id, int arg_num);
-//c等待接口，无参数的回调，可不传入lua栈
-void luat_cbcwait_noarg(uint64_t id);
 
 #endif

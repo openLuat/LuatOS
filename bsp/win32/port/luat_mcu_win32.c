64a291ccdb8fdf73533203bc1965d554bc893956
Wendal Chen <wendal1985@gmail.com>
2023-11-28 06:45:34 +0800

    fix: 修复bsp/win32编译报错

--- bsp/win32/port/luat_mcu_win32.c
+++ bsp/win32/port/luat_mcu_win32.c
@@ -5,3 +5,13 @@
 long luat_mcu_ticks(void) {
     return GetTickCount();
 }
+
+int luat_mcu_us_period(void) {
+    return 1;
+}
+
+static uint64_t tick64;
+uint64_t luat_mcu_tick64(void) {
+    tick64 ++;
+    return tick64;
+}

8a39c328e16e104a0a3860de2d69c02fcc174bb3
Dozingfiretruck <1473454180@qq.com>
2023-12-14 17:35:05 +0800

    update:iotauth c lua 剥离

---
+++ components/iotauth/luat_iotauth.h
@@ -0,0 +1,41 @@
+#ifndef LUAT_LCD
+#define LUAT_LCD
+
+#define CLIENT_ID_LEN 192
+#define USER_NAME_LEN 192
+#define PASSWORD_LEN 256
+
+typedef struct iotauth_ctx
+{
+    char client_id[CLIENT_ID_LEN];
+    char user_name[USER_NAME_LEN];
+    char password[PASSWORD_LEN];
+}iotauth_ctx_t;
+
+void luat_aliyun_token(const char* product_key,const char* device_name,const char* device_secret,long long cur_timestamp,const char* method,uint8_t is_tls,char* client_id, char* user_name, char* password);
+void luat_onenet_token(const char* product_id,const char* device_name,const char* device_secret,long long cur_timestamp,char * method,char * version,char *token);
+void luat_iotda_token(const char* device_id,const char* device_secret,long long cur_timestamp,int ins_timestamp,char* client_id,const char* password);
+void luat_qcloud_token(const char* product_id,const char* device_name,const char* device_secret,long long cur_timestamp,const char* method,const char* sdk_appid,char* username,char* password);
+void luat_tuya_token(const char* device_id,const char* device_secret,long long cur_timestamp,const char* password);
+void luat_baidu_token(const char* iot_core_id,const char* device_key,const char* device_secret,const char* method,long long cur_timestamp,char* username,char* password);
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

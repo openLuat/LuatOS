8619968624008636213da49d1846d14e0d5229e5
Dozingfiretruck <1473454180@qq.com>
2023-12-19 10:13:14 +0800

    update:更新通用内存管理接口实现

--- luat/modules/luat_lib_mem.c
+++ luat/modules/luat_lib_mem.c
@@ -1,6 +1,6 @@
 
 #include "luat_base.h"
-#include "luat_malloc.h"
+#include "luat_mem.h"
 
 #define LUAT_LOG_TAG "mem"
 #include "luat_log.h"
@@ -22,11 +22,19 @@ LUAT_WEAK void* luat_heap_opt_realloc(LUAT_HEAP_TYPE_E type,void* ptr, size_t le
 }
 
 LUAT_WEAK void* luat_heap_opt_calloc(LUAT_HEAP_TYPE_E type,size_t count, size_t size){
-    return luat_heap_calloc(count, size);
+    return luat_heap_opt_zalloc(type,count*size);
 }
 
 LUAT_WEAK void* luat_heap_opt_zalloc(LUAT_HEAP_TYPE_E type,size_t size){
-    return luat_heap_zalloc(size);
+    void *ptr = luat_heap_opt_malloc(type,size);
+    if (ptr) {
+        memset(ptr, 0, size);
+    }
+    return ptr;
+}
+
+LUAT_WEAK void luat_meminfo_opt_sys(LUAT_HEAP_TYPE_E type,size_t* total, size_t* used, size_t* max_used){
+    luat_meminfo_sys(total, used, max_used);
 }
 
 LUAT_WEAK void* luat_heap_calloc(size_t count, size_t _size) {

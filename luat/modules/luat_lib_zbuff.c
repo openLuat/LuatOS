d36333e2660394c28a4f91d82c32e9ed7c79cc42
Dozingfiretruck <1473454180@qq.com>
2023-12-19 15:44:36 +0800

    update:默认使用zbuff.HEAP_SRAM

--- luat/modules/luat_lib_zbuff.c
+++ luat/modules/luat_lib_zbuff.c
@@ -97,7 +97,7 @@ uint32_t get_framebuffer_point(luat_zbuff_t *buff,uint32_t point)
 @api zbuff.create(length,data,type)
 @int 字节数
 @any 可选参数，number时为填充数据，string时为填充字符串
-@number 可选参数，内存类型，可选：zbuff.HEAP_AUTO(自动申请,如存在psram则在psram进行申请,如不存在或失败则在sram进行申请,默认) zbuff.HEAP_SRAM(内部sram) zbuff.HEAP_PSRAM(外部psram) 注意:此项与硬件支持有关
+@number 可选参数，内存类型，可选：zbuff.HEAP_SRAM(内部sram,默认) zbuff.HEAP_PSRAM(外部psram) zbuff.HEAP_AUTO(自动申请,如存在psram则在psram进行申请,如不存在或失败则在sram进行申请) 注意:此项与硬件支持有关
 @return object zbuff对象，如果创建失败会返回nil
 @usage
 -- 创建zbuff
@@ -109,7 +109,7 @@ local buff = zbuff.create(1024, "123321456654") -- 创建，并填充一个已�
 -- zbuff.create({width,height,bit},data,type)
 -- table 宽度、高度、色位深度
 @int 可选参数，填充数据
-@number 可选参数，内存类型，可选：zbuff.HEAP_AUTO(自动申请,如存在psram则在psram进行申请,如不存在或失败则在sram进行申请,默认) zbuff.HEAP_SRAM(内部sram) zbuff.HEAP_PSRAM(外部psram) 注意:此项与硬件支持有关
+@number 可选参数，内存类型，可选：zbuff.HEAP_SRAM(内部sram,默认) zbuff.HEAP_PSRAM(外部psram) zbuff.HEAP_AUTO(自动申请,如存在psram则在psram进行申请,如不存在或失败则在sram进行申请) 注意:此项与硬件支持有关
 @return object zbuff对象，如果创建失败会返回nil
 @usage
 -- 创建zbuff
@@ -142,7 +142,9 @@ static int l_zbuff_create(lua_State *L)
         return 0;
     }
     if (lua_isinteger(L, 3))
-    	buff->type = luaL_optinteger(L, 3, LUAT_HEAP_AUTO);
+    	buff->type = luaL_optinteger(L, 3, LUAT_HEAP_SRAM);
+    else
+        buff->type = LUAT_HEAP_SRAM;
     buff->addr = (uint8_t *)luat_heap_opt_malloc(buff->type,len);
     if (buff->addr == NULL)
     {

b9c20c62e876e842c1f7aa29d30bfd02d1543cf2
Wendal Chen <wendal1985@gmail.com>
2024-01-05 07:45:13 +0800

    fix: 修复air105编译报错,提示l_multimedia_raw_handler未定义

--- components/multimedia/luat_multimedia.h
+++ components/multimedia/luat_multimedia.h
@@ -66,4 +66,9 @@ void mp3_decoder_init(void *decoder);
 void mp3_decoder_set_debug(void *decoder, uint8_t onoff);
 int mp3_decoder_get_info(void *decoder, const uint8_t *input, uint32_t len, uint32_t *hz, uint8_t *channel);
 int mp3_decoder_get_data(void *decoder, const uint8_t *input, uint32_t len, int16_t *pcm, uint32_t *out_len, uint32_t *hz, uint32_t *used);
+
+#ifdef __LUATOS__
+int l_multimedia_raw_handler(lua_State *L, void* ptr);
+#endif
+
 #endif

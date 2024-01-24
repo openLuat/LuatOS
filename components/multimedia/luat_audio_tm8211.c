676da5749325e888e27a623cc0e4e99204570282
Dozingfiretruck <1473454180@qq.com>
2024-01-24 14:57:23 +0800

    update:8211实现注释

--- components/multimedia/luat_audio_tm8211.c
+++ components/multimedia/luat_audio_tm8211.c
@@ -6,19 +6,21 @@
 #define LUAT_LOG_TAG "tm8211"
 #include "luat_log.h"
 
-static int tm8211_codec_channels(luat_audio_codec_conf_t* conf,uint8_t channels){
-    return 0;
-}
 
 static int tm8211_codec_init(luat_audio_codec_conf_t* conf,uint8_t mode){
+    //如果有dac pin 初始化dac pin
+    //如果有pa pin 初始化pa pin
     return 0;
 }
 
 static int tm8211_codec_deinit(luat_audio_codec_conf_t* conf){
+    //如果有dac pin 控制断电后去初始化
+    //如果有pa pin 控制断电后去初始化
     return 0;
 }
 
 static void tm8211_codec_pa(luat_audio_codec_conf_t* conf,uint8_t on){
+    //如果pa_pin
 //    if (conf->pa_pin == LUAT_CODEC_PA_NONE) return;
 //	if (on){
 //        if (conf->dummy_time_len)
@@ -43,10 +45,12 @@ static int tm8211_codec_control(luat_audio_codec_conf_t* conf,luat_audio_codec_c
 }
 
 static int tm8211_codec_start(luat_audio_codec_conf_t* conf){
+    //dac pin 控制上电, pa_pin 控制使能功放
     return 0;
 }
 
 static int tm8211_codec_stop(luat_audio_codec_conf_t* conf){
+    //dac pin 控制断电, pa_pin 控制失能功放
     return 0;
 }
 

757dfd2402b46e7efc73a369dd216788c0a79e25
chenxuuu <lolicon@papapoi.com>
2024-01-30 17:15:16 +0800

    fix: 消息文档没生成

--- tools/sys_pub.py
+++ tools/sys_pub.py
@@ -135,7 +135,7 @@ for file in file_list:
 ##################  接口数据提取完毕  ##################
 
 os.remove("../../luatos-wiki/api/sys_pub.md")
-doc = open("../../luatos-wiki/api/sys_pub.md", "a+",encoding='utf-8')
+doc = open("../../luatos-wiki/api/sys_pub.md", "w+",encoding='utf-8')
 doc.write("# 📮 sys系统消息\n")
 doc.write("\n\n")
 doc.write("此处列举了LuatOS框架中自带的系统消息列表\n\n")

fc0ac1b998a2557cd707a0503b3ef3663d114783
Dozingfiretruck <1473454180@qq.com>
2024-02-05 00:44:56 +0800

    fix:录音失效

--- components/multimedia/luat_multimedia_audio.c
+++ components/multimedia/luat_multimedia_audio.c
@@ -227,8 +227,9 @@ LUAT_WEAK int luat_audio_pm_request(uint8_t multimedia_id,luat_audio_pm_mode_t m
     if (audio_conf!=NULL && audio_conf->bus_type == MULTIMEDIA_AUDIO_BUS_I2S){
         switch (mode){
         case AUDIO_PM_MODE_RESUME:
-            if (!audio_conf->speech_uplink_type && !audio_conf->speech_downlink_type && !audio_conf->record_mode)
-				luat_audio_play_blank(multimedia_id);
+            //同下,何时传输空白音
+            // if (!audio_conf->speech_uplink_type && !audio_conf->speech_downlink_type && !audio_conf->record_mode)
+			// 	luat_audio_play_blank(multimedia_id);
             audio_conf->codec_conf.codec_opts->start(&audio_conf->codec_conf);
 			audio_conf->wakeup_ready = 0;
 			audio_conf->pa_on_enable = 0;
@@ -239,6 +240,7 @@ LUAT_WEAK int luat_audio_pm_request(uint8_t multimedia_id,luat_audio_pm_mode_t m
             audio_conf->codec_conf.codec_opts->stop(&audio_conf->codec_conf);
             //非控制的关闭i2s输出?输出白噪音?此处或codec具体处理
             // luat_i2s_close(audio_conf->codec_conf.i2s_id);
+            audio_conf->is_sleep = 1;
             break;
         case AUDIO_PM_MODE_SHUTDOWN:
             audio_conf->codec_conf.codec_opts->control(&audio_conf->codec_conf,LUAT_CODEC_SET_PA,0);

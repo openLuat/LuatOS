68fefdbbaff194672f02a6c115086f1f8f63b2a1
doudou <1491766334@qq.com>
2024-03-01 15:45:38 +0800

    update:ipv6 demo添加718P型号判断

--- demo/ipv6/client/main.lua
+++ demo/ipv6/client/main.lua
@@ -27,9 +27,9 @@ end
 -- 演示task
 function ipv6test()
     -- 仅EC618系列支持, 例如Air780E/Air600E/Air780UG/Air700E
-    if rtos.bsp() ~= "EC618" then
+    if rtos.bsp() ~= "EC618" and rtos.bsp() ~= "EC718P" then
         while 1 do
-            log.info("ipv6", "only Air780E/Air600E/Air780UG/Air700E supported")
+            log.info("ipv6", "only Air780E/Air600E/Air780UG/Air700E/Air780EP supported")
             sys.wait(1000)
         end
     end

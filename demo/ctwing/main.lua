cad663fe1e97aa46198c814558797a5dea3cda64
Wendal Chen <wendal1985@gmail.com>
2024-03-04 16:55:06 +0800

    fix: demo/ctwing直接不能跑的, 修正之

--- demo/ctwing/main.lua
+++ demo/ctwing/main.lua
@@ -3,11 +3,11 @@ VERSION = '2.0.0'
 
 LOG_LEVEL = log.LOG_INFO
 log.setLevel(LOG_LEVEL )
-require 'ctwing'
 local sys = require "sys"
 _G.sysplus = require("sysplus")
 
 
+require 'ctwingdemo'
 
 
 sys.run()

e464d82c39ba46f498827e0ed7a51598c6b8e7e8
Wendal Chen <wendal1985@gmail.com>
2024-05-31 07:56:50 +0800

    fix: LITTLE_FLASH_CHIP_TABLE多了个空元素,导致Wmissing-braces警告

--- components/little_flash/inc/little_flash_table.h
+++ components/little_flash/inc/little_flash_table.h
@@ -18,7 +18,7 @@ extern "C" {
 #define LITTLE_FLASH_CHIP_TABLE                                                                              \
 {                                                                                                            \
     {"W25N01GVZEIG", LF_MF_ID_WINBOND, 0xAA21, LF_DRIVER_NAND_FLASH, 128L*1024L*1024L, 0xD8, 64L*2048L},     \
-    {"W25Q128FVSG", LF_MF_ID_WINBOND, 0x4018, LF_DRIVER_NOR_FLASH, 16L*1024L*1024L, 0x20, 4096L},            \
+    {"W25Q128FVSG", LF_MF_ID_WINBOND, 0x4018, LF_DRIVER_NOR_FLASH, 16L*1024L*1024L, 0x20, 4096L}             \
 }
 
 #ifdef __cplusplus

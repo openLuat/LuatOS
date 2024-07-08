e672eba774c4e441cc85b1b329de22f9003ac98c
alienwalker <alienwalker@sina.com>
2024-07-08 08:46:58 +0800

    add：新增死机后处理模式

--- luat/modules/luat_lib_mcu.c
+++ luat/modules/luat_lib_mcu.c
@@ -237,7 +237,7 @@ LUAT_WEAK int luat_i2c_set_iomux(int id, uint8_t value){return -1;}
 /*
 mcu死机时处理模式，目前只有EC618平台适用
 @api mcu.hardfault(mode)
-@int 处理模式，0死机停机，1死机后重启，2死机后尽量将错误信息提交给外部工具后重启
+@int 处理模式，0死机停机，1死机后重启，2死机后尽量将错误信息提交给外部工具后重启 3.死机时写入关键信息到flash后立刻重启
 @usage
 mcu.hardfault(0)	--死机后停机，一般用于调试状态
 mcu.hardfault(1)	--死机后重启，一般用于正式产品

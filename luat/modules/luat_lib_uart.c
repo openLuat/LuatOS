d7483e02223ac56c63b5f14c702490da01bf346b
alienwalker <alienwalker@sina.com>
2024-07-22 18:41:07 +0800

    fix:softuart，在没有发送完成的情况下就关闭，之后再打开无法发送数据了

--- luat/modules/luat_lib_uart.c
+++ luat/modules/luat_lib_uart.c
@@ -232,6 +232,7 @@ static void luat_uart_soft_close(void)
 		luat_gpio_close(prv_uart_soft->pin485);
 	}
 	luat_heap_alloc(NULL, prv_uart_soft->rx_buffer.Data, 0, 0);
+	prv_uart_soft->rx_buffer.Data = NULL;
 	memset(&prv_uart_soft->rx_buffer, 0, sizeof(Buffer_Struct));
 	prv_uart_soft->is_tx_busy = 0;
 	prv_uart_soft->is_rx_busy= 0;

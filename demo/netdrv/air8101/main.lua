2d4fbb1e0c327596928b81c95cce45e48d6c523b
Wendal Chen <wendal1985@gmail.com>
2025-01-20 11:18:42 +0800

    update: 更新netdrv的以太网demo

---
+++ demo/netdrv/air8101/main.lua
@@ -0,0 +1,32 @@
+
+-- LuaTools需要PROJECT和VERSION这两个信息
+PROJECT = "netdrv"
+VERSION = "1.0.1"
+
+
+-- sys库是标配
+_G.sys = require("sys")
+--[[特别注意, 使用mqtt库需要下列语句]]
+_G.sysplus = require("sysplus")
+
+sys.taskInit(function()
+    sys.wait(1000)
+    netdrv.setup(socket.LWIP_ETH)
+    netdrv.dhcp(socket.LWIP_ETH)
+end)
+
+sys.taskInit(function()
+    -- sys.waitUntil("IP_READY")
+    while 1 do
+        sys.wait(6000)
+        log.info("http", http.request("GET", "http://httpbin.air32.cn/bytes/4096", nil, nil, {adapter=socket.LWIP_ETH}).wait())
+        log.info("lua", rtos.meminfo())
+        log.info("sys", rtos.meminfo("sys"))
+        log.info("ip", socket.localIP(socket.LWIP_ETH))
+    end
+end)
+
+-- 用户代码已结束---------------------------------------------
+-- 结尾总是这一句
+sys.run()
+-- sys.run()之后后面不要加任何语句!!!!!

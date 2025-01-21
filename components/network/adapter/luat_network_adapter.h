0356b5a4d3bc7063cb75150bf34e05cc468ea197
alienwalker <alienwalker@sina.com>
2025-01-21 10:37:49 +0800

    update:部分屏幕需要RB SWAP

--- components/network/adapter/luat_network_adapter.h
+++ components/network/adapter/luat_network_adapter.h
@@ -2,7 +2,7 @@
 #define __LUAT_NW_ADAPTER_H__
 #include "luat_base.h"
 // #ifdef LUAT_USE_NETWORK
-#if defined(LUAT_EC7XX_CSDK) || defined(CHIP_EC618) || defined(__AIR105_BSP__)
+#if defined(LUAT_EC7XX_CSDK) || defined(CHIP_EC618) || defined(__AIR105_BSP__) || defined(CONFIG_SOC_8910) || defined(CONFIG_SOC_8850)
 #include "bsp_common.h"
 #endif
 #include "luat_rtos.h"

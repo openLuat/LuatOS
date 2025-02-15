5c49135a1aa055460c4b08e2c0b1502c6d26c78f
Wendal Chen <wendal1985@gmail.com>
2025-02-15 21:56:08 +0800

    change: 补充air8101设置静态ip的方法, 需要2025.2.15之后编译的固件,否则会死机

--- demo/netdrv/air8101/main.lua
+++ demo/netdrv/air8101/main.lua
@@ -10,17 +10,21 @@ _G.sys = require("sys")
 _G.sysplus = require("sysplus")
 
 sys.taskInit(function()
-    sys.wait(1000)
+    sys.wait(500)
     gpio.setup(13, 1, gpio.PULLUP) -- 打开开发板的LDO供电,否则3.3V没电
     netdrv.setup(socket.LWIP_ETH)
+    -- log.info("设置启用DHCP")
     netdrv.dhcp(socket.LWIP_ETH, true)
+    -- log.info("设置静态IPV4")
+    -- netdrv.ipv4(socket.LWIP_ETH, "192.168.1.129", "255.255.255.0", "192.168.1.1")
+    -- log.info("ip", socket.localIP(socket.LWIP_ETH))
 end)
 
 sys.taskInit(function()
     -- sys.waitUntil("IP_READY")
     while 1 do
         sys.wait(6000)
-        log.info("http", http.request("GET", "http://httpbin.air32.cn/bytes/4096", nil, nil, {adapter=socket.LWIP_ETH}).wait())
+        log.info("http", http.request("GET", "http://httpbin.air32.cn/get", nil, nil, {adapter=socket.LWIP_ETH,timeout=3000}).wait())
         log.info("lua", rtos.meminfo())
         log.info("sys", rtos.meminfo("sys"))
         log.info("ip", socket.localIP(socket.LWIP_ETH))

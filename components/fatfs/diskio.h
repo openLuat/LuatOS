f38aa01b8eff5b5345fcff8f87d54cd76d3adfe6
Dozingfiretruck <1473454180@qq.com>
2025-02-18 17:49:55 +0800

    update:fatfs to 15.a

--- components/fatfs/diskio.h
+++ components/fatfs/diskio.h
@@ -54,6 +54,7 @@ DRESULT disk_read (BYTE pdrv, BYTE* buff, LBA_t sector, UINT count);
 DRESULT disk_write (BYTE pdrv, const BYTE* buff, LBA_t sector, UINT count);
 DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 
+
 /* Disk Status Bits (DSTATUS) */
 
 #define STA_NOINIT		0x01	/* Drive not initialized */

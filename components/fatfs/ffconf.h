ad61f0b982312b9c8e03d8ba871d50c69bed6229
Dozingfiretruck <1473454180@qq.com>
2025-04-15 10:33:00 +0800

    update:关闭FF_FS_REENTRANT

--- components/fatfs/ffconf.h
+++ components/fatfs/ffconf.h
@@ -286,7 +286,7 @@
 /      lock control is independent of re-entrancy. */
 
 
-#define FF_FS_REENTRANT	1
+#define FF_FS_REENTRANT	0
 #define FF_FS_TIMEOUT	1000
 /* The option FF_FS_REENTRANT switches the re-entrancy (thread safe) of the FatFs
 /  module itself. Note that regardless of this option, file access to different

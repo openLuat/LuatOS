070a9f9f1af97b209432947efd242624b2a1d0cb
Wendal Chen <wendal1985@gmail.com>
2025-04-18 10:06:02 +0800

    fix: airlink,drv_uart缺了个函数声明

--- components/airlink/include/luat_airlink.h
+++ components/airlink/include/luat_airlink.h
@@ -235,6 +235,7 @@ int luat_airlink_drv_wlan_ap_stop(void);
 int luat_airlink_drv_uart_setup(luat_uart_t* uart);
 int luat_airlink_drv_uart_write(int uart_id, void* data, size_t length);
 int luat_airlink_drv_uart_read(int uart_id, void* buffer, size_t length);
+int luat_airlink_drv_uart_close(int uart_id);
 
 extern uint32_t g_airlink_debug;
 

8dff5cd876142c87b009ba903f96c64d4d7ba7dc
Wendal Chen <wendal1985@gmail.com>
2025-04-22 14:40:15 +0800

    change: gpio,新增VBUS,USIM_DET常量

--- luat/modules/luat_lib_gpio.c
+++ luat/modules/luat_lib_gpio.c
@@ -730,24 +730,26 @@ static const rotable_Reg_t reg_gpio[] =
     { "LOW_IRQ",        ROREG_INT(Luat_GPIO_LOW_IRQ)},
 #if (defined CHIP_EC618) || (defined CHIP_EC718) || (defined CHIP_EC716)
 
-    //@const WAKEUP0 number 休眠唤醒脚0
+    //@const WAKEUP0 number 休眠唤醒脚0,不支持输出
     { "WAKEUP0",         ROREG_INT(HAL_WAKEUP_0)},
-    //@const WAKEUP1 number 休眠唤醒脚1
+    //@const WAKEUP1 number VBUS,USB唤醒脚,不支持输出
+    { "VBUS",            ROREG_INT(HAL_WAKEUP_1)},
     { "WAKEUP1",         ROREG_INT(HAL_WAKEUP_1)},
-    //@const WAKEUP2 number 休眠唤醒脚2
+    //@const WAKEUP2 number USIM热插拔脚,不支持输出
+    { "USIM_DET",        ROREG_INT(HAL_WAKEUP_2)},
     { "WAKEUP2",         ROREG_INT(HAL_WAKEUP_2)},
 #if (defined CHIP_EC718) || (defined CHIP_EC716)
-    //@const WAKEUP3 number 休眠唤醒脚3
+    //@const WAKEUP3 number 休眠唤醒脚3,与GPIO20是同一个引脚
     { "WAKEUP3",         ROREG_INT(HAL_WAKEUP_3)},
-    //@const WAKEUP4 number 休眠唤醒脚4
+    //@const WAKEUP4 number 休眠唤醒脚4,与GPIO21是同一个引脚
     { "WAKEUP4",         ROREG_INT(HAL_WAKEUP_4)},
-    //@const WAKEUP5 number 休眠唤醒脚5
+    //@const WAKEUP5 number 休眠唤醒脚5,与GPIO22是同一个引脚
     { "WAKEUP5",         ROREG_INT(HAL_WAKEUP_5)},
-    //@const WAKEUP6 number 休眠唤醒脚6
+    //@const WAKEUP6 number 休眠唤醒脚6,,不支持输出
     { "WAKEUP6",         ROREG_INT(HAL_WAKEUP_CHARGE)},
     { "CHG_DET",         ROREG_INT(HAL_WAKEUP_CHARGE)},
 #endif
-	//@const PWR_KEY number WAKEUP_PAD
+	//@const PWR_KEY number 开机键,支持双向触发中断,不支持输出
 	{ "PWR_KEY",         ROREG_INT(HAL_WAKEUP_PWRKEY)},
 #endif
 	{ NULL,             ROREG_INT(0) }

9d94ba73ecca16831b12130087e11e2864e6ce78
Dozingfiretruck <1473454180@qq.com>
2025-04-25 15:28:16 +0800

    update: tp.init支持型号

--- components/tp/luat_lib_tp.c
+++ components/tp/luat_lib_tp.c
@@ -93,7 +93,7 @@ int l_tp_callback(luat_tp_config_t* luat_tp_config, luat_tp_data_t* luat_tp_data
 /*
 触摸初始化
 @api tp.init(tp, args)
-@string tp类型，当前支持：<br>gt911
+@string tp类型，当前支持：<br>gt911 <br>gt9157 <br>jd9261t
 @table 附加参数,与具体设备有关：<br>port 驱动方式<br>port：硬件i2c端口,例如0,1,2...如果为软件i2c对象<br>pin_rst：复位引脚<br>pin_int：中断引脚<br>w:宽度<br>h:高度
 
 @usage

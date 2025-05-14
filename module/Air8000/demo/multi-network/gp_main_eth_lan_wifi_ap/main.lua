21639739c382073eeea099da8935e2521b156784
梁健 <liangjian@airm2m.com>
2025-05-14 18:14:09 +0800

    fix: 修改错误的air8000多网融合demo

--- module/Air8000/demo/multi-network/gp_main_eth_lan_wifi_ap/main.lua
+++ module/Air8000/demo/multi-network/gp_main_eth_lan_wifi_ap/main.lua
@@ -3,6 +3,12 @@
 PROJECT = "air8000_wifi"
 VERSION = "1.0.5"
 
+--[[
+本demo演示的功能是:
+1. 创建AP, 提供wifi设备, 通过4G上网
+2. 创建以太网, 为局域网内的设备, 通过4G上网
+]]
+
 dnsproxy = require("dnsproxy")
 dhcpsrv = require("dhcpsrv")
 httpplus = require("httpplus")
@@ -14,40 +20,30 @@ function PWR8000S(val)
     gpio.set(23, val)
 end
 
-gpio.debounce(0, 1000)
-gpio.setup(0, function()
-    sys.taskInit(function()
-        log.info("复位Air8000S")
-        PWR8000S(0)
-        sys.wait(20)
-        PWR8000S(1)
-    end)
-end, gpio.PULLDOWN)
 
 function test_ap()
-    log.info("执行AP创建操作")
+    log.info("执行AP创建操作", airlink.ready() , "正常吗?")
     wlan.createAP("uiot5678", "12345678")
     netdrv.ipv4(socket.LWIP_AP, "192.168.4.1", "255.255.255.0", "0.0.0.0")
-    sys.wait(5000)
-    dnsproxy.setup(socket.LWIP_AP, socket.LWIP_ETH)
+    log.info("netdrv", "等待AP就绪")
+    while netdrv.ready(socket.LWIP_AP) ~= true do
+        sys.wait(100)
+    end
+    log.info("netdrv", "创建dhcp服务器, 供AP使用")
     dhcpsrv.create({adapter=socket.LWIP_AP})
+    log.info("netdrv", "等待4G就绪")
+    while netdrv.ready(socket.LWIP_GP) ~= true do
+        sys.wait(100)
+    end
+    log.info("netdrv", "创建dns代理服务, 供AP使用")
+    dnsproxy.setup(socket.LWIP_AP, socket.LWIP_GP)
     while 1 do
-        if netdrv.ready(socket.LWIP_ETH) then
-            log.info("以太网作为网关")
-            netdrv.napt(socket.LWIP_ETH)
+        if netdrv.ready(socket.LWIP_GP) then
+            log.info("netdrv", "4G作为网关")
+            netdrv.napt(socket.LWIP_GP)
             break
         end
         sys.wait(1000)
-    end
-    icmp.setup(socket.LWIP_ETH)
-    while 1 do
-        -- 持续ping网关
-        local ip,mark,gw = netdrv.ipv4(socket.LWIP_ETH)
-        if gw then
-            log.info("ping", gw)
-            icmp.ping(socket.LWIP_ETH, gw)
-        end
-        sys.wait(3000)
     end
 end
 
@@ -75,61 +71,46 @@ end)
 --     end
 -- end)
 
+
 function eth_lan()
-    sys.wait(500)
-    log.info("ch390", "打开LDO供电")
-    gpio.setup(140, 1, gpio.PULLUP)     --打开ch390供电
-    sys.wait(6000)
+    -- sys.wait(3000)
     local result = spi.setup(
-        1,--spi_id
+        1,--spi id
         nil,
         0,--CPHA
         0,--CPOL
         8,--数据宽度
-        25600000--,--频率
-        -- spi.MSB,--高低位顺序    可选，默认高位在前
-        -- spi.master,--主模式     可选，默认主
-        -- spi.full--全双工       可选，默认全双工
+        51200000--,--波特率
     )
-    log.info("main", "open",result)
+    log.info("main", "open spi",result)
     if result ~= 0 then--返回值为0，表示打开成功
         log.info("main", "spi open error",result)
         return
     end
-
-    -- 初始化指定netdrv设备,
-    -- socket.LWIP_ETH 网络适配器编号
-    -- netdrv.CH390外挂CH390
-    -- SPI ID 1, 片选 GPIO12
-    netdrv.setup(socket.LWIP_ETH, netdrv.CH390, {spi=1,cs=12})
-    sys.wait(3000)
-    local ipv4,mark, gw = netdrv.ipv4(socket.LWIP_ETH, "192.168.4.1", "255.255.255.0", "192.168.4.1")
-    log.info("ipv4", ipv4,mark, gw)
-    while netdrv.link(socket.LWIP_ETH) ~= true do
+    log.info("netdrv", "初始化以太网")
+    netdrv.setup(socket.LWIP_ETH, netdrv.CH390, {spi=1,cs=12,irq=255})
+    log.info("netdrv", "等待以太网就绪")
+    sys.wait(1000)
+    netdrv.ipv4(socket.LWIP_ETH, "192.168.5.1", "255.255.255.0", "0.0.0.0")
+    while netdrv.ready(socket.LWIP_ETH) ~= true do
+        -- log.info("netdrv", "等待以太网就绪")
         sys.wait(100)
     end
-    while netdrv.link(socket.LWIP_GP) ~= true do
-        sys.wait(100)
-    end
-    sys.wait(2000)
-    dhcpsrv.create({adapter=socket.LWIP_ETH})
+    log.info("netdrv", "以太网就绪")
+    log.info("netdrv", "创建dhcp服务器, 供以太网使用")
+    dhcpsrv.create({adapter=socket.LWIP_ETH, gw={192,168,5,1}})
+    log.info("netdrv", "创建dns代理服务, 供以太网使用")
     dnsproxy.setup(socket.LWIP_ETH, socket.LWIP_GP)
-    netdrv.napt(socket.LWIP_GP)
-    if iperf then
-        log.info("启动iperf服务器端")
-        iperf.server(socket.LWIP_ETH)
-    end
 end
 
 sys.taskInit(function()
+    while airlink.ready() ~= true do
+        sys.wait(100)
+    end
     eth_lan()
     wlan.init()
     sys.wait(300)
     test_ap()
-    while 1 do
-        log.info("测试使用4G 给以太网和wifi 设备供应网络")
-        sys.wait(1000)
-    end
 end)
 
 -- 用户代码已结束---------------------------------------------

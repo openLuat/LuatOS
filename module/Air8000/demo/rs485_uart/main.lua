8c008824973ea63bcf3d6c22cd2595a3aaed2414
马梦阳 <mamengyang@airm2m.com>
2025-05-23 01:20:55 +0800

    fix：修改rs485代码，并单独新建一个目录。

--- module/Air8000/demo/rs485_uart/main.lua
+++ module/Air8000/demo/rs485_uart/main.lua
@@ -1,7 +1,8 @@
--- Luatools需要PROJECT和VERSION这两个信息
-PROJECT = "uart_RS485"
+-- LuaTools需要PROJECT和VERSION这两个信息
+PROJECT = "rs485_uart"
 VERSION = "1.0.0"
 
+-- 打印版本信息
 log.info("main", PROJECT, VERSION)
 
 -- 引入必要的库文件(lua编写), 内部库不需要require
@@ -13,15 +14,18 @@ if wdt then
     sys.timerLoopStart(wdt.feed, 3000)--3s喂一次狗
 end
 
-log.info("main", "uart demo run......")
+-- 打印提示信息
+log.info("main", "rs485 uart demo")
 
+-- 定义串口ID和485引脚
 local uartid = 1        -- 根据实际设备选取不同的uartid
-local uart485Pin = 27   -- 用于控制485接收和发送的使能引脚
+local uart485Pin = 17   -- 用于控制485收发转换的方向转换脚
 
-gpio.setup(1, 1)        --打开电源(开发板485供电脚是gpio1，用开发板测试需要开机初始化拉高gpio1)
+-- 打开485供电使能脚
+gpio.setup(16, 1)       -- 打开电源(开发板485供电脚是gpio16，用开发板测试需要开机初始化拉高gpio16)
 
---初始化
-uart.setup(uartid, 9600, 8, 1, uart.NONE, uart.LSB, 1024, uart485Pin, 0, 2000)
+-- 初始化串口参数
+uart.setup(uartid, 9600, 8, 1, uart.NONE, uart.LSB, 1024, uart485Pin, 0, 20000)
 
 -- 收取数据会触发回调, 这里的"receive" 是固定值
 uart.on(uartid, "receive", function(id, len)
@@ -40,7 +44,10 @@ sys.taskInit(function()
     -- 循环两秒向串口发一次数据
     while true do
         sys.wait(2000)
+        -- 写入可见字符串
         uart.write(uartid, "test data.")
+        -- 写入十六进制字符串
+        -- uart.write(1, string.char(0x55,0xAA,0x4B,0x03,0x86))
     end
 end)
 

6aa0382b28077c128662b6813faa37868bdcd873
Dozingfiretruck <1473454180@qq.com>
2025-05-27 11:59:34 +0800

    update:同步变量名

--- luat/demo/lcd/main.lua
+++ luat/demo/lcd/main.lua
@@ -188,7 +188,7 @@ if tp then
             while 1 do 
                 local result, tp_device, tp_data = sys.waitUntil("TP")
                 if result then
-                    if tp_data[1].event == tp.EVENT_TYPE_DOWN or tp_data[1].event == tp.EVENT_TYPE_MOVE then
+                    if tp_data[1].event == tp.EVENT_DOWN or tp_data[1].event == tp.EVENT_MOVE then
                         lcd.drawPoint(tp_data[1].x, tp_data[1].y, 0xF800)
                         if lcd_use_buff then
                             lcd.flush()

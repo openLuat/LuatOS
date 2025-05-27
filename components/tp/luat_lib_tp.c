6c123cd33ae96944bfe21adea2664cb8a422b6a4
Dozingfiretruck <1473454180@qq.com>
2025-05-27 17:36:14 +0800

    update:完善触摸函数注释

--- components/tp/luat_lib_tp.c
+++ components/tp/luat_lib_tp.c
@@ -95,6 +95,8 @@ int l_tp_callback(luat_tp_config_t* luat_tp_config, luat_tp_data_t* luat_tp_data
 @api tp.init(tp, args)
 @string tp类型，当前支持：<br>gt911 <br>gt9157 <br>jd9261t
 @table 附加参数,与具体设备有关：<br>port 驱动方式<br>port：硬件i2c端口,例如0,1,2...如果为软件i2c对象<br>pin_rst：复位引脚<br>pin_int：中断引脚<br>w:宽度<br>h:高度
+@function 回调函数, 回调参数:tp_device,tp_data:触摸数据,内部为多个触摸点数据表,每个表中有参数event:触摸事件 x:x坐标 y:y坐标 
+@return tp_device
 
 @usage
 // tp.init("gt911",{port=0,pin_rst = 22,pin_int = 23,w = 320,h = 480})

14dae50f1bc575dd4ce6f86fa3e8c917a19602c8
黄何 <h1095798460@163.com>
2025-06-09 17:45:24 +0800

    update:删除Air780EHV的audio和record的demo中没用的语句

--- module/Air780EHV/DEMO/audio/main.lua
+++ module/Air780EHV/DEMO/audio/main.lua
@@ -1,6 +1,5 @@
-
 -- LuaTools需要PROJECT和VERSION这两个信息
-PROJECT = "audio"
+PROJECT = "testtts"
 VERSION = "1.0.0"
 
 --[[
@@ -11,43 +10,50 @@ VERSION = "1.0.0"
 _G.sys = require("sys")
 _G.sysplus = require("sysplus")
 
+-- gpio.setup(24, 1, gpio.PULLUP)          -- i2c工作的电压域
 
-local i2c_id = 0            -- i2c_id 0
+local i2c_id = 0 -- i2c_id 0
 
-local pa_pin = 162           -- 喇叭pa功放脚
-local power_pin = 27         -- es8311电源脚
+local pa_pin = 28 -- 喇叭pa功放脚
+local power_pin = 20 -- es8311电源脚
 
-local i2s_id = 0            -- i2s_id 0
-local i2s_mode = 0          -- i2s模式 0 主机 1 从机
-local i2s_sample_rate = 16000   -- 采样率
-local i2s_bits_per_sample = 16  -- 数据位数
-local i2s_channel_format = i2s.MONO_R   -- 声道, 0 左声道, 1 右声道, 2 立体声
-local i2s_communication_format = i2s.MODE_LSB   -- 格式, 可选MODE_I2S, MODE_LSB, MODE_MSB
-local i2s_channel_bits = 16     -- 声道的BCLK数量
+local i2s_id = 0 -- i2s_id 0
+local i2s_mode = 0 -- i2s模式 0 主机 1 从机
+local i2s_sample_rate = 16000 -- 采样率
+local i2s_bits_per_sample = 16 -- 数据位数
+local i2s_channel_format = i2s.MONO_R -- 声道, 0 左声道, 1 右声道, 2 立体声
+local i2s_communication_format = i2s.MODE_LSB -- 格式, 可选MODE_I2S, MODE_LSB, MODE_MSB
+local i2s_channel_bits = 16 -- 声道的BCLK数量
 
-local multimedia_id = 0         -- 音频通道 0
-local pa_on_level = 1           -- PA打开电平 1 高电平 0 低电平
-local power_delay = 3           -- 在DAC启动前插入的冗余时间，单位100ms
-local pa_delay = 100            -- 在DAC启动后，延迟多长时间打开PA，单位1ms
-local power_on_level = 1        -- 电源控制IO的电平，默认拉高
-local power_time_delay = 100    -- 音频播放完毕时，PA与DAC关闭的时间间隔，单位1ms
+local multimedia_id = 0 -- 音频通道 0
+local pa_on_level = 1 -- PA打开电平 1 高电平 0 低电平
+local power_delay = 3 -- 在DAC启动前插入的冗余时间，单位100ms
+local pa_delay = 100 -- 在DAC启动后，延迟多长时间打开PA，单位1ms
+local power_on_level = 1 -- 电源控制IO的电平，默认拉高
+local power_time_delay = 100 -- 音频播放完毕时，PA与DAC关闭的时间间隔，单位1ms
 
-local voice_vol = 70        -- 喇叭音量
-local mic_vol = 80          -- 麦克风音量
-
-gpio.setup(power_pin, 1, gpio.PULLUP)         
+local voice_vol = 50 -- 喇叭音量
+local mic_vol = 80 -- 麦克风音量
+gpio.setup(power_pin, 1, gpio.PULLUP)
 gpio.setup(pa_pin, 1, gpio.PULLUP)
 
-function audio_setup()
-    pm.power(pm.LDO_CTL, false)  --开发板上ES8311由LDO_CTL控制上下电
-    sys.wait(100)
-    pm.power(pm.LDO_CTL, true)  --开发板上ES8311由LDO_CTL控制上下电
+pins.setup(58, "I2C0_SDA")
+pins.setup(57, "I2C0_SCL")
 
-    i2c.setup(i2c_id,i2c.FAST)      --设置i2c
-    i2s.setup(i2s_id, i2s_mode, i2s_sample_rate, i2s_bits_per_sample, i2s_channel_format, i2s_communication_format,i2s_channel_bits)    --设置i2s
+function audio_setup()
+
+    sys.wait(200)
+
+    i2c.setup(i2c_id, i2c.FAST)
+    i2s.setup(i2s_id, i2s_mode, i2s_sample_rate, i2s_bits_per_sample, i2s_channel_format, i2s_communication_format,
+        i2s_channel_bits)
 
     audio.config(multimedia_id, pa_pin, pa_on_level, power_delay, pa_delay, power_pin, power_on_level, power_time_delay)
-    audio.setBus(multimedia_id, audio.BUS_I2S,{chip = "es8311",i2cid = i2c_id , i2sid = i2s_id, voltage = audio.VOLTAGE_1800})	--通道0的硬件输出通道设置为I2S
+    audio.setBus(multimedia_id, audio.BUS_I2S, {
+        chip = "es8311",
+        i2cid = i2c_id,
+        i2sid = i2s_id
+    }) -- 通道0的硬件输出通道设置为I2S
 
     audio.vol(multimedia_id, voice_vol)
     audio.micVol(multimedia_id, mic_vol)
@@ -59,12 +65,12 @@ sys.taskInit(audio_setup)
 
 local taskName = "task_audio"
 
-local MSG_MD = "moreData"   -- 播放缓存有空余
-local MSG_PD = "playDone"   -- 播放完成所有数据
+local MSG_MD = "moreData" -- 播放缓存有空余
+local MSG_PD = "playDone" -- 播放完成所有数据
 
 audio.on(0, function(id, event)
-    --使用play来播放文件时只有播放完成回调
-    local succ,stop,file_cnt = audio.getError(0)
+    -- 使用play来播放文件时只有播放完成回调
+    local succ, stop, file_cnt = audio.getError(0)
     if not succ then
         if stop then
             log.info("用户停止播放")
@@ -75,15 +81,48 @@ audio.on(0, function(id, event)
     sysplus.sendMsg(taskName, MSG_PD)
 end)
 
-
 local function audio_task()
-    local result    
+    local result
     sys.waitUntil("AUDIO_READY")
+    -- 初始化spi flash, 如果是极限版TTS_ONCHIP,就不需要初始化
+    if sfud then
+        spi_flash = spi.deviceSetup(1, 12, 0, 0, 8, 25600000, spi.MSB, 1, 0)
+        local ret = sfud.init(spi_flash)
+        if ret then
+            log.info("sfud.init ok")
+        else
+            log.info("sfud.init error", ret)
+            return
+        end
+    else
+        log.info("tts", "TTS_ONCHIP?? skip sfud")
+    end
+
+    -- 本例子是按行播放 "千字文", 文本来源自wiki百科
+    local fd = nil
+    local line = nil
     while true do
         log.info("开始播放")
-        result = audio.play(0,"/luadb/1.mp3")
+        line = nil
+        if not fd then
+            fd = io.open("/luadb/qianzw.txt")
+        end
+        if fd then
+            line = fd:read("*l")
+            if line == nil then
+                fd:close()
+                fd = nil
+            end
+        end
+        if line == nil then
+            line =
+                "一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十"
+        end
+        line = line:trim()
+        log.info("播放内容", line)
+        result = audio.tts(0, line)
         if result then
-        --等待音频通道的回调消息，或者切换歌曲的消息
+            -- 等待音频通道的回调消息，或者切换歌曲的消息
             while true do
                 msg = sysplus.waitMsg(taskName, nil)
                 if type(msg) == 'table' then
@@ -104,19 +143,15 @@ local function audio_task()
             audio.playStop(0)
         end
         if audio.pm then
-		    audio.pm(0,audio.STANDBY)       --PM模式 待机模式，PA断电，codec待机状态，系统不能进低功耗状态，如果PA不可控，codec进入静音模式
+            audio.pm(0, audio.STANDBY)
         end
-		-- audio.pm(0,audio.SHUTDOWN)	--低功耗可以选择SHUTDOWN或者POWEROFF，如果codec无法断电用SHUTDOWN
+        -- audio.pm(0,audio.SHUTDOWN)	--低功耗可以选择SHUTDOWN或者POWEROFF，如果codec无法断电用SHUTDOWN
         log.info("mem", "sys", rtos.meminfo("sys"))
         log.info("mem", "lua", rtos.meminfo("lua"))
         sys.wait(1000)
     end
+    sysplus.taskDel(taskName)
 end
-
-sys.timerLoopStart(function()
-    log.info("mem.lua", rtos.meminfo())
-    log.info("mem.sys", rtos.meminfo("sys"))
- end, 3000)
 
 sysplus.taskInitEx(audio_task, taskName)
 

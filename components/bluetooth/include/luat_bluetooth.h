f0d66bda37eb97564fdb49852248e9b02dc8a4c2
Dozingfiretruck <1473454180@qq.com>
2025-06-10 15:30:44 +0800

    update:完善ble扫描

--- components/bluetooth/include/luat_bluetooth.h
+++ components/bluetooth/include/luat_bluetooth.h
@@ -7,6 +7,8 @@
 #define LUAT_BLUETOOTH_TYPE "BLUETOOTH*"
 #define LUAT_BLE_TYPE "BLE*"
 
+#define LUAT_BLUETOOTH_MAC_LEN    6
+
 typedef struct luat_bluetooth{
     luat_ble_t* luat_ble;
     luat_bt_t* luat_bt;
@@ -20,5 +22,7 @@ int luat_bluetooth_deinit(luat_bluetooth_t* luat_bluetooth);
 
 int luat_bluetooth_get_mac(luat_bluetooth_t* luat_bluetooth, uint8_t *addr);
 int luat_bluetooth_set_mac(luat_bluetooth_t* luat_bluetooth, uint8_t *addr, uint8_t len);
+
+void luat_bluetooth_mac_swap(uint8_t* mac);
 
 #endif
---
+++ components/bluetooth/src/luat_bluetooth.c
@@ -0,0 +1,18 @@
+#include "luat_base.h"
+#include "luat_mem.h"
+#include "luat_bluetooth.h"
+
+#include "luat_log.h"
+#define LUAT_LOG_TAG "bluetooth"
+
+void luat_bluetooth_mac_swap(uint8_t* mac){
+    uint8_t len = 0;
+    uint8_t tmp_mac[LUAT_BLUETOOTH_MAC_LEN] = {0};
+    memcpy(tmp_mac, mac, LUAT_BLUETOOTH_MAC_LEN);
+    for(int i=0;i<LUAT_BLUETOOTH_MAC_LEN;i++){
+        mac[i] = tmp_mac[LUAT_BLUETOOTH_MAC_LEN-1-i];
+    }
+}
+
+
+

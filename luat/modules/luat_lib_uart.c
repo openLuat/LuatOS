05e3d6776c546c10e0ba77cf948db9c5ae1ccd70
Wendal Chen <wendal1985@gmail.com>
2025-06-16 15:32:31 +0800

    remove: uatt库的sent日志有点多余了

--- luat/modules/luat_lib_uart.c
+++ luat/modules/luat_lib_uart.c
@@ -384,7 +384,7 @@ int l_uart_handler(lua_State *L, void* ptr) {
 	#endif
     // sent event
     if (msg->arg2 == 0) {
-        LLOGD("uart%ld sent callback", uart_id);
+        // LLOGD("uart%ld sent callback", uart_id);
         if (uart_cbs[uart_id].sent) {
             lua_geti(L, LUA_REGISTRYINDEX, uart_cbs[uart_id].sent);
             if (lua_isfunction(L, -1)) {

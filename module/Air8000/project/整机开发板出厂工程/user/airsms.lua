f45c071e5f064cb01c8de35d942f736e2effc408
梁健 <liangjian@airm2m.com>
2025-06-18 19:29:49 +0800

    fix: 更新短信的demo

--- module/Air8000/project/整机开发板出厂工程/user/airsms.lua
+++ module/Air8000/project/整机开发板出厂工程/user/airsms.lua
@@ -4,12 +4,20 @@ dnsproxy = require("dnsproxy")
 dhcpsrv = require("dhcpsrv")
 httpplus = require("httpplus")
 local run_state = false  -- 判断本UI DEMO 是否运行
-local showdata=""
-
+local msg_status
+local showdatan=""
+local showdatat_top=""
+local showdatat_mid=""
+local showdatat_last=""
+local msg_Quantity = 0
+local send_sms_num = "13316920513"
+local send_sms_txt = "我是测试短信"
 
 function start_send_sms()                                       --创建“发短信”功能函数
-     sms.send("13316920513", "102")                                   --发送短信到10001，内容102
-end
+     msg_status = sms.send(send_sms_num, send_sms_txt) 
+    --  log.info("状态：",msg_status)         --发送短信到10001，内容102
+     return msg_status
+end 
 
 function split( str,reps )                                      --创建正则表达式分割函数，用于分割外部文件内容，截取关键信息，str:外部文件内容，reps：分隔符
     local resultStrList = {}                                    --创建resultStrList的table表用于接收分割后的全部数据，便于调取
@@ -24,17 +32,30 @@ function airsms.run()
     lcd.setFont(lcd.font_opposansm12_chinese) -- 设置中文字体
     run_state = true
     spi_init()
-
+    sys.taskInit(read_msg_table)                                                   --执行read_msg_table功能函数，进入waitUntil状态等待信息
+    sys.taskInit(read_msg) 
     while true do
         sys.wait(10)
         -- airlink.statistics()
         lcd.clear(_G.bkcolor) 
-        lcd.drawStr(0,80,"需要代码内修改发送短信的号码" )
-        lcd.drawStr(0,95,"需要代码内修改发送短信的号码" )
-        lcd.drawStr(0,100,showdata)
-        -- 此处可填写短信demo ui
-        --]
-
+        lcd.drawStr(0,80,"按下方  开始  键,您将向"..send_sms_num.."发送短信.")
+        lcd.drawStr(0,100,"内容为 : ".. send_sms_txt)
+        --短信发送状态
+        lcd.drawStr(0,120,"发送状态 : " )
+        if msg_status then 
+            lcd.drawStr(65,120,"发送成功" )
+        elseif msg_status == nil then
+            lcd.drawStr(65,120,"等待发送" )
+        else
+            lcd.drawStr(65,120,"发送失败" )
+        end
+        --短信内容
+        lcd.drawStr(0,140,"来信号码 : "..showdatan)
+        lcd.drawStr(0,155,"来信内容 : ")
+        lcd.drawStr(0,175,showdatat_top)
+        lcd.drawStr(0,195,showdatat_mid)
+        lcd.drawStr(0,205,showdatat_last)
+        lcd.drawStr(0,235,"已接收短信数量 : "..msg_Quantity)
 
         lcd.showImage(20,360,"/luadb/back.jpg")
         if ap_state then
@@ -63,25 +84,25 @@ end
 
 function spi_init()                                                             --创建SPI初始化功能函数
     --方式一：使用TF卡记录短信信息                                         
---     gpio.setup(140, 1)                                                       --初始化SPI
---     sys.wait(2000)
---     local spi_id, pin_cs = 1,20 
---     -- 仅SPI方式需要自行初始化spi, sdio不需要
---     spi.setup(spi_id, nil, 0, 0, pin_cs, 400 * 1000)
---     gpio.setup(pin_cs, 1)
---     fatfs.mount(fatfs.SPI, "/sd", spi_id, pin_cs, 24 * 1000 * 1000)          --挂载TF卡，并创建主目录/sd
---     local data, err = fatfs.getfree("/sd")                                   --获取TF卡剩余内存空间，用于区分是否挂载成功。
---     if data then
---         log.info("fatfs", "getfree", json.encode(data))
---     else
---         log.info("fatfs", "err", err)
---     end
--- --------------------------------------------------------------------------------------------------------
+    --     gpio.setup(140, 1)                                                       --初始化SPI
+    --     sys.wait(2000)
+    --     local spi_id, pin_cs = 1,20 
+    --     -- 仅SPI方式需要自行初始化spi, sdio不需要
+    --     spi.setup(spi_id, nil, 0, 0, pin_cs, 400 * 1000)
+    --     gpio.setup(pin_cs, 1)
+    --     fatfs.mount(fatfs.SPI, "/sd", spi_id, pin_cs, 24 * 1000 * 1000)          --挂载TF卡，并创建主目录/sd
+    --     local data, err = fatfs.getfree("/sd")                                   --获取TF卡剩余内存空间，用于区分是否挂载成功。
+    --     if data then
+    --         log.info("fatfs", "getfree", json.encode(data))
+    --     else
+    --         log.info("fatfs", "err", err)
+    --     end
+    ------------------------------------------------------------------------------------------------------
     --方式二：使用Air8000内部文件空间记录短信信息
     --初始化SPI，挂载SD卡
     local f = io.open("/text.txt","w")                                         --使用"w"写入模式打开文件/text.txt，如没有该文件则创建，如有这个文件则覆盖原文件重新创建。确保开机时有这个文件。
     if f then                                                                  --判断是否正常打开，再执行写入。
-    f:write("无信息")                                                          --写入初始信息"无信息"。
+    -- f:write("无信息")                                                          --写入初始信息"无信息"。
     f:close()                                                                  --每次使用后需要关闭文件。
     log.info("文件创建成功")
     end
@@ -91,13 +112,15 @@ function read_msg()                                                            -
     while 1 do                                                                 --该功能在处理完当前收到的短信后需要再回到等待底层消息的状态，所以使用无限循环。
         local ret, num, txt = sys.waitUntil("SMS_INC")                         --创建变量用于接收底层publish过来的"SMS_INC"信息，表示底层接收到了文件，返回“状态”，“来信号码”，“短信文本内容”
         log.info("收到短信,信息为：",num,"号码：",txt,ret)
-        showdata=txt
+        showdatat=txt
+        showdatan=num
         local fd1 = io.open("/text.txt","a")                                   --使用"a"追加模式打开文件/text.txt
         if fd1 then                                     
             fd1:write(num.."*&*"..txt.."\r\n")                                 --将信息写入文件，格式为：来信号码*&*短信内容（\r\n）回车换行，*&*为分隔符。
             log.info("写入完成","号码 =",num,"内容 =",txt)
             fd1:close()                                                        --关闭文件
             sys.publish("new_msg")                                             --推送"new_msg"信息，告诉订阅方来了信的消息并且写到了文件中。
+            msg_Quantity = msg_Quantity + 1
         end
     end
          
@@ -113,7 +136,7 @@ function read_msg_table()                                                      -
     while 1 do                                                                 --该功能在处理完当前的信息后需要回到等待新的信息被写入文件再执行，所以此处使用无限循环
         sys.waitUntil("new_msg")                                               --等待"new_msg"消息
         local fd2 = io.open("/text.txt", "r+")                                 --使用"r+"读写模式打开已存在的/text.txt文件
-        if fd2 then                                                             
+        if fd2 then                                                           
             while true do                                                      --因为read()函数一次只能读一条，所以需要循环逐条输出，直到最后一条信息为nill后跳出循环。
                 msg_temp = fd2:read("l")                                       --使用read()函数输出文件中的数据赋值给msg_temp，"l"模式读取下一行，在文件尾 (EOF) 处返回 nil
                 if msg_temp and #msg_temp ~= 0 then                            --判断msg_temp收到信息
@@ -123,18 +146,18 @@ function read_msg_table()                                                      -
                 else
                     break                                                      --如果msg_temp没收到信息则跳出循环。
                 end
-                log.info("最新短信：",num_new,txt_new)
-                sys.wait(1000)
+                -- log.info("最新短信：",num_new,txt_new)
+                -- sys.wait(1000)
             end
             
         log.info("最新短信：",num_new,txt_new)
         fd2:close()                                                            --关闭文件
-            
+        showdatat_top = string.sub(txt_new,1,60)
+        showdatat_mid = string.sub(txt_new,61,120)
+        showdatat_last = string.sub(txt_new,121)
         end
-    end
-    return num_new,txt_new                                                     --该函数返回最近一次来信号码和来信信息。
+    end                                              
 end 
-sys.taskInit(read_msg_table)                                                   --执行read_msg_table功能函数，进入waitUntil状态等待信息
-sys.taskInit(read_msg)                                                         --执行read_msg功能函数，进入waitUntil状态等待信息
+                                                        --执行read_msg功能函数，进入waitUntil状态等待信息
 
 return airsms

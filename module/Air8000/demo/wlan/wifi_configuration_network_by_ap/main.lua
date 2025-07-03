1478ba18459da5f7ba5c5771fc478367eb20e2de
王文中 <1481840715@qq.com>
2025-07-03 17:08:43 +0800

    add: 添加wififota接口，保证模组为最新WiFi固件

--- module/Air8000/demo/wlan/wifi_configuration_network_by_ap/main.lua
+++ module/Air8000/demo/wlan/wifi_configuration_network_by_ap/main.lua
@@ -8,10 +8,27 @@ require "sysplus"
 dnsproxy = require("dnsproxy")
 dhcpsrv = require("dhcpsrv")
 httpplus = require("httpplus")
+local fota_wifi = require("fota_wifi")
 
--- 如果需要升级WIFI固件，请打开下面两行注释
--- local fota_wifi = require("fota_wifi")
--- sys.taskInit(fota_wifi.request)
+local function wifi_fota_task_func()
+    local result = fota_wifi.request()
+    if result then
+        log.info("fota_wifi", "升级任务执行成功")
+    else
+        log.info("fota_wifi", "升级任务执行失败")
+    end
+end
+
+-- 判断网络是否正常
+local function wait_ip_ready()
+    local result, ip, adapter = sys.waitUntil("IP_READY", 30000)
+    if result then
+        log.info("fota_wifi", "开始执行升级任务")
+        sys.taskInit(wifi_fota_task_func)
+    else
+        log.error("当前正在升级WIFI&蓝牙固件，请插入可以上网的SIM卡")
+    end
+end
 
 -- 初始化LED灯, 这里演示控制Air8000核心板蓝灯，其他开发板请查看硬件原理图自行修改(如果使用整机开发板可以用GPIO146)
 local LEDA = gpio.setup(20, 0, gpio.PULLUP)
@@ -88,8 +105,11 @@ function main_task()
     create_ap()
     wifi_networking()
 
-    
+
 end
+
+-- 在设备启动时检查网络状态
+sys.taskInit(wait_ip_ready)
 
 sys.subscribe("WLAN_SCAN_DONE", scan_done_handle)
 sys.subscribe("IP_READY", ip_ready_handle)

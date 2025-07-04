a9a60b255bd1b7985f668ebc007975c78c468948
梁健 <liangjian@airm2m.com>
2025-07-04 16:41:10 +0800

    add: 增加air8000 整机开发板的对讲demo

---
+++ module/Air8000/project/整机开发板出厂工程/user/talk.lua
@@ -0,0 +1,111 @@
+local talk = {}
+
+dnsproxy = require("dnsproxy")
+dhcpsrv = require("dhcpsrv")
+httpplus = require("httpplus")
+local run_state = false -- 判断本UI DEMO 是否运行
+local airaudio  = require "airaudio"
+local speech_topic = "15055190176"  --  填写手机号，需要保证所有对讲设备，网页都是同一个号码   
+local mqtt_host = "lbsmqtt.airm2m.com"
+local mqtt_port = 1884
+local mqtt_isssl = false
+local client_id = nil
+local user_name = "user"
+local password = "password"
+local mqttc = nil
+local message = ""
+local event = ""
+local talk_state = ""
+
+
+
+local function talk_event_cb(event, param)
+    log.info("talk event", event, param)
+    event  = event
+end
+
+local function mqtt_cb(mqtt_client, event, data, payload)
+    log.info("mqtt", "event", event, mqtt_client, data, payload)
+end
+
+
+
+local function init_talk()
+    log.info("init_call")
+    airaudio.init() 
+
+    client_id = mobile.imei()
+
+
+    mqttc = mqtt.create(nil, mqtt_host, mqtt_port, mqtt_isssl, {rxSize = 4096})
+    airtalk.config(airtalk.PROTOCOL_DEMO_MQTT_8K, mqttc, 200) -- 缓冲至少200ms播放
+    airtalk.on(talk_event_cb)
+    airtalk.start(client_id, speech_topic)
+
+    mqttc:auth(client_id,user_name,password) -- client_id必填,其余选填
+    mqttc:keepalive(240) -- 默认值240s
+    mqttc:autoreconn(true, 3000) -- 自动重连机制
+    mqttc:debug(false)
+    mqttc:on(mqtt_cb)
+
+    -- mqttc自动处理重连, 除非自行关闭
+    mqttc:connect()
+
+end
+
+local function stop_talk()
+    talk_state = "语音停止采集"
+    airtalk.uplink(false)
+end
+
+
+local function start_talk()
+    talk_state = "语音采集上传中"
+    airtalk.uplink(true)
+end
+
+function talk.run()
+    log.info("talk.run")
+    lcd.setFont(lcd.font_opposansm12_chinese) -- 设置中文字体
+    run_state = true
+
+    init_talk() -- 初始化电话功能相关配置
+
+    while true do
+        sys.wait(10)
+        lcd.clear(_G.bkcolor)
+        lcd.drawStr(0, 80, "开始对讲测试:"..talk_state)
+        lcd.drawStr(0, 100, event)
+        
+
+        lcd.drawStr(0, 120, "事件:" .. event)
+
+
+        -- [
+        -- 此处可填写demo ui
+        -- ]
+
+
+        lcd.showImage(130, 350, "/luadb/start.jpg") -- 对讲开始按钮
+        lcd.showImage(130, 397, "/luadb/stop.jpg") -- 对讲停止按钮
+        
+        lcd.showImage(0, 448, "/luadb/Lbottom.jpg")
+        lcd.flush()
+
+        if not run_state then -- 等待结束，返回主界面
+            return true
+        end
+    end
+end
+
+function talk.tp_handal(x, y, event)
+    if x > 20 and x < 100 and y > 360 and y < 440 then
+        run_state = false
+    elseif x > 130 and x < 230 and y > 350 and y < 397 then
+        sysplus.taskInitEx(start_talk, "start_talk")
+    elseif x > 130 and x < 230 and y > 397 and y < 444 then
+        sysplus.taskInitEx(stop_talk, "stop_talk")
+    end
+end
+
+return talk

757da3c1ac3522cb6862f2bbc22109f77cdf5ada
alienwalker <alienwalker@sina.com>
2025-07-25 13:18:34 +0800

    update:更正一些注释

--- luat/demo/airtalk/airtalk_net_ctrl.lua
+++ luat/demo/airtalk/airtalk_net_ctrl.lua
@@ -1,11 +1,10 @@
-local mqtt_host = "lbsmqtt.openluat.com"
-local mqtt_port = 1886
-local mqtt_isssl = false
-local client_id = nil
-local user_name = "mqtt_hz_test_1"
-local password = "Ck8WpNCp"
-local mqttc = nil
 require "audio_config"
+
+local mqttc = nil
+
+local function task_cb(msg)
+    log.info("未处理消息", msg[1], msg[2], msg[3], msg[4])
+end
 
 local function airtalk_event_cb(event, param)
     log.info("airtalk event", event, param)
@@ -15,15 +14,18 @@ local function mqtt_cb(mqtt_client, event, data, payload)
     log.info("mqtt", "event", event, mqtt_client, data, payload)
 end
 
-function airtalk_demo_mqtt_8k(speech_topic)
+function airtalk_mqtt_task()
+    local msg
     --client_id也可以自己设置
-    client_id = mobile.imei()
+    local client_id = mobile.imei()
+    local user = mobile.imei()
+    local password = mobile.muid()
     audio_init()
 
-    mqttc = mqtt.create(nil, mqtt_host, mqtt_port, mqtt_isssl, {rxSize = 4096})
-    airtalk.config(airtalk.PROTOCOL_DEMO_MQTT_8K, mqttc, 200) -- 缓冲至少200ms播放
+    mqttc = mqtt.create(nil, mqtt.airtalk.luatos.com, 1883, false, {rxSize = 4096})
+    airtalk.config(airtalk.PROTOCOL_MQTT, mqttc, 200) -- 缓冲至少200ms播放
     airtalk.on(airtalk_event_cb)
-    airtalk.start(client_id, speech_topic)
+    airtalk.start()
 
     mqttc:auth(client_id,user_name,password) -- client_id必填,其余选填
     mqttc:keepalive(240) -- 默认值240s
@@ -34,7 +36,13 @@ function airtalk_demo_mqtt_8k(speech_topic)
     -- mqttc自动处理重连, 除非自行关闭
     mqttc:connect()
     while true do
-        --全是底层自动运行，到这里没什么可以演示的了
-        sys.wait(60000000)
+        msg = sys.waitMsg(AIRTALK_TASK_NAME)
+        if type(result) == 'table' then
+
+        end
     end
+end
+
+function airtalk_mqtt_init()
+    sys.taskInitEx(airtalk_mqtt_task, AIRTALK_TASK_NAME, task_cb)
 end

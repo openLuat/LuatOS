9690d351915b197ad2bf0d03a73e2b435e5df47b
alienwalker <alienwalker@sina.com>
2025-07-25 16:17:17 +0800

    update:airtalk控制协议适配到鉴权和获取设备列表

--- luat/demo/airtalk/airtalk_net_ctrl.lua
+++ luat/demo/airtalk/airtalk_net_ctrl.lua
@@ -3,6 +3,7 @@ require "audio_config"
 
 local mqttc = nil
 local client_id
+local downlink_topic
 local topic_auth
 local topic_list_update
 local topic_talk_start
@@ -10,6 +11,7 @@ local topic_talk_stop
 local topic_list_update_ack
 local topic_talk_start_ack
 local topic_talk_stop_ack
+local dev_list
 
 local function task_cb(msg)
     log.info("未处理消息", msg[1], msg[2], msg[3], msg[4])
@@ -19,15 +21,45 @@ local function airtalk_event_cb(event, param)
     log.info("airtalk event", event, param)
 end
 
+local function next_auth()
+    sys.sendMsg(AIRTALK_TASK_NAME, MSG_NET_READY)
+end
+
 local function mqtt_cb(mqtt_client, event, data, payload)
     log.info(event)
     if event == "conack" then
         -- 联上了
         mqtt_client:subscribe("ctrl/downlink/" .. client_id .. "/#")--单主题订阅
     elseif event == "suback" then
-        sys.sendMsg(AIRTALK_TASK_NAME, MSG_MQTT_READY)
+        sys.sendMsg(AIRTALK_TASK_NAME, MSG_NET_READY)
     elseif event == "recv" then
-        log.info("mqtt", "downlink", "topic", data, payload)
+        local result = string.match(data, downlink_topic)
+        if result then
+            log.info(data, payload)
+            local obj,res,err = json.decode(payload)
+            if result == "0102" then
+
+            elseif result == "0103" then
+            elseif result == "0101" then
+            elseif result == "8003" then
+            elseif result == "8004" then
+            elseif result == "8001" then
+                if res and obj["result"] then
+                    sys.sendMsg(AIRTALK_TASK_NAME, MSG_AUTH_ACK, obj["result"], obj["version"])
+                end
+            elseif result == "8002" then
+                if res and obj["result"] == SUCC then
+                    dev_list = obj["dev_list"]
+                    for i=1,#dev_list do
+                        log.info(dev_list[i]["id"],dev_list[i]["name"])
+                    end
+                end
+            else
+            end
+            decode_data = nil
+        end
+        result = nil
+        
     elseif event == "sent" then
         -- log.info("mqtt", "sent", "pkgid", data)
     -- elseif event == "disconnect" then
@@ -41,7 +73,7 @@ function airtalk_mqtt_task()
     local msg,data,obj
     --client_id也可以自己设置
     client_id = mobile.imei()
-
+    downlink_topic = "ctrl/downlink/" .. client_id .. "/(%w%w%w%w)"
     topic_auth = "ctrl/uplink/" .. client_id .."/0001"
     topic_list_update = "ctrl/uplink/" .. client_id .."/0002"
     topic_talk_start = "ctrl/uplink/" .. client_id .."/0003" 
@@ -49,11 +81,11 @@ function airtalk_mqtt_task()
     topic_list_update_ack = "ctrl/uplink/" .. client_id .."/8101"
     topic_talk_start_ack = "ctrl/uplink/" .. client_id .."/8102"
     topic_talk_stop_ack = "ctrl/uplink/" .. client_id .."/8103"
-
+    sys.timerLoopStart(next_auth, 900000)
 
     audio_init()
 
-    mqttc = mqtt.create(nil, "mqtt.airtalk.luatos.com", 1883, false, {rxSize = 4096})
+    mqttc = mqtt.create(nil, "mqtt.airtalk.luatos.com", 1883, false, {rxSize = 32768})
     airtalk.config(airtalk.PROTOCOL_MQTT, mqttc, 200) -- 缓冲至少200ms播放
     airtalk.on(airtalk_event_cb)
     airtalk.start()
@@ -68,14 +100,22 @@ function airtalk_mqtt_task()
     mqttc:connect()
     while true do
         msg = sys.waitMsg(AIRTALK_TASK_NAME)
-        if type(msg) == 'table' and type(msg[1]) == "number" and msg[1] < 5 then
-            if msg[1] == MSG_MQTT_READY then
+        if type(msg) == 'table' and type(msg[1]) == "number" and msg[1] < MSG_QTY then
+            if msg[1] == MSG_NET_READY then
                 obj = {["key"] = PRODUCT_KEY, ["device_type"] = 1}
                 data = json.encode(obj)
                 log.info(topic_auth, data)
                 mqttc:publish(topic_auth, data)
-                obj = nil
+                sys.timerLoopStart(next_auth, 60000)    --1分钟就尝试重新鉴权
+            elseif msg[1] == MSG_AUTH_ACK then
+                if msg[2] == SUCC then
+                    sys.timerLoopStart(next_auth, 3600000) --鉴权通过则60分钟后尝试重新鉴权
+                    mqttc:publish(topic_list_update, "")
+                else
+                    log.info("auth", msg[2])
+                end                
             end
+            obj = nil
         end
     end
 end
--- luat/demo/airtalk/demo_define.lua
+++ luat/demo/airtalk/demo_define.lua
@@ -1,3 +1,6 @@
 
 AIRTALK_TASK_NAME = "airtalk_task"
-MSG_MQTT_READY = 0
+MSG_NET_READY = 0
+MSG_AUTH_ACK = 1
+MSG_QTY = 10
+SUCC = "success"

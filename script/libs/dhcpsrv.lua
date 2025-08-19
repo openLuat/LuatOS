49c11c0e72020e66705b371e5f242726f340d4d8
liustu <123123123@123.com>
2025-08-19 16:44:22 +0800

    fix:优化dhcpsrv的配置项 https://gitee.com/openLuat/LuatOS/issues/ICKOW7

--- script/libs/dhcpsrv.lua
+++ script/libs/dhcpsrv.lua
@@ -302,12 +302,16 @@ dhcpsrv.create({adapter=socket.LWIP_AP})
 local dhcpsrv_opts = {
     adapter=socket.LWIP_AP, -- 监听哪个网卡, 必须填写
     mark = {255, 255, 255, 0}, -- 网络掩码, 默认 255.255.255.0
-    gw = {192, 168, 4, 1}, -- 网关, 默认 192.168.4.1
+    gw = {192, 168, 4, 1}, -- 网关, 默认自动获取网卡IP，如果获取失败则使用 192.168.4.1
     ip_start = 100, -- ip起始地址, 默认100
     ip_end = 200, -- ip结束地址, 默认200
     ack_cb = function(ip, mac) end, -- ack回调, 有客户端连接上来时触发, ip和mac地址会传进来
 }
 dhcpsrv.create(dhcpsrv_opts)
+
+-- 自动功能说明：
+-- 如果不指定gw参数，系统会自动获取网卡IP作为网关地址
+-- 这样可以确保DHCP分配的IP与网卡IP在同一网段
 ]]
 function dhcpsrv.create(opts)
     local srv = {}

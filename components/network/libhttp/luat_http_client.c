0daeee303b8bb16bf1697f9d1c1646fb1cb6ce01
Wendal Chen <wendal1985@gmail.com>
2025-09-10 19:05:48 +0800

    change: http,调整报错日志,展示域名和端口号

--- components/network/libhttp/luat_http_client.c
+++ components/network/libhttp/luat_http_client.c
@@ -627,7 +627,7 @@ int32_t luat_lib_http_callback(void *data, void *param){
 	LLOGD("luat_lib_http_callback %d %d %p",event->ID - EV_NW_RESULT_BASE,event->Param1, http_ctrl);
 	if (event->Param1){
 		//LLOGD("LINK %d ON_LINE %d EVENT %d TX_OK %d CLOSED %d",EV_NW_RESULT_LINK & 0x0fffffff,EV_NW_RESULT_CONNECT & 0x0fffffff,EV_NW_RESULT_EVENT & 0x0fffffff,EV_NW_RESULT_TX & 0x0fffffff,EV_NW_RESULT_CLOSE & 0x0fffffff);
-		LLOGE("http_ctrl close %08X %d",event->ID - EV_NW_RESULT_BASE, event->Param1);
+		LLOGE("error event %08X %d host %s port %d",event->ID - EV_NW_RESULT_BASE, event->Param1, http_ctrl->netc->domain_name, http_ctrl->netc->remote_port);
 		if (http_ctrl->luatos_mode) {
 			http_resp_error(http_ctrl, event->ID == EV_NW_RESULT_CONNECT ? HTTP_ERROR_CONNECT : HTTP_ERROR_CLOSE);
 		} else {

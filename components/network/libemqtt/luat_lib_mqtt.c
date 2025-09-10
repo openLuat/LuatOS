fd5c90fa945e2c54df57959ac359220d9edf3827
Wendal Chen <wendal1985@gmail.com>
2025-09-10 11:16:24 +0800

    update: mqtt,create函数的最后一个参数的描述改善一下,改名opts

--- components/network/libemqtt/luat_lib_mqtt.c
+++ components/network/libemqtt/luat_lib_mqtt.c
@@ -347,12 +347,12 @@ static int l_mqtt_set_debug(lua_State *L){
 
 /*
 mqtt客户端创建
-@api mqtt.create(adapter,host,port,ssl,isipv6)
+@api mqtt.create(adapter,host,port,ssl,opts)
 @int 适配器序号, 如果不填,会选择平台自带的方式,然后是最后一个注册的适配器,可选值请查阅socket库的常量表
 @string 服务器地址,可以是域名, 也可以是ip
 @int  	端口号
 @bool/table  是否为ssl加密连接,默认不加密,true为无证书最简单的加密，table为有证书的加密 <br>server_cert 服务器ca证书数据 <br>client_cert 客户端证书数据 <br>client_key 客户端私钥加密数据 <br>client_password 客户端私钥口令数据 <br>verify 是否强制校验 0不校验/1可选校验/2强制校验 默认2
-@bool/table  bool 是否为ipv6，默认不是  table mqtt扩展参数, ipv6 是否为ipv6, rxSize 接收缓冲区大小
+@table  mqtt扩展参数, ipv6 是否为ipv6, rxSize 接收缓冲区大小
 @return userdata 若成功会返回mqtt客户端实例,否则返回nil
 @usage
 -- 普通TCP链接

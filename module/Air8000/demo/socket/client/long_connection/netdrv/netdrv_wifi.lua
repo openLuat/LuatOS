5b2207e2e48a6bc388a724e8a0cd70a6feb449ca
zhutianhua <278610562@qq.com>
2025-09-22 14:42:13 +0800

    update：Air8000/demo/socket/client/long_connection，支持PC模拟器上的单网卡配置，真机环境上的单网卡配置使用exnetif扩展库接口

--- module/Air8000/demo/socket/client/long_connection/netdrv/netdrv_wifi.lua
+++ module/Air8000/demo/socket/client/long_connection/netdrv/netdrv_wifi.lua
@@ -13,33 +13,20 @@
 本文件没有对外接口，直接在其他功能模块中require "netdrv_wifi"就可以加载运行；
 ]]
 
-local function ip_ready_func()
-    log.info("netdrv_wifi.ip_ready_func", "IP_READY", json.encode(wlan.getInfo()))
+local exnetif = require "exnetif"
+
+local function ip_ready_func(ip, adapter)
+    if adapter == socket.LWIP_STA then
+        log.info("netdrv_wifi.ip_ready_func", "IP_READY", socket.localIP(socket.LWIP_STA))
+    end
 end
 
-local function ip_lose_func()
-    log.warn("netdrv_wifi.ip_lose_func", "IP_LOSE")
+local function ip_lose_func(adapter)    
+    if adapter == socket.LWIP_STA then
+        log.warn("netdrv_wifi.ip_lose_func", "IP_LOSE")
+    end
 end
 
-
-
---此处订阅"IP_READY"和"IP_LOSE"两种消息
---在消息的处理函数中，仅仅打印了一些信息，便于实时观察WIFI的连接状态
---也可以根据自己的项目需求，在消息处理函数中增加自己的业务逻辑控制，例如可以在连网状态发生改变时更新网络图标
-sys.subscribe("IP_READY", ip_ready_func)
-sys.subscribe("IP_LOSE", ip_lose_func)
-
-
--- 设置默认网卡为socket.LWIP_STA
-socket.dft(socket.LWIP_STA)
-
-
-wlan.init()
---连接WIFI热点，连接结果会通过"IP_READY"或者"IP_LOSE"消息通知
---Air8000仅支持2.4G的WIFI，不支持5G的WIFI
---此处前两个参数表示WIFI热点名称以及密码，更换为自己测试时的真实参数即可
---第三个参数1表示WIFI连接异常时，内核固件会自动重连
-wlan.connect("iPhone", "xiaoshuai", 1)
 
 --WIFI联网成功（做为STATION成功连接AP，并且获取到了IP地址）后，内核固件会产生一个"IP_READY"消息
 --各个功能模块可以订阅"IP_READY"消息实时处理WIFI联网成功的事件
@@ -48,3 +35,25 @@ wlan.connect("iPhone", "xiaoshuai", 1)
 --WIFI断网后，内核固件会产生一个"IP_LOSE"消息
 --各个功能模块可以订阅"IP_LOSE"消息实时处理WIFI断网的事件
 --也可以在任何时刻调用socket.adapter(socket.LWIP_STA)来获取WIFI网络是否连接成功
+
+--此处订阅"IP_READY"和"IP_LOSE"两种消息
+--在消息的处理函数中，仅仅打印了一些信息，便于实时观察WIFI的连接状态
+--也可以根据自己的项目需求，在消息处理函数中增加自己的业务逻辑控制，例如可以在连网状态发生改变时更新网络图标
+sys.subscribe("IP_READY", ip_ready_func)
+sys.subscribe("IP_LOSE", ip_lose_func)
+
+
+-- 配置WiFi设备模式的单网卡，exnetif.set_priority_order使用的网卡编号为socket.LWIP_STA
+-- ssid为要连接的WiFi路由器名称；
+-- password为要连接的WiFi路由器密码；
+-- 注意：仅支持2.4G的WiFi，不支持5G的WiFi；
+-- 实际测试时，根据自己要连接的WiFi热点信息修改以下参数
+exnetif.set_priority_order({
+    {
+        WIFI = {
+            ssid = "茶室-降功耗,找合宙!", 
+            password = "Air123456"
+        }
+    }
+})
+

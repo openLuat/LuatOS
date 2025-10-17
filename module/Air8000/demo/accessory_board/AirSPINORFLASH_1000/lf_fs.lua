4bbe044670fb9b3b9585578c58f5457a7480a4bd
马亚丹 <18317857567@163.com>
2025-10-17 16:34:33 +0800

    update:修改Air8000的flash的demo

--- module/Air8000/demo/accessory_board/AirSPINORFLASH_1000/lf_fs.lua
+++ module/Air8000/demo/accessory_board/AirSPINORFLASH_1000/lf_fs.lua
@@ -7,7 +7,7 @@
 @usage
 本demo演示的功能为：使用Air8000核心板通过SPI库实现对 NOR Flash的操作，演示读数据写数据、删除数据等操作。
 以 Air8000核心板为例, 接线如下:
-Air8000       AirSPINAND_1000配件版
+Air8000       AirSPINORFLASH_1000配件版
 GND(任意)          GND
 VDD_EXT            VCC
 GPIO12/SPI1_CS     CS,片选

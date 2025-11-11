3df802a0beddca2d2a524d55a4efcf9519e2f70e
王城钧 <wangchengjun@airm2m.com>
2025-11-11 09:56:02 +0800

    update：780EPM AirETH1000 demo更新

--- module/Air780EPM/demo/accessory_board/AirETH_1000/http/netdrv_device.lua
+++ module/Air780EPM/demo/accessory_board/AirETH_1000/http/netdrv_device.lua
@@ -20,8 +20,14 @@
 
 -- 根据自己的项目需求，只需要require以下其中的一种即可；
 
+-- 加载“4G网卡”驱动模块
+-- require "netdrv_4g"
+
 -- 加载“通过SPI外挂CH390H芯片的以太网卡”驱动模块
 require "netdrv_eth_spi"
 
 -- 加载“可以配置优先级的多种网卡”驱动模块
 -- require "netdrv_multiple"
+
+-- 加载“pc模拟器网卡”驱动模块
+-- require "netdrv_pc"

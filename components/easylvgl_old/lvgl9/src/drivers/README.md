50fd69694b96fc0188096ec44f5e2930636acae8
zengeshuai <1287666071@qq.com>
2025-12-04 16:27:52 +0800

    remove: easylvgl，移除old_easylvgl参考代码

--- components/easylvgl_old/lvgl9/src/drivers/README.md
+++
@@ -1,1 +0,0 @@
-High level drivers for display controllers, frame buffers, etc
--- components/easylvgl_old/lvgl9/src/libs/frogfs/include/frogfs/frogfs_types.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../../../../lv_conf_internal.h"
-#include LV_STDINT_INCLUDE
-
-typedef intptr_t ssize_t;

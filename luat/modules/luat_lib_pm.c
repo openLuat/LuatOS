ee27075065e445a71986916f07450db67c04fb71
Wendal Chen <wendal1985@gmail.com>
2026-01-06 09:20:38 +0800

    update: pm,开关wifi电源时,增加日志

--- luat/modules/luat_lib_pm.c
+++ luat/modules/luat_lib_pm.c
@@ -355,9 +355,16 @@ static int l_pm_power_ctrl(lua_State *L) {
     }
     if (id == LUAT_PM_POWER_WIFI) {
         #ifdef LUAT_MODEL_AIR8000
+        extern int luat_airlink_has_wifi(void);
+        if (!luat_airlink_has_wifi()) {
+            lua_pushboolean(L, 0);
+            return 1;
+        }
         if(onoff) {
+            LLOGI("pm", "wifi power on");
             luat_gpio_mode(23, LUAT_GPIO_OUTPUT, LUAT_GPIO_PULLUP, 1);
         } else {
+            LLOGI("pm", "wifi power off");
             luat_gpio_close(23);
         }
         lua_pushboolean(L, 1);

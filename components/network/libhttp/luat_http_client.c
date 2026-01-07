937a1b6d05da576ab2f951996465d6d1100f39ec
alienwalker <alienwalker@sina.com>
2026-01-07 22:35:05 +0800

    fix:上一个对http修改导致csdk编译通不过了

--- components/network/libhttp/luat_http_client.c
+++ components/network/libhttp/luat_http_client.c
@@ -13,7 +13,9 @@
 #include "luat_spi.h"
 #include "luat_timer.h"
 #include "luat_str.h"
+#ifdef __LUATOS__
 #include "luat_msgbus.h"
+#endif
 
 #include "luat_fs.h"
 #include "luat_network_adapter.h"
@@ -642,6 +644,7 @@ static void http_send_message(luat_http_ctrl_t *http_ctrl){
 	}
 }
 
+#ifdef __LUATOS__
 static int luat_http_timeout_resp_error(lua_State *L, void* ptr) {
     luat_http_ctrl_t *http_ctrl = (luat_http_ctrl_t*)ptr;
     if (!http_ctrl) return 0;
@@ -656,14 +659,19 @@ static int luat_http_timeout_network_close(lua_State *L, void* ptr) {
     http_network_close(http_ctrl);
     return 0;
 }
+#endif
 
 LUAT_RT_RET_TYPE luat_http_timer_callback(LUAT_RT_CB_PARAM){
 	luat_http_ctrl_t * http_ctrl = (luat_http_ctrl_t *)param;
 	if (http_ctrl->luatos_mode) {
+#ifdef __LUATOS__
 		rtos_msg_t msg = {0};
 		msg.handler = luat_http_timeout_resp_error;
 		msg.ptr = http_ctrl;
 		luat_msgbus_put(&msg, 0);
+#else
+		http_resp_error(http_ctrl, HTTP_ERROR_TIMEOUT);
+#endif
 	} else {
 		if (http_ctrl->new_data)
 		{
@@ -671,10 +679,16 @@ LUAT_RT_RET_TYPE luat_http_timer_callback(LUAT_RT_CB_PARAM){
 		}
 		else
 		{
+#ifdef __LUATOS__
 			rtos_msg_t msg = {0};
 			msg.handler = luat_http_timeout_network_close;
 			msg.ptr = http_ctrl;
 			luat_msgbus_put(&msg, 0);
+#else
+			LLOGD("http timeout error!");
+			http_ctrl->error_code = HTTP_ERROR_TIMEOUT;
+			http_network_close(http_ctrl);
+#endif
 		}
 	}
 }

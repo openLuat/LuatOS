263d5009edf4ea866adaa8ecefb4a0921f18dcb3
zengeshuai <1287666071@qq.com>
2026-01-09 14:55:19 +0800

    update: easylvgl, 打开lottie动画组件支持设置

--- components/easylvgl/inc/luat_easylvgl_conf.h
+++ components/easylvgl/inc/luat_easylvgl_conf.h
@@ -51,11 +51,11 @@
     #define LV_IME_PINYIN_CAND_TEXT_NUM 9 // 设置拼音候选词数量
 
     // lottie 支持
-    // #define LV_USE_LOTTIE 1
-    // #define LV_USE_FLOAT 1
-    // #define LV_USE_MATRIX 1
-    // #define LV_USE_VECTOR_GRAPHIC  1
-    // #define LV_USE_THORVG_INTERNAL 1
+    #define LV_USE_LOTTIE 1
+    #define LV_USE_FLOAT 1
+    #define LV_USE_MATRIX 1
+    #define LV_USE_VECTOR_GRAPHIC  1
+    #define LV_USE_THORVG_INTERNAL 1
 
 #elif defined(LUAT_USE_EASYLVGL_LUATOS)
     /* LuatOS 平台配置：使用 FreeRTOS 以支持 LVGL 多线程渲染 */

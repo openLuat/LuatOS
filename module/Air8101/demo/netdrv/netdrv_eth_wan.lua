fdc4befa0ec33a77c5ab41cad001d84b82d95e11
wjq <weijianqiang@airm2m.com>
2026-01-13 15:46:33 +0800

    update:Air8101,netdrv demo,修改spi参数

--- module/Air8101/demo/netdrv/netdrv_eth_wan.lua
+++ module/Air8101/demo/netdrv/netdrv_eth_wan.lua
@@ -69,8 +69,8 @@ local function eth_wan_setup()
         -- netdrv.CH390外挂CH390
         -- SPI ID 1, 片选 GPIO12
         netdrv.setup(eth_adapter, netdrv.CH390, {
-            spi = 1,
-            cs = 5
+            spi = 0,
+            cs = 15
         })
     end
     sys.wait(1000) -- 等待以太网模块初始化完成,去掉会导致以太网初始化失败

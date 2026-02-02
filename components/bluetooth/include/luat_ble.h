4c2b94d79327dff8e0c86d4c10cd1c270d36d48d
PeakRacing <1473454180@qq.com>
2026-02-02 12:13:14 +0800

    add:添加一些ble注释

--- components/bluetooth/include/luat_ble.h
+++ components/bluetooth/include/luat_ble.h
@@ -94,6 +94,28 @@ typedef enum{
 // Descriptors UUIDS 0X2900 - 0X2915
 #define LUAT_BLE_GATT_DESC_MIN      0X2900
 #define LUAT_BLE_GATT_DESC_MAX      0X2915
+// 0x2900 Characteristic Extended Properties
+// 0x2901 Characteristic User Description
+// 0x2902 Client Characteristic Configuration
+// 0x2903 Server Characteristic Configuration
+// 0x2904 Characteristic Presentation Format
+// 0x2905 Characteristic Aggregate Format
+// 0x2906 Valid Range
+// 0x2907 External Report Reference
+// 0x2908 Report Reference
+// 0x2909 Number of Digitals
+// 0x290A Value Trigger Setting
+// 0x290B Environmental Sensing Configuration
+// 0x290C Environmental Sensing Measurement
+// 0x290D Environmental Sensing Trigger Setting
+// 0x290E Time Trigger Setting
+// 0x290F Complete BR-EDR Transport Block Data
+// 0x2910 Observation Schedule
+// 0x2911 Valid Range and Accuracy
+// 0x2912 Measurement Description
+// 0x2913 Manufacturer Limits
+// 0x2914 Process Tolerances
+// 0x2915 IMD Trigger Setting
 
 // Declarations
 #define LUAT_BLE_PRIMARY_SERVICE    0x2800

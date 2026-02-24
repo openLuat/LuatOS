8e44a6cd1605a4768c9796683fc715ee3c41983c
许璐 <x128238@163.com>
2026-02-24 17:26:40 +0800

    update：tcp自动化测试修改以及接口补充

--- testcase/function_testcase_network/TCP/scripts/tcp_test.lua
+++ testcase/function_testcase_network/TCP/scripts/tcp_test.lua
@@ -5,12 +5,150 @@ local taskName = "TCP_ECHO_TASK"
 local netCB = nil
 local protocol = false
 local ssl = false
+local ip = "airtest.openluat.com"
+local port = 2901
+local rtos_bsp = rtos.bsp()
+local wifi_module = {"Air8000", "Air8101"} -- 支持wifi的模块
+
+-- 查看网卡的联网状态(正常情况)
+function tcp_tests.test_get_socket_adapter()
+    local is_wifi_module = false
+    local is_ready
+    for _, model in ipairs(wifi_module) do
+        if rtos_bsp == model then
+            is_wifi_module = true
+            break
+        end
+    end
+    -- 支持wifi的模块用socket.LWIP_STA
+    if is_wifi_module == true then
+        is_ready, _ = socket.adapter(socket.LWIP_STA)
+    else
+        -- 支持4G的模块用socket.LWIP_GP
+        is_ready, _ = socket.adapter(socket.LWIP_GP)
+    end
+    assert(is_ready == true, "网卡的网络未就绪！")
+
+end
+
+-- 默认使用wifi网卡连接，失败后使用默认网卡
+function tcp_tests.test_get_socket_adapter_error()
+    -- 1.默认使用socket.LWIP_STA网卡
+    local is_ready, _ = socket.adapter(socket.LWIP_STA)
+    if is_ready ~= true then
+        -- 失败使用默认网关
+        local is_ready1, _ = socket.adapter(socket.dft())
+        assert(is_ready1 == true, "网卡的网络未就绪！")
+    else
+        assert(is_ready == true, "网卡的网络未就绪！")
+    end
+end
+
+-- 使用默认网关进行配置
+-- 联网成功后
+-- 获取某个网卡上的本地 ip地址、网络掩码和网关地址；
+-- adapter_id：1-14
+
+function tcp_tests.test_get_socket_localIP()
+    local adapter_id, last_reg_adapter_id = socket.dft()
+    local dft_adapter = false
+    if adapter_id >= 1 and adapter_id <= 14 then
+        dft_adapter = true
+    end
+    log.info("adapter_id", adapter_id, dft_adapter)
+
+    -- 查询默认网卡是否在定义的网络适配器范围内
+    assert(dft_adapter == true, "默认网卡不在定义的网络适配器范围内！")
+
+    -- 查看网卡的联网状态
+    local is_ready, _ = socket.adapter(adapter_id)
+    assert(is_ready == true, "网卡的网络未就绪！")
+
+    -- 获取网卡上的本地 ip地址、网络掩码和网关地址；
+    local ip, netmask, gatewa = socket.localIP(adapter_id)
+
+    -- 联网成功后
+    -- 返回值类型判断
+    assert(type(ip) == "string", "socket.localIP()第一个返回值类型错误！")
+    assert(type(netmask) == "string", "socket.localIP()第二个返回值类型错误！")
+    assert(type(gatewa) == "string", "socket.localIP()第三个返回值类型错误！")
+
+    -- 返回值不为空
+    assert(ip ~= nil, "socket.localIP()第一个返回值为空！")
+    assert(netmask ~= nil, "socket.localIP()第二个返回值为空！")
+    assert(gatewa ~= nil, "socket.localIP()第三个返回值为空！")
+end
+
+-- 网卡未就绪情况下的参数
+
+function tcp_tests.test_get_socket_localIP_error()
+
+    -- 查看网卡的联网状态
+    local is_ready, _ = socket.adapter(15)
+    assert(is_ready == false, "网卡的网络就绪！")
+
+    -- 获取网卡上的本地 ip地址、网络掩码和网关地址；
+    local ip, netmask, gatewa = socket.localIP(15)
+
+    -- 网络未就绪，返回值为空
+    assert(ip == nil, "socket.localIP()第一个返回值应为空！")
+    assert(netmask == nil, "socket.localIP()第二个返回值应为空！")
+    assert(gatewa == nil, "socket.localIP()第三个返回值应为空！")
+end
+
+function tcp_tests.test_get_socket_sslLog_error()
+
+    local success, err = pcall(function()
+        socket.sslLog(-1)
+    end)
+    assert(success == true, "设置内核固件中ssl功能的log等级失败，原因是", err)
+end
+
+-- adapter_id：1-14(正确范围)
+
+function tcp_tests.test_get_socket_create_error()
+
+    netCB = socket.create(15, taskName)
+    assert(netCB == nil, " 创建socket对象异常!")
+end
+
+
+
+function tcp_tests.test_get_socket_state_error()
+
+    netCB = socket.create(15, taskName)
+    assert(netCB == nil, " 创建socket对象异常!")
+    -- 获取 socket 当前状态；
+    local state, str = socket.state(netCB)
+
+    -- 第一个返回值
+    assert(state == nil ,"socket.state()第一个返回值应为空！")
+
+    -- 第二个返回值
+    assert(str == nil, "socket.state()第二个返回值应为空！")
+
+end
+
+
+
+function tcp_tests.test_get_socket_debug_error()
+
+    netCB = socket.create(15, taskName)
+    assert(netCB == nil, " 创建socket对象异常!")
+    -- 获取 socket 当前状态；
+       local success, err = pcall(function()
+        socket.debug(netCB, true)
+    end)
+    assert(success == true, "debug日志信息打开失败，错误原因是：", err)
+
+end
+
+
 
 -- TCP回环测试函数
---40次复杂数据回环，每次数据都不完全一致。
+-- 40次复杂数据回环，每次数据都不完全一致。
 function tcp_tests.test_tcp_echo()
-    local ip = "airtest.openluat.com"
-    local port = 2901
+
     local test_count = 40 -- 总发送次数
     local interval = 3000 -- 间隔时间
     local recive_count = 0 -- 接收计数
@@ -28,9 +166,32 @@ function tcp_tests.test_tcp_echo()
     local recv_buff = zbuff.create(1024)
     assert(recv_buff ~= nil, "接收数据缓冲区创建失败！")
 
+    --查询默认网卡
+    local adapter_id, last_reg_adapter_id = socket.dft()
+    local dft_adapter = false
+    if adapter_id >= 1 and adapter_id <= 14 then
+        dft_adapter = true
+    end
+    log.info("adapter_id", adapter_id, dft_adapter)
+
+    -- 查询默认网卡是否在定义的网络适配器范围内
+    assert(dft_adapter == true, "默认网卡不在定义的网络适配器范围内！")
+
     -- 创建socket对象
-    netCB = socket.create(nil, taskName)
+    netCB = socket.create(adapter_id, taskName)
     assert(netCB ~= nil, " 创建socket对象失败!")
+
+    --获取 socket 当前状态；
+    local state, str = socket.state(netCB)
+    --返回值类型判断
+    assert(type(state) == "number", "socket.state()第一个返回值类型错误！")
+    assert(type(str) == "string", "socket.state()第二个返回值类型错误！")
+
+    --第一个返回值
+    assert(state >= 0 and state <= 8, "socket.state()第一个返回值不在指定范围内！")
+
+    --第二个返回值
+    assert(str ~= nil, "socket.state()第二个返回值不为空！")
 
     -- 打开network的debug日志信息
     local success, err = pcall(function()
@@ -42,7 +203,6 @@ function tcp_tests.test_tcp_echo()
     local config_result = socket.config(netCB, nil, protocol, ssl)
     assert(type(config_result) == "boolean", "socket.config返回的数据类型错误")
     assert(config_result == true, "配置socket对象的参数失败")
-
 
     -- 客户端连接服务器 (超时时间为30S)
     local connect_result = libnet.connect(taskName, 30000, netCB, ip, port)
@@ -70,7 +230,7 @@ function tcp_tests.test_tcp_echo()
             math.random(0, 65535), -- 随机十六进制数
             math.random(1000, 9999) -- 随机数
         )
-        
+
         log.info("tcp_echo", string.format("第%d次发送数据: %s", recive_count + 1, test_str))
 
         -- 清空发送缓冲区并写入新数据

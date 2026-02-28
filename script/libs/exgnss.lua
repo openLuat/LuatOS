9c045fb377443b14bc93bd2e844c5aecbcc9464c
li <1398051498@qq.com>
2026-02-28 15:55:23 +0800

    update：修改了exvib扩展库中，i2c.send中间加等待的代码，修改了exgnss在没有进行exgnss.setup的情况下，GNSS_STATE消息中exgnss.opts.rtc不存在的问题

--- script/libs/exgnss.lua
+++ script/libs/exgnss.lua
@@ -173,7 +173,7 @@ sys.subscribe("GNSS_STATE", function(event)
         save_loc()
         tid=sys.timerLoopStart(save_loc,600000)
         timetid=sys.timerStart(timer_fnc,10000)
-        if exgnss.opts.rtc==true then
+        if exgnss.opts.rtc then
             sys.publish("NTP_UPDATE")
         end
     elseif event == "LOSE" or event == "CLOSE" then

119b5c9e18ebf08095ec8abf0cc451e3918ca1d0
tuoyiheng <1481840715@qq.com>
2026-03-16 14:37:07 +0800

    update: 更新所有demo中联网相关操作，修改关于exnetif.set_priority_order接口用法

--- module/Air780EPM/demo/httpsrv/netdrv_eth_spi.lua
+++ module/Air780EPM/demo/httpsrv/netdrv_eth_spi.lua
@@ -55,17 +55,24 @@ sys.subscribe("IP_READY", ip_ready_func)
 sys.subscribe("IP_LOSE", ip_lose_func)
 
 
+local function netdrv_eth_spi_task_func()
 -- 配置SPI外接以太网芯片CH390H的单网卡，exnetif.set_priority_order使用的网卡编号为socket.LWIP_ETH
 -- 本demo使用Air780EPM V1.3版本开发板测试，开发板上的硬件配置为：
 -- GPIO20为CH390H以太网芯片的供电使能控制引脚
 -- 使用spi0，片选引脚使用GPIO8
 -- 如果使用的硬件和以上描述的环境不同，根据自己的硬件配置修改以下参数
-exnetif.set_priority_order({
-    {
-        ETHERNET = {
-            pwrpin = 20, 
-            tp = netdrv.CH390,
-            opts = {spi = 0, cs = 8}
+    exnetif.set_priority_order({
+        {
+            ETHERNET = {
+                pwrpin = 20, 
+                tp = netdrv.CH390,
+                opts = {spi = 0, cs = 8}
+            }
         }
-    }
-})
+    })
+end
+
+-- 启动一个task，task的处理函数为netdrv_eth_spi_task_func
+-- 在处理函数中调用exnetif.set_priority_order设置网卡优先级
+-- 因为exnetif.set_priority_order要求必须在task中被调用，所以此处启动一个task
+sys.taskInit(netdrv_eth_spi_task_func)

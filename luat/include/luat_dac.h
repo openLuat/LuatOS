a26c98263106c5c8165ffbca59657aaf048a9deb
PeakRacing <peakracing@foxmail.com>
2026-04-13 15:06:22 +0800

    update:dac添加异步设计

--- luat/include/luat_dac.h
+++ luat/include/luat_dac.h
@@ -31,11 +31,19 @@ typedef enum {
 	LUAT_DAC_BITS_MAX,
 } luat_dac_bits_t;
 
+typedef enum {
+    LUAT_DAC_EVENT_TX_DONE,
+    LUAT_DAC_EVENT_TX_ERR,
+} luat_dac_event_t;
+
 typedef struct {
 	luat_dac_chl_t dac_chl;
 	luat_dac_samp_t samp_rate;
     luat_dac_bits_t bits;
 	uint32_t work_mode;
+    uint32_t cb_tx_len;                                     // 接收触发回调数据长度
+    int (*luat_dac_event_callback)(uint8_t id, luat_dac_event_t event, uint32_t tx_len, void *param); // 回调函数
+    void *userdata;                                         // 用户数据
 } luat_dac_config_t;
 
 int luat_dac_setup(uint32_t ch, luat_dac_config_t* config);

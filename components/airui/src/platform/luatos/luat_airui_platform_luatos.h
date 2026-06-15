3514365d82306c3c4deb33d8027c7986d314525c
zengeshuai <1287666071@qq.com>
2026-06-15 16:59:36 +0800

    add:airui, 深度休眠时支持tp休眠的触控芯片会进入休眠模式

--- components/airui/src/platform/luatos/luat_airui_platform_luatos.h
+++ components/airui/src/platform/luatos/luat_airui_platform_luatos.h
@@ -40,6 +40,7 @@ typedef struct {
     uint8_t rotation_buf_in_psram;   /**< 是否分配在 PSRAM（1）或堆（0） */
     uint8_t tp_suspended;            /**< 触摸是否已被 AirUI 挂起 */
     uint8_t tp_resume_needs_init;    /**< 唤醒时是否需要重新初始化触摸 */
+    uint8_t tp_resume_use_wakeup;    /**< 唤醒时是否通过 tp.wakeup 恢复 */
 } luatos_platform_data_t;
 
 /**

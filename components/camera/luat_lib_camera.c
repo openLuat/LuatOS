f7a4805d55a5b68763cf0e2e0b24e92a08d5b395
alienwalker <alienwalker@sina.com>
2026-06-18 12:37:45 +0800

    update:camera.stream的最小图片数据量默认值调整到1KB

--- components/camera/luat_lib_camera.c
+++ components/camera/luat_lib_camera.c
@@ -736,7 +736,7 @@ camera输出/停止数据流
 @id camera id
 @app_id 如果是usb摄像头，则输入usb应用id，其他留空
 @int 跳帧，针对USB摄像头，跳过N帧后上报，一般情况正常传输是30fps，如果脚本处理不过来，可以跳过N帧上报，默认是0，即不跳
-@int 图像数据最小长度，针对USB摄像头ISO传输可能漏数据的情况，只有大于最小长度的图像帧会上报，默认是10KB
+@int 图像数据最小长度，针对USB摄像头ISO传输可能漏数据的情况，只有大于最小长度的图像帧会上报，默认是1KB
 @return boolean 成功返回true,否则返回false
 @usage
 =
@@ -746,7 +746,7 @@ static int l_camera_stream(lua_State *L) {
 	int camera_id = luaL_checkinteger(L, 1);
     int app_id = luaL_optinteger(L, 2, -1);
     uint8_t jump_frame_cnt = luaL_optinteger(L, 3, 0);
-    uint32_t min_data_len = luaL_optinteger(L, 4, 10*1024);
+    uint32_t min_data_len = luaL_optinteger(L, 4, 1*1024);
 
     uint8_t usb_mode = 0;
     if (camera_id >= LUAT_CAMERA_TYPE_USB)

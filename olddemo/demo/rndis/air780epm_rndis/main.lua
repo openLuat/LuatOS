5599ff04ee4470a150e8f58184dd9a2ced592ee4
Wendal Chen <wendal1985@gmail.com>
2026-06-29 09:57:10 +0800

    fix: olddemo,air780epm,rndis的测试脚本,只需要加rndis就行, 后续加个ch390也好,不应该开wifi ap,那是air8000的事

--- olddemo/demo/rndis/air780epm_rndis/main.lua
+++ olddemo/demo/rndis/air780epm_rndis/main.lua
@@ -22,35 +22,6 @@ sys.taskInit(function()
     -- mobile.config(mobile.CONF_USB_ETHERNET, 1 + (1 << 1) + (1 << 2))
 end)
 
-dnsproxy = require("dnsproxy")
-dhcpsrv = require("dhcpsrv")
-httpplus = require("httpplus")
-
-function create_ap()
-    log.info("执行AP创建操作", "luatos8888")
-    wlan.createAP("luatos8888", "12345678")
-    sys.wait(1000)
-    netdrv.ipv4(socket.LWIP_AP, "192.168.4.1", "255.255.255.0", "0.0.0.0")
-    dnsproxy.setup(socket.LWIP_AP, socket.LWIP_GP)
-    apdhcpd = dhcpsrv.create({adapter=socket.LWIP_AP})
-    while 1 do
-        if netdrv.ready(socket.LWIP_GP) then
-            netdrv.napt(socket.LWIP_GP)
-            break
-        end
-        sys.wait(1000)
-    end
-end
-
-sys.taskInit(function()
-    sys.wait(100)
-    wlan.init()
-    sys.wait(100)
-
-    -- 启动AP测试
-    create_ap()
-end)
-
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
 sys.run()

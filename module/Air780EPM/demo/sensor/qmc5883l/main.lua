fba1a7b4a21ef1bd9eeec0f0d0e300167cc05078
江访 <1059053471@qq.com>
2026-07-21 11:45:01 +0800

    update:规范传感器demo main.lua 格式

--- module/Air780EPM/demo/sensor/qmc5883l/main.lua
+++ module/Air780EPM/demo/sensor/qmc5883l/main.lua
@@ -35,10 +35,29 @@ log.info("main", PROJECT, VERSION)
 -- 设置日志输出风格为样式2（建议调试时开启）
 -- log.style(2)
 
+
 -- 如果内核固件支持errDump功能，此处进行配置，【强烈建议打开此处的注释】
+-- 因为此功能模块可以记录并且上传脚本在运行过程中出现的语法错误或者其他自定义的错误信息，可以初步分析一些设备运行异常的问题
+-- 以下代码是最基本的用法，更复杂的用法可以详细阅读API说明文档
+-- 启动errDump日志存储并且上传功能，600秒上传一次
 -- if errDump then
 --     errDump.config(true, 600)
 -- end
+
+
+-- 使用LuatOS开发的任何一个项目，都强烈建议使用远程升级FOTA功能
+-- 可以使用合宙的iot.openluat.com平台进行远程升级
+-- 也可以使用客户自己搭建的平台进行远程升级
+-- 远程升级的详细用法，可以参考fota的demo进行使用
+
+
+-- 启动一个循环定时器
+-- 每隔3秒钟打印一次总内存，实时的已使用内存，历史最高的已使用内存情况
+-- 方便分析内存使用是否有异常
+-- sys.timerLoopStart(function()
+--     log.info("mem.lua", rtos.meminfo())
+--     log.info("mem.sys", rtos.meminfo("sys"))
+-- end, 3000)
 
 -- 加载 qmc5883l_demo.lua 演示模块
 require "qmc5883l_demo"

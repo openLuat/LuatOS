80a0dd2f5c52a5b79482f308e28b98f5ef3f23df
Wendal Chen <wendal1985@gmail.com>
2026-07-30 14:41:23 +0800

    fix: pc模拟器,sms,补齐新增的函数

--- bsp/pc/port/driver/luat_sms_pc.c
+++ bsp/pc/port/driver/luat_sms_pc.c
@@ -8,3 +8,7 @@ int luat_sms_send_msg_v2(uint8_t *pdu_data, size_t pdu_len) {
     LLOGE("sms send pdu not support in pc env, yet %p %d", pdu_data, pdu_len);
     return -1;
 }
+
+void luat_sms_get_last_send_result(uint8_t *rp_cause, uint8_t *tp_cause, uint8_t *msg_ref) {
+
+}

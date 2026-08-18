ee0bdcaefe58906646bcdee75a351fb88d5311a1
shenyuanyuan <34321268@qq.com>
2026-08-18 09:44:14 +0800

    update: 修改air724 crypto demo

--- module/Air724/demo/crypto/crypto_app.lua
+++ module/Air724/demo/crypto/crypto_app.lua
@@ -1,6 +1,6 @@
 --[[
 @module  crypto_app
-@summary crypto_app应用功能模块 
+@summary crypto_app应用功能模块
 @version 1.0
 @date    2025.10.15
 @author  沈园园
@@ -11,7 +11,7 @@
 本文件没有对外接口，直接在main.lua中require "crypto_app"就可以加载运行；
 ]]
 
---加密运算主函数
+-- 加密运算主函数
 local function crypto_task_func()
 
     -- MD5/哈希MD5,输出结果已经hex编码
@@ -42,7 +42,7 @@ local function crypto_task_func()
     log.info("AES", "aes-128-ecb", data_encrypt:toHex())
     log.info("AES", "aes-128-cbc", data2_encrypt:toHex())
 
-    -- AES解密, 未经Hex编码, AES-128-ECB/CBC 算法 对称解密,，对齐方式："PKCS7"
+    -- AES解密, 未经Hex编码, AES-128-ECB/CBC 算法 对称解密，对齐方式："PKCS7"
     local data_decrypt = crypto.cipher_decrypt("AES-128-ECB", "PKCS7", data_encrypt, "1234567890123456")
     local data2_decrypt = crypto.cipher_decrypt("AES-128-CBC", "PKCS7", data2_encrypt, "1234567890123456", "1234567890666666")
     log.info("AES", "aes-128-ecb", data_decrypt)
@@ -105,7 +105,7 @@ local function crypto_task_func()
     
     log.info("随机数测试")
     for i=1, 10 do
-        sys.wait(100)      -- 随机数生成间隔，100ms
+        sys.wait(100)   -- 等待100ms，避免连续打印日志过快
         log.info("crypto", "真随机数",string.unpack("I",crypto.trng(4)))
         -- log.info("crypto", "伪随机数",math.random()) -- 输出的是浮点数,不推荐
         -- log.info("crypto", "伪随机数",math.random(1, 65525)) -- 不推荐
@@ -131,7 +131,7 @@ local function crypto_task_func()
     --将数据进行base64解码
     -- 本函数与 string.fromBase64 是同一个
     local data = crypto.base64_decode("MTIz")
-    log.info("base64", "decode", bdata, data)--123    
+    log.info("base64", "decode", bdata, data)--123
 
     -- 打印所有支持的cipher
     if crypto.cipher_list then
@@ -275,8 +275,8 @@ local function crypto_task_func()
     end    
 
     log.info("crypto", "ALL Done")
-    sys.wait(100000)  -- 演示结束后保持运行，避免重复执行
+    sys.wait(100000)   -- 等待100000ms，演示结束后保持任务运行
 end    
 
---创建一个task，并且运行task的主函数crypto_task_func
+-- 创建一个task，并且运行task的主函数crypto_task_func
 sys.taskInit(crypto_task_func)
--- module/Air724/demo/crypto/main.lua
+++ module/Air724/demo/crypto/main.lua
@@ -1,6 +1,6 @@
 --[[
 @module  main
-@summary LuatOS用户应用脚本文件入口，总体调度应用逻辑 
+@summary LuatOS用户应用脚本文件入口，总体调度应用逻辑
 @version 1.0
 @date    2025.10.15
 @author  沈园园
@@ -9,8 +9,6 @@
 演示有关加解密的各种 API 的功能包含MD5，SHA，哈希(MD5，SHA)，AES/DEC/3DES，CRC，checksum校验和
 更多说明参考本目录下的readme.md文件
 ]]
-
-
 --[[
 必须定义PROJECT和VERSION变量，Luatools工具会用到这两个变量，远程升级功能也会用到这两个变量
 PROJECT：项目名，ascii string类型
@@ -18,17 +16,18 @@ PROJECT：项目名，ascii string类型
 VERSION：项目版本号，ascii string类型
         如果使用合宙iot.openluat.com进行远程升级，必须按照"XXX.YYY.ZZZ"三段格式定义：
             X、Y、Z各表示1位数字，三个X表示的数字可以相同，也可以不同，同理三个Y和三个Z表示的数字也是可以相同，可以不同
-            因为历史原因，YYY这三位数字必须存在，但是没有任何用处，可以一直写为999
+            因为历史原因，YYY这三位数字必须存在，但是没有任何用处，可以一直写为000
         如果不使用合宙iot.openluat.com进行远程升级，根据自己项目的需求，自定义格式即可
 ]]
+
 PROJECT = "crypto_demo"
 VERSION = "001.999.000"
-
 
 -- 在日志中打印项目名和项目版本号
 log.info("main", PROJECT, VERSION)
 
-
+-- 设置日志输出风格为样式2（建议调试时开启）
+-- log.style(2)
 
 
 -- 如果内核固件支持errDump功能，此处进行配置，【强烈建议打开此处的注释】
@@ -54,10 +53,12 @@ log.info("main", PROJECT, VERSION)
 --     log.info("mem.sys", rtos.meminfo("sys"))
 -- end, 3000)
 
--- 加载crypto_app应用功能模块
+
+-- 加载加解密应用功能模块
 require "crypto_app"
+
 
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
 sys.run()
--- sys.run()之后不要加任何语句!!!!!因为添加的任何语句都不会被执行
+-- sys.run()之后后面不要加任何语句!!!!!

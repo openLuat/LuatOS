3d49f4ed270ed9e9fed2517d747835124149aae8
Wendal Chen <wendal1985@gmail.com>
2026-09-16 11:37:30 +0800

    add: log库支持日志口用户自定义指令 log.set_usercmd_cb/log.usercmd_write, 宏LUAT_USE_LOG_USER_CMD控制

--- luat/include/luat_log.h
+++ luat/include/luat_log.h
@@ -52,4 +52,9 @@ void luat_log_dump(const char* tag, void* ptr, size_t len);
 
 #define LLOGC(cond, format, ...) if(cond) do { luat_log_log(LUAT_LOG_DEBUG, LUAT_LOG_TAG, format, ##__VA_ARGS__); } while(0)
 
+#ifdef LUAT_USE_LOG_USER_CMD
+// BSP 日志口收到用户自定义指令时调用(由 log 库实现);data 生命周期仅本次调用
+void luat_log_user_cmd_push(int cmd, const uint8_t *data, size_t len);
+#endif
+
 #endif

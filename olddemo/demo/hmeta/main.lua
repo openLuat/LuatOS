b73fa4da6a03f515c2b95ed98ffe7a150b30d740
Wendal Chen <wendal1985@gmail.com>
2026-09-21 14:45:38 +0800

    update: olddemo,hmeta,把devid和muid也打印出来

--- olddemo/demo/hmeta/main.lua
+++ olddemo/demo/hmeta/main.lua
@@ -18,7 +18,7 @@ sys.taskInit(function()
         -- hmeta识别底层模组类型的
         -- 不同的模组可以使用相同的bsp,但根据封装的不同,根据内部数据仍可识别出具体模块
         log.info("hmeta", hmeta.model(), hmeta.hwver and hmeta.hwver())
-        log.info("bsp",   rtos.bsp())
+        log.info("bsp",   rtos.bsp(), hmeta.devid(), hmeta.muid())
         sys.wait(3000)
     end
     log.info("这个bsp不支持hmeta库哦")
